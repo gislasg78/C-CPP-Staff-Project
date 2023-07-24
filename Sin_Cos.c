323e9036e37a2b9ba63d610d9dd6af5724ed4719
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2023-07-24 09:53:44 -0600

    Sine and cosine program using Taylor series
    Added different functions such as absolute value. The factorial and power functions were improved. Some function instructions have been reordered and their indentation has been improved. Documented some feature comments in more detail. New symbolic constants were created and the aesthetics of the results presentation messages were fixed.

--- Sin_Cos.c
+++ Sin_Cos.c
@@ -1,66 +1,103 @@
 /********** Trigonometric functions of sine and cosine. **********
  ** Source Code:	Sin_Cos.c	               		**
  ** Author:		Gustavo Islas Gálvez.			**
- ** Creation Date:	Friday, June 30, 2023.          	**
+ ** Creation Date:	Thursday, August 31, 2023.          	**
  ** Purpose:		Calculates through the Taylor series	**
  **			the trigonometric functions of sine and	**
  **			cosine according to a determined value	**
  **			that is expressed in radians.		**
+ **			Most of the math functions were		**
+ **			implemented.				**
 *****************************************************************/
-// C Standard Libraries.
+//C Standard Libraries.
 #include <stdio.h>
 
-// Work Symbolic Constants.
-#define EVEN_AND_ODD(value, addneu)	(2 * (value) + (addneu))
-#define NUM_TERMS			11
-#define PI				3.14159265359
-#define RADIANS(angle)			((angle) * PI / 180.0)
+//Working Macros.
+#define EVEN_AND_ODD(value, addneutr)	(V_TWO * (value) + (addneutr))
+#define RADIANS(angle)			((angle) * V_PI / V_HALF_CIRC)
 
 //Numeric Symbolic Constants.
-#define V_MINUS_ONE			-1
+#define V_HALF_CIRC			180.0
+#define V_NUM_TERMS			11
+#define V_MINUS_ONE			-1.0
 #define V_ONE				1
+#define V_PI				3.14159265359
+#define	V_TWO				2
 #define V_ZERO				0
 
 /*****************************************************************
+ ** Function:		absolute.				**
+ ** Explanation:	Returns as a result the positive or 	**
+ **			unsigned value of a certain value,	**
+ **			as long as it is subjected to a series	**
+ **			of conditions such as being greater	**
+ **			than zero, to be positive; be zero,	**
+ **			to be zero, and less than zero,		**
+ **			to become positive by sign		**
+ **			neutralization.				**
+ ** Input Parms:	double int_value.			**
+ ** Output Parms:	None.					**
+ ** Result:		the base value (int_value) is		**
+ **			sign-neutralized.			**
+ ****************************************************************/
+double absolute(int int_value)
+	{
+		return	(int_value==V_ZERO) ? V_ZERO
+		: (int_value>V_ZERO) ? int_value : -int_value;
+	}
+
+/*****************************************************************
  ** Function:		factorial.				**
- ** Explanation:	Gets the value of the factorial of any  **
+ ** Explanation:	Gets the value of the factorial of any	**
  **			integer by recursively multiplying it by**
  **			its descendant surrogate numbers.	**
- ** Input Parms:	integer int_n.                          **
+ ** Input Parms:	integer int_num.			**
  ** Output Parms:	None.					**
  ** Result:		Factorial from 'i' to 'n'.		**
  ****************************************************************/
-double factorial(int int_n)
+double factorial(int int_num)
     {
-        return (int_n <= V_ONE) ? V_ONE
-        		: (double)int_n * factorial(int_n - V_ONE);
+        return (int_num <= V_ZERO) ? V_ONE
+        	: (double) int_num * factorial(int_num - V_ONE);
     }
 
 /*****************************************************************
  ** Function:		potency.				**
  ** Explanation:	Returns a base coefficient raised to the**
  **			specified power recursively by means of	**
- **			successive multiplications.		**
+ **			successive multiplications or divisions.**
+ **								**
+ **			This function can also be programmed	**
+ **			recursively in the form:		**
+ **			(int_exp == V_ZERO) ? V_ONE : dbl_base	**
+ **			 * potency(dbl_base, int_exp - V_ONE);	**
+ **								**
  ** Input Parms:	double dbl_base,			**
  **			integer int_exp.			**
  ** Output Parms:	None.					**
  ** Result:		dbl_base raised to int_exp.		**
  ****************************************************************/
 double potency(double dbl_base, int int_exp)
-    {
-        return (int_exp == V_ZERO) ? V_ONE
-			: dbl_base * potency(dbl_base, int_exp - V_ONE);
-    }
+	{
+		double dbl_ret=V_ONE;
+		int int_exp_pow=absolute(int_exp);
+
+		for (int int_i=V_ZERO; int_i<int_exp_pow; int_i++)
+			dbl_ret = (int_exp > V_ZERO) ? dbl_ret * dbl_base :
+				  (int_exp < V_ZERO) ? dbl_ret / dbl_base : V_ONE;
+
+		return dbl_ret;
+	}
 
 /*****************************************************************
- ** Function:		sin_cos.				**
+ ** Function:		sinus_cosinus.				**
  ** Explanation:	The value of the sine or cosine is	**
  **			obtained as a result, depending on an	**
  **			additive neutral passed as a parameter, **
  **			of a certain value in angles expressed	**
  **			in radians, through the Taylor series.	**
  ** Input Parms:	double dbl_base,			**
- **			integer int_addneut.			**
+ **			integer int_addneutr.			**
  ** Output Parms:	None.					**
  ** Result:		To calculate the trigonometric function **
  **			of sine, the Taylor number series	**
@@ -73,18 +110,15 @@ double potency(double dbl_base, int int_exp)
  **			applied with 'even' numbers is:		**
  **			cos(x) = 1 - (x^2 / 2!) + (x^4 / 4!) -	**
  **			(x^6 / 6!) + ...			**
- **								**
  ****************************************************************/
-double sin_cos(double dbl_angle, int int_addneut)
+double sinus_cosinus(double dbl_angle, int int_addneutr)
     {
-        double dbl_outcome = V_ZERO;
+        double dbl_outcome=V_ZERO;
 
-        for (int int_i = V_ZERO; int_i < NUM_TERMS; int_i++)
-            {
+        for (int int_i=V_ZERO; int_i<V_NUM_TERMS; int_i++)
                 dbl_outcome += potency(V_MINUS_ONE, int_i)
-                            *  potency(dbl_angle, EVEN_AND_ODD(int_i, int_addneut))
-                            /  factorial(EVEN_AND_ODD(int_i, int_addneut));
-            }
+                            *  potency(dbl_angle, EVEN_AND_ODD(int_i, int_addneutr))
+                            /  factorial(EVEN_AND_ODD(int_i, int_addneutr));
 
         return dbl_outcome;
     }
@@ -100,20 +134,30 @@ double sin_cos(double dbl_angle, int int_addneut)
  **			cosine of an angle expressed in radians **
  **			are obtained.				**
  ****************************************************************/
-int main(int int_argc, char *ch_argv[])
+int main()
     {
-        double dbl_value = V_ZERO;
-        double dbl_radns = V_ZERO;
+        double dbl_value=V_ZERO;
+        double dbl_radns=V_ZERO;
 
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
+	printf("| Funciones de seno y coseno por series de Taylor. |\n");
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
 	printf("Valor en grados: ");
 	scanf("%lf", &dbl_value);
 
         dbl_radns=RADIANS(dbl_value);
 
         printf("\n");
-        printf("El valor  de [%g] a radianes aprox es: [%g].\n", dbl_value, dbl_radns);
-        printf("El seno   de [%g] aproximadamente  es: [%g].\n", dbl_value, sin_cos(dbl_radns, V_ONE));
-        printf("El coseno de [%g] aproximadamente  es: [%g].\n", dbl_value, sin_cos(dbl_radns, V_ZERO));
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
+	printf("| Funciones trigonométricas del seno y del coseno. |\n");
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
+        printf("| Valor en grados   : [%lf].\n", dbl_value);
+	printf("| Valor en radianes : [%lf].\n", dbl_radns);
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
+        printf("| Valor del seno    : [%lf].\n", sinus_cosinus(dbl_radns, V_ONE));
+        printf("| Valor del coseno  : [%lf].\n", sinus_cosinus(dbl_radns, V_ZERO));
+	printf("+---|----+---|----+---|----+---|----+---|----+---|--\n");
+	printf("\n");
 
         return dbl_radns;
     }
