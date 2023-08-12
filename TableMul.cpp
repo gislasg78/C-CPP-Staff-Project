6379446e8af4d8ee1a43246f71bb213ecb7549e8
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2023-08-12 10:31:37 -0600

    Dynamic Multiplication Tables Program
    This program uses two types of collectors: the list and the vector; Its purpose is to store in these collectors an object of type 'My List' that also has its own collector of type pointer to list, with the aim of learning to use dynamic structures from the C++ STL library. The 'size_t' data type is also used.

--- TableMul.cpp
+++ TableMul.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <vector>
 
 #define V_ZERO	0
 
@@ -8,64 +9,72 @@ using namespace std;
 class MyList
 	{
 		private:
-			static int int_Counting;
-			std::list<int> *ptr_data=NULL;;
+			static int sttc_int_Counting;
+			std::list<int> *ptr_lst_int_data=nullptr;
 
 		public:
 			MyList()
 				{
-					int_Counting++;
-					this->ptr_data=new std::list<int>();
+					sttc_int_Counting++;
+					this->ptr_lst_int_data=new std::list<int>();
 				}
 
 			void addItem (int int_Item)
 				{
-					this->ptr_data->push_back(int_Item);
+					this->ptr_lst_int_data->push_back(int_Item);
 				}
 
 			void printItems()
 				{
-					for (auto it_ptr_data=this->ptr_data->begin(); it_ptr_data!=this->ptr_data->end(); ++it_ptr_data)
-						std::cout << "[" << *it_ptr_data << "]\t";
+					for (auto it_ptr_lst_int_data=this->ptr_lst_int_data->begin();
+						it_ptr_lst_int_data!=this->ptr_lst_int_data->end(); ++it_ptr_lst_int_data)
+							std::cout << "[" << *it_ptr_lst_int_data << "]\t";
 
 					std::cout << std::endl;
 				}
 	};
 
-int MyList::int_Counting=V_ZERO;
+int MyList::sttc_int_Counting=V_ZERO;
 
 
 int main()
 	{
-		std::list<MyList> lst_MyList;
 		MyList *ptr_MyList=NULL;
 
-		int int_Rows=V_ZERO;
-		int int_Cols=V_ZERO;
+		std::list<MyList> lst_MyList;
+		std::vector<MyList> vec_MyList;
+
+		size_t szt_Rows=V_ZERO;
+		size_t szt_Cols=V_ZERO;
 
 		std::cout << endl;
 		std::cout << "+---|----+---|----+---|----+---+" << endl;
 		std::cout << "|Dynamic multiplication tables.|" << endl;
 		std::cout << "+---|----+---|----+---|----+---+" << endl;
 		std::cout << "Rows: ";
-		std::cin >> int_Rows;
+		std::cin >> szt_Rows;
 		std::cout << "Cols: ";
-		std::cin >> int_Cols;
+		std::cin >> szt_Cols;
 
-		for (int int_row=V_ZERO; int_row<int_Rows; int_row++)
+		for (size_t szt_row=V_ZERO; szt_row<szt_Rows; szt_row++)
 			{
                         	ptr_MyList=new MyList();
 
-				for (int int_col=V_ZERO; int_col<int_Cols; int_col++)
-					ptr_MyList->addItem(int_row * int_col);
+				for (size_t szt_col=V_ZERO; szt_col<szt_Cols; szt_col++)
+					ptr_MyList->addItem(szt_row * szt_col);
 
 				lst_MyList.push_back(*ptr_MyList);
+				vec_MyList.push_back(*ptr_MyList);
 
 				delete ptr_MyList;
 			}
 
-		std::cout << "Size:\t[" << lst_MyList.size() << "]." << endl;
+		std::cout << endl << "List size.\tRows:\t[" << lst_MyList.size() << "]." << endl;
 		for (auto it=std::begin(lst_MyList); it!=std::end(lst_MyList); ++it)
+			it->printItems();
+
+		std::cout << endl << "Vector size.\tRows:\t[" << vec_MyList.size() << "]." << endl;
+		for (auto it=std::begin(vec_MyList); it!=std::end(vec_MyList); ++it)
 			it->printItems();
 
 		return V_ZERO;
