c47bad4c621f7b21e9700c4d963494827fa15274
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-05 12:11:46 -0600

    Pointer pointer to an integer type times tables
    This program generates multiplication tables in ANSI C and some output messages were changed, a count of each generated result was added and these output messages were translated into English.

--- TableMul.c
+++ TableMul.c
@@ -1,7 +1,7 @@
-/************** Basic multiplication table generator *************
+/************** Basic multiplication table generator. ************
  ** Source Code:	TableMul.c				**
  ** Author:		Gustavo Islas Gálvez.			**
- ** Creation Date:	Thursday, August 31, 2023.		**
+ ** Creation Date:	Saturday, December 30, 2023.		**
  ** Purpose:		This program generates a set of		**
  **			multiplication tables from zero to ten	**
  **			using the creation of a pointer to a	**
@@ -14,15 +14,15 @@
 #include <stdlib.h>
 
 //Symbolic constants of minimum and maximum limits.
-#define LIM_MIN	0
-#define LIM_MAX	10
+#define V_LIM_MAX	100
+#define V_LIM_MIN	0
 
 //Work Symbolic Constants.
-#define V_ONE	1
-#define V_ZERO	0
+#define V_ONE		1
+#define V_ZERO		0
 
 /*****************************************************************
- ** Function:		tablemult.				**
+ ** Function:		int **ptr_ptr_int_tablemult.		**
  ** Explanation:	This function returns a pointer pointer	**
  **			to an integer type containing the array	**
  **			of times tables requested by the user,	**
@@ -38,29 +38,36 @@
  **			as many rows and columns as they are	**
  **			passed by formal and current parameters.**
  ****************************************************************/
-int **tablemult(int int_Rows, int int_Cols)
+int **ptr_ptr_int_tablemult(int int_Rows, int int_Cols)
 	{
-		int **ppa = NULL;
+		int int_counting_results = V_ZERO;
+		int **ptr_ptr_int_tablemult_aux = NULL;
 
 		/*------------------------------------------------
 		 * Creation and dynamic assignment of 		--
 		 * multiplication tables, given the number of	--
 		 * rows and columns they must contain.		--
 		 *----------------------------------------------*/
-		if ( ( ppa = (int **) malloc( (int_Rows+V_ONE) * sizeof(int *) ) ) != NULL )
-			for (int int_row=V_ZERO; int_row<=int_Rows; int_row++)
-				if ( ( ppa[int_row] = (int *) malloc ( (int_Cols+V_ONE) * sizeof(int) ) ) != NULL)
-					{
-						for (int int_col=V_ZERO; int_col<=int_Cols; int_col++)
-							{
-								ppa[int_row][int_col] = int_row * int_col;
-								printf("[%2.d]x[%2.i]=[%3.d].\n", int_row, int_col, *(*(ppa+int_row)+int_col));
-							}
+		if ( ( ptr_ptr_int_tablemult_aux = (int **) malloc( (int_Rows+V_ONE) * sizeof(int *) ) ) != NULL )
+			{
+				printf("\n");
 
-						printf("\n");
-					}
+				for (int int_row=V_ZERO; int_row<=int_Rows; int_row++)
+					if ( ( ptr_ptr_int_tablemult_aux[int_row] = (int *) malloc ( (int_Cols+V_ONE) * sizeof(int) ) ) != NULL)
+						{
+							for (int int_col=V_ZERO; int_col<=int_Cols; int_col++)
+								{
+									ptr_ptr_int_tablemult_aux[int_row][int_col] = int_row * int_col;
+									printf("# [%5.d]:\t[%3.d]\tx\t[%3.i]\t=\t[%5.d].\n", int_counting_results++, int_row, int_col, *(*(ptr_ptr_int_tablemult_aux+int_row)+int_col));
+								}
 
-		return (ppa);
+							printf("\n");
+						}
+
+				printf("[%d] Generated output results.\n", int_counting_results);
+			}
+
+		return (ptr_ptr_int_tablemult_aux);
 	}
 
 /*****************************************************************
@@ -79,17 +86,17 @@ int **tablemult(int int_Rows, int int_Cols)
 *****************************************************************/
 int main()
 	{
-		int **ppRec=NULL;
 		int int_Y=V_ZERO;
 		int int_X=V_ZERO;
+		int **ptr_ptr_int_tablemult_recovered=NULL;
 
-		printf("+---|----+---|----+---|----+---|----+\n");
-		printf("|       Tablas de multiplicar.      |\n");
-		printf("+---|----+---|----+---|----+---|----+\n");
-		printf("Renglones desde [%d] hasta [%d]: ", LIM_MIN, LIM_MAX);
-		scanf("%i", &int_Y);
-		printf("Columnas  desde [%d] hasta [%d]: ", LIM_MIN, LIM_MAX);
+		printf("+---|----+---|----+---|----+---|---+----|---+\n");
+		printf("| Dynamic multiplication tables generator.  |\n");
+		printf("+---|----+---|----+---|----+---|---+----|---+\n");
+		printf("Columns from [%d] to [%d]: ", V_LIM_MIN, V_LIM_MAX);
 		scanf("%d", &int_X);
+		printf("Rows    from [%d] to [%d]: ", V_LIM_MIN, V_LIM_MAX);
+		scanf("%i", &int_Y);
 
 		printf("\n");
 
@@ -98,27 +105,27 @@ int main()
 		 * for rows and columns of the multiplication	**
 		 * tables are within established intervals.	**
 		 * -------------------------------------------- */
-		if (int_Y >= LIM_MIN && int_Y <= LIM_MAX)
-			if (int_X >= LIM_MIN && int_X <= LIM_MAX)
+		if (int_Y >= V_LIM_MIN && int_Y <= V_LIM_MAX)
+			if (int_X >= V_LIM_MIN && int_X <= V_LIM_MAX)
 				{
 					/* Call to the function that returns the pointer of pointer. */
-					ppRec=tablemult(int_Y, int_X);
+					ptr_ptr_int_tablemult_recovered=ptr_ptr_int_tablemult(int_Y, int_X);
 
 					/* Validate that the pointer of pointers has a valid memory address value */
-					if (ppRec!=NULL)
+					if (ptr_ptr_int_tablemult_recovered)
 						{
 							/* Free the memory allocated to each of the rows. */
 							for (int int_n_row=V_ZERO; int_n_row<int_Y; int_n_row++)
-								free(ppRec[int_n_row]);
+								free(*(ptr_ptr_int_tablemult_recovered+int_n_row));
 
                                 			/* Free the memory allocated to the array of pointers. */
-                                			free(ppRec);
+                                			free(ptr_ptr_int_tablemult_recovered);
 						}
 				}
 			else
-				printf("¡Error! [%d] columnas  no están entre [%d] y [%d].\n", int_X, LIM_MIN, LIM_MAX);
+				printf("Error! [%d] Columns are not between [%d] and [%d].\n", int_X, V_LIM_MIN, V_LIM_MAX);
 		else
-				printf("¡Error! [%d] renglones no están entre [%d] y [%d].\n", int_Y, LIM_MIN, LIM_MAX);
+				printf("Error! [%d] Rows    are not between [%d] and [%d].\n", int_X, V_LIM_MIN, V_LIM_MAX);
 
 		return V_ZERO;
 	}
