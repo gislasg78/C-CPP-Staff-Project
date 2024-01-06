f7eafd5911e31076d8587a2b5adfc0280876f372
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-06 07:47:18 -0600

    Pointer pointer to an integer type times tables
    Some output messages were changed in this program to inform about each table and each record corresponding to it, the limits and nomenclatures of these were modified to establish limits on the number of records generated and some sections of the code were refactored.

--- TableMul.c
+++ TableMul.c
@@ -14,12 +14,14 @@
 #include <stdlib.h>
 
 //Symbolic constants of minimum and maximum limits.
-#define V_LIM_MAX	100
-#define V_LIM_MIN	0
+#define	V_LOWER_LIMIT_COLS	1
+#define V_LOWER_LIMIT_ROWS	1
+#define V_UPPER_LIMIT_COLS	11
+#define V_UPPER_LIMIT_ROWS	11
 
 //Work Symbolic Constants.
-#define V_ONE		1
-#define V_ZERO		0
+#define V_ONE			1
+#define V_ZERO			0
 
 /*****************************************************************
  ** Function:		int **ptr_ptr_int_tablemult.		**
@@ -48,21 +50,25 @@ int **ptr_ptr_int_tablemult(int int_Rows, int int_Cols)
 		 * multiplication tables, given the number of	--
 		 * rows and columns they must contain.		--
 		 *----------------------------------------------*/
-		if ( ( ptr_ptr_int_tablemult_aux = (int **) malloc( (int_Rows+V_ONE) * sizeof(int *) ) ) != NULL )
+		if ( ( ptr_ptr_int_tablemult_aux = (int **) malloc( (int_Rows + V_ONE) * sizeof(int *) ) ) != NULL )
 			{
 				printf("\n");
 
-				for (int int_row=V_ZERO; int_row<=int_Rows; int_row++)
-					if ( ( ptr_ptr_int_tablemult_aux[int_row] = (int *) malloc ( (int_Cols+V_ONE) * sizeof(int) ) ) != NULL)
-						{
-							for (int int_col=V_ZERO; int_col<=int_Cols; int_col++)
-								{
-									ptr_ptr_int_tablemult_aux[int_row][int_col] = int_row * int_col;
-									printf("# [%5.d]:\t[%3.d]\tx\t[%3.i]\t=\t[%5.d].\n", int_counting_results++, int_row, int_col, *(*(ptr_ptr_int_tablemult_aux+int_row)+int_col));
-								}
+				for (int int_row = V_ZERO; int_row < int_Rows; int_row++)
+					{
+						printf("Table # [%2.i] of [%2.i].\n", int_row, int_Rows);
 
-							printf("\n");
-						}
+						if ( ( ptr_ptr_int_tablemult_aux[int_row] = (int *) malloc ( (int_Cols + V_ONE) * sizeof(int) ) ) != NULL)
+							{
+								for (int int_col = V_ZERO; int_col < int_Cols; int_col++)
+									{
+										ptr_ptr_int_tablemult_aux[int_row][int_col] = int_row * int_col;
+										printf("# [%3.d]:\t[%2.d]\tx\t[%2.i]\t=\t[%3.d].\n", int_counting_results++, int_row, int_col, *(*(ptr_ptr_int_tablemult_aux+int_row)+int_col));
+									}
+
+								printf("\n");
+							}
+					}
 
 				printf("[%d] Generated output results.\n", int_counting_results);
 			}
@@ -86,36 +92,34 @@ int **ptr_ptr_int_tablemult(int int_Rows, int int_Cols)
 *****************************************************************/
 int main()
 	{
-		int int_Y=V_ZERO;
-		int int_X=V_ZERO;
-		int **ptr_ptr_int_tablemult_recovered=NULL;
+		int int_Y = V_ZERO;
+		int int_X = V_ZERO;
+		int **ptr_ptr_int_tablemult_recovered = NULL;
 
 		printf("+---|----+---|----+---|----+---|---+----|---+\n");
 		printf("| Dynamic multiplication tables generator.  |\n");
 		printf("+---|----+---|----+---|----+---|---+----|---+\n");
-		printf("Columns from [%d] to [%d]: ", V_LIM_MIN, V_LIM_MAX);
-		scanf("%d", &int_X);
-		printf("Rows    from [%d] to [%d]: ", V_LIM_MIN, V_LIM_MAX);
+		printf("Rows    from [%d] to [%d]: ", V_LOWER_LIMIT_ROWS, V_UPPER_LIMIT_ROWS);
 		scanf("%i", &int_Y);
-
-		printf("\n");
+		printf("Columns from [%d] to [%d]: ", V_LOWER_LIMIT_COLS, V_UPPER_LIMIT_COLS);
+		scanf("%d", &int_X);
 
 		/* -------------------------------------------- **
 		 * Here it is checked that the values entered	**
 		 * for rows and columns of the multiplication	**
 		 * tables are within established intervals.	**
 		 * -------------------------------------------- */
-		if (int_Y >= V_LIM_MIN && int_Y <= V_LIM_MAX)
-			if (int_X >= V_LIM_MIN && int_X <= V_LIM_MAX)
+		if (int_Y >= V_LOWER_LIMIT_ROWS && int_Y <= V_UPPER_LIMIT_ROWS)
+			if (int_X >= V_LOWER_LIMIT_COLS && int_X <= V_UPPER_LIMIT_COLS)
 				{
 					/* Call to the function that returns the pointer of pointer. */
-					ptr_ptr_int_tablemult_recovered=ptr_ptr_int_tablemult(int_Y, int_X);
+					ptr_ptr_int_tablemult_recovered = ptr_ptr_int_tablemult(int_Y, int_X);
 
 					/* Validate that the pointer of pointers has a valid memory address value */
 					if (ptr_ptr_int_tablemult_recovered)
 						{
 							/* Free the memory allocated to each of the rows. */
-							for (int int_n_row=V_ZERO; int_n_row<int_Y; int_n_row++)
+							for (int int_n_row = V_ZERO; int_n_row < int_Y; int_n_row++)
 								free(*(ptr_ptr_int_tablemult_recovered+int_n_row));
 
                                 			/* Free the memory allocated to the array of pointers. */
@@ -123,9 +127,9 @@ int main()
 						}
 				}
 			else
-				printf("Error! [%d] Columns are not between [%d] and [%d].\n", int_X, V_LIM_MIN, V_LIM_MAX);
+				printf("Error! [%d] Columns are not between [%d] and [%d].\n", int_X, V_LOWER_LIMIT_COLS, V_UPPER_LIMIT_COLS);
 		else
-				printf("Error! [%d] Rows    are not between [%d] and [%d].\n", int_X, V_LIM_MIN, V_LIM_MAX);
+				printf("Error! [%d] Rows    are not between [%d] and [%d].\n", int_Y, V_LOWER_LIMIT_ROWS, V_UPPER_LIMIT_ROWS);
 
 		return V_ZERO;
 	}
