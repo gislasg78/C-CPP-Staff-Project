63f34aef1bb9ef34bc8972416fcd0b65db1a75ae
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-07 11:59:52 -0600

    Hanoi Towers Algorithm Solving Program
    This program solves the Towers of Hanoi algorithm through recursive cycles that solve the enigma of moving a certain number of saucers from one container to another, passing through two other containers, in such a way that the discs are stacked from the smallest to the largest starting through the upper part and that each transfer it is checked that a larger disc can never be on a smaller disc. The algorithm requires (2^n) iterations to solve.

--- Hanoi.c
+++ Hanoi.c
@@ -1,188 +1,217 @@
-/*************************************************************************
-** Programa:	Hanoi.c							**
-** Autor:	Gustavo Islas Gálvez.					**
-** Fecha:	Viernes 09 de Junio de 2023.				**
-** Descripción:	Este programa realiza las iteraciones necesarias	**
-** 		para desplazar determinado número de discos de un pilar	**
-**		hacia otro siguiendo el algoritmo del enigma de las	**
-** 		Torres de Hanoi.					**
-**************************************************************************/
-/* Librerías estándar de trabajo. */
+/**************** Hanoi Towers recursive generator. **************
+ ** Source Code:	Hanoi.c					**
+ ** Author:		Gustavo Islas Gálvez.			**
+ ** Creation Date:	Saturday, December 30, 2023.		**
+ ** Purpose:		The purpose of this program is, through	**
+ **			recursive calls, to solve the enigma of **
+ **			the Towers of Hanoi, which basically	**
+ **			consists of moving a stack of disks	**
+ **			ordered from smallest to largest	**
+ **			starting from the top of a pole,	**
+ **			and moving them to two adjacent poles,	**
+ **			fulfilling as a fundamental restriction	**
+ **			that when the discs are moved, a smaller**
+ **			one always remains on top of a larger	**
+ **			one, that is, there cannot be larger	**
+ **			discs on smaller discs.			**
+ **			This program performs the necessary	**
+ **			iterations to move a certain number of	**
+ **			discs on a pillar towards another	**
+ **			following the algorithm of the enigma	**
+ **			of the Towers of Hanoi.			**
+*****************************************************************/
+//C Standard Libraries.
 #include <stdio.h>
 
-/* Constantes simbólicas numéricas de trabajo. */
-#define ZERO_V		0
-#define ONE_V		1
-#define TWO_V		2
-#define ONE_HUNDRED_V	100
+//Symbolic constants of minimum and maximum limits.
+#define V_MINUS_ONE	-1
+#define V_ONE		1
+#define V_ONE_HUNDRED	100.0
+#define V_TWO		2
+#define V_ZERO		0
 
-/* Constantes simbólicas tipo carácter de trabajo. */
-#define NULL_CHARACTER	'\0'
+//Work Symbolic Constants.
 #define FIRST_TOWER	'A'
 #define SECOND_TOWER	'B'
 #define THIRD_TOWER	'C'
 
-/* Máxima cantidad de platillos en los pilares de las Torres de Hanoi. */
-#define MAX_DISHES	28
+//Minimum and maximum number of dishes on the pillars of the Towers of Hanoi.
+#define V_MAX_DISHES	28
+#define V_MIN_DISHES	1
 
-/* Función 'potency' que devuelve un número elevado a
-   a determinada potencia */
-long potency(int int_x, int int_y)
+/*****************************************************************
+ ** Function:           static size_t szt_potency.		**
+ ** Explanation:	The purpose of this function is to	**
+ **			return a number raised to a given power,**
+ **			through recursive iterations, calling	**
+ **			the function itself and multiplying a	**
+ **			base 'x' by an exponent 'y' in a	**
+ **			cumulative manner.			**
+ ** Input Parms:        const size_t szt_x,			**
+ **			const size_t szt_y.			**
+ ** Output Parms:       None.                                   **
+ ** Result:		'potency' function that returns a number**
+ **			raised to at a certain power.		**
+ ****************************************************************/
+static size_t szt_potency(const size_t szt_x, const size_t szt_y)
 	{
-		long ret=ONE_V;
-
-		for (int i=ZERO_V; i<int_y; i++)
-			ret*=int_x;
-
-		return ret;
+		return (szt_y < V_ONE) ? V_ONE : szt_x * szt_potency(szt_x, szt_y + V_MINUS_ONE);
 	}
 
-
-/* Función 'getpause' que devuelve el carácter presionado como parte de
-   la pausa solicitada. */
-char getpause (const char *str_Message)
-    {
-        static char chr_Car=NULL_CHARACTER;
-
-	printf("%s", str_Message);
-	scanf("%*c%c", &chr_Car);
-
-        return (chr_Car);
-    }
-
-/* Función 'lng_HanoiTowers' para realizar y contabilizar las iteraciones
-   de los discos entre las torres. */
-long lng_HanoiTowers(const char n_ch_dishes,
-		 const char ch_first_tower,
-		 const char ch_second_tower,
-		 const char ch_third_tower,
-		 const long lng_n_maximum_cycles)
+/*****************************************************************
+ ** Function:		static size_t szt_HanoiTowers		**
+ **				(const size_t szt_num_dishes,	**
+ **				 const char chr_first_tower,	**
+ **				 const char chr_second_tower,	**
+ **				 const char chr_third_tower,	**
+ **				 const size_t 			**
+ **					szt_num_maximum_cycles).**
+ ** Explanation:	In this function, the recursive		**
+ **			iterations necessary to solve the	**
+ **			enigma of the Towers of Hanoi are	**
+ **			carried out, which consists of moving a	**
+ **			series of plates arranged from smallest	**
+ **			to largest starting from the top and	**
+ **			placed on a given pillar, towards two	**
+ **			underlying poles, in such a way that in	**
+ **			the last one they are equally ordered	**
+ **			as in their origin.			**
+ **			The fundamental rule to follow in this	**
+ **			algorithm is that as the plates are	**
+ **			moved, a smaller one cannot be below a	**
+ **			larger one, so the three pillars or	**
+ **			poles must validate these conditions.	**
+ ** Input Parms:	const size_t szt_num_dishes,		**
+ **			const char chr_first_tower,		**
+ **			const char chr_second_tower,		**
+ **			const char chr_third_tower,		**
+ **			const size_t szt_num_maximum_cycles.	**
+ ** Output Parms:	None.					**
+ ** Result:		Function 'szt_HanoiTowers' to perform	**
+ **			and account for iterations of the disks	**
+ **			between the towers.			**
+ ****************************************************************/
+static size_t szt_HanoiTowers(const size_t szt_num_dishes, const char chr_first_tower, const char chr_second_tower, const char chr_third_tower, const size_t szt_num_maximum_cycles)
 	{
-		/* El movimiento de los aros empieza con tres torres,
-		   numeradas de la primera (1ª) a la tercera (3ª)
-		   y se contabilizan sus ciclos. */
-		static long lng_n_loops_HanoiTowers=ZERO_V;
-		float flt_perc_advantage=ZERO_V;
+		/* ----------------------------------------------------	--
+		 * The movement of the rings begins with three rooks,	--
+		 * numbered from the first (1st) to the third (3rd) and	--
+		 * their cycles are counted.				--
+		 * ----------------------------------------------------	*/
+		static size_t szt_num_loops_HanoiTowers = V_ZERO;
+		double dbl_perc_advantage = V_ZERO;
 
-		/* Contabilizar el número de ciclos y su porcentaje que
-		   se llevan de los cálculos de las Torres de Hanoi. */
-		lng_n_loops_HanoiTowers++;
-		flt_perc_advantage = (	(float) lng_n_loops_HanoiTowers /
-					(float) (lng_n_maximum_cycles-ONE_V) )
-							  * ONE_HUNDRED_V;
+		/* ----------------------------------------------------	--
+		 * Count the number of cycles and their percentage that	--
+		 * they are taken from the calculations of the Towers	--
+		 * of Hanoi.						--
+		 * ---------------------------------------------------- */
+		szt_num_loops_HanoiTowers++;
+		dbl_perc_advantage = (	(double) szt_num_loops_HanoiTowers / (double) (szt_num_maximum_cycles + V_MINUS_ONE) ) * V_ONE_HUNDRED;
 
-		/* Se valida para las recursiones si el número de discos
-		   siempre es mayor de la unidad y menor
-		   al máximo de ciclos calculados. */
-		if (n_ch_dishes > ONE_V && lng_n_loops_HanoiTowers < lng_n_maximum_cycles)
+		/* ----------------------------------------------------	--
+		 * It is validated for recursions if the number of	--
+		 * disks is always greater than unity and less than to	--
+		 * the maximum calculated cycles.			--
+		 * ----------------------------------------------------	*/
+		if (szt_num_dishes > V_ONE && szt_num_loops_HanoiTowers < szt_num_maximum_cycles)
 			{
-				/* -----------------------------------------------------*/
-				/* Se invierten las astas de las que se van a extraer	*/
-				/* los aros, ahora es de la 1ª, la 3ª y la 2ª.		*/
-				/* -----------------------------------------------------*/
-				lng_HanoiTowers(n_ch_dishes-ONE_V,
-						ch_first_tower,
-						ch_third_tower,
-						ch_second_tower,
-						lng_n_maximum_cycles);
+				/* ------------------------------------------------------------	--
+				 * The antlers from which they are going to be extracted are	--
+				 * inverted the rings, now it is the 1st, the 3rd and the 2nd.	--
+				 * ------------------------------------------------------------	*/
+				szt_HanoiTowers(szt_num_dishes + V_MINUS_ONE, chr_first_tower, chr_third_tower, chr_second_tower, szt_num_maximum_cycles);
 
-				/* -----------------------------------------------------*/
-				/* Este mensaje se imprime mientras haya aros encima	*/
-				/* del asta actual a movilizarse.			*/
-				/* -----------------------------------------------------*/
-				printf("Movimiento: [%ld] de [%ld]. [%3.2f]%% realizado. Se traslada el aro [%2d] del asta [%c] al asta [%c]. \r",
-					lng_n_loops_HanoiTowers,
-					lng_n_maximum_cycles-ONE_V,
-					flt_perc_advantage,
-					n_ch_dishes,
-					ch_first_tower,
-					ch_third_tower);
+				/* ------------------------------------------------------------	--
+				 * This message prints while there are rings on it of the	--
+				 * current flagpole to be mobilized.				--
+				 * ------------------------------------------------------------	*/
+				printf("Motion: [%10.ld].\t[%5.2lf]%% advanced.\tRing # [%2.ld].\tFrom antler [%c] to [%c].\t\r", szt_num_loops_HanoiTowers, dbl_perc_advantage, szt_num_dishes, chr_first_tower, chr_third_tower);
 
-				/* -----------------------------------------------------*/
-				/* Se invierten las astas de las que se van a extraer	*/
-				/* los aros, ahora es de la 2ª, la 1ª y la 3ª.		*/
-				/* -----------------------------------------------------*/
-				lng_HanoiTowers(n_ch_dishes-ONE_V,
-						ch_second_tower,
-						ch_first_tower,
-						ch_third_tower,
-						lng_n_maximum_cycles);
+				/* ------------------------------------------------------------	--
+				 * The antlers from which they are going to be extracted are	--
+				 * inverted the rings, now it is the 2nd, the 1st and the 3rd.	--
+				 * ------------------------------------------------------------	*/
+				szt_HanoiTowers(szt_num_dishes + V_MINUS_ONE, chr_second_tower, chr_first_tower, chr_third_tower, szt_num_maximum_cycles);
 			}
 
-		/* Este mensaje se imprime si se llegó al último aro del
-		   bloque que se desea mover desde un asta específica. */
-		printf("Movimiento: [%ld] de [%ld]. [%3.2f]%% realizado. Se traslada el aro [%2d] del asta [%c] al asta [%c]. \r",
-			lng_n_loops_HanoiTowers,
-			lng_n_maximum_cycles-ONE_V,
-			flt_perc_advantage,
-			n_ch_dishes,
-			ch_first_tower,
-			ch_third_tower);
+		/* ------------------------------------------------------------	--
+		 * This message is printed if the last ring of the block that	--
+		 * you want to move from a specific pole.			--
+		 * ------------------------------------------------------------	*/
+		printf("Motion: [%10.ld].\t[%5.2lf]%% advanced.\tRing # [%2.ld].\tFrom antler [%c] to [%c]. \r", szt_num_loops_HanoiTowers, dbl_perc_advantage, szt_num_dishes, chr_first_tower, chr_third_tower);
 
-		return (lng_n_loops_HanoiTowers);
+		return (szt_num_loops_HanoiTowers);
 	}
 
 
-/* Programa Principal que desplaza los platillos entre las astas
-   que componen el algoritmo del enigma de las Torres de Hanoi. */
+/*****************************************************************
+ ** Function:		main.					**
+ ** Explanation:	In this main function, a series of discs**
+ **			stacked from largest to smallest on a	**
+ **			given shaft move towards two adjacent	**
+ **			shafts, in such a way that a disc of a	**
+ **			smaller size must necessarily be placed	**
+ **			on top of a disc of a larger size, thus,**
+ **			all the discs from the initial stack	**
+ **			must be arranged in the same way in the	**
+ **			final stack.				**
+ ** Input Parms:	None.					**
+ ** Output Parms:	None.					**
+ ** Result:		Main program that moves the plates	**
+ **			between the shafts that make up the	**
+ **			algorithm of the Towers of Hanoi enigma.**
+*****************************************************************/
 int main()
     {
-		/* Declaración de variables. */
-		char n_ch_dishes=ZERO_V;
-		long lng_n_loops_HanoiTowers=ZERO_V;
-		long lng_n_maximum_cycles_HanoiTowers=ZERO_V;
+		/* Declaration of numerical work variables. */
+		size_t szt_num_dishes = V_ZERO;
+		size_t szt_num_loops_HanoiTowers = V_ZERO;
+		size_t szt_num_maximum_cycles_HanoiTowers = V_ZERO;
 
-   		printf("****************************************************\n");
-		printf("**  Algoritmo para resolver las Torres de Hanoi.  **\n");
-   		printf("****************************************************\n");
-		printf("Número de aros a desplazar [%d-%d]: ", ONE_V, MAX_DISHES);
-   		scanf("%d%*c", &n_ch_dishes);
+   		printf("|---|----+---|----+---|----+---|----+\n");
+		printf("|     Towers of Hanoi Algorithm.    |\n");
+   		printf("|---|----+---|----+---|----+---|----+\n");
+		printf("Rings to move between [%2.d] and [%2.d]: ", V_MIN_DISHES, V_MAX_DISHES);
+   		scanf("%ld", &szt_num_dishes);
 
-		/* Se valida que el número de discos esté en un rango
-		   permisible y tolerable por el procesador. */
-		if (n_ch_dishes>=ONE_V && n_ch_dishes<=MAX_DISHES)
+		/* ------------------------------------------------------------	--
+		 * It is validated that the number of disks is in a range	--
+		 * allowable and tolerable by the processor.			--
+		 * ------------------------------------------------------------	*/
+		if (szt_num_dishes >= V_MIN_DISHES && szt_num_dishes <= V_MAX_DISHES)
 			{
-				/* Se determina el número máximo de ciclos del procesador
-				   para resolver las Torres de Hanoi con 'n' discos. */
-				lng_n_maximum_cycles_HanoiTowers=potency(TWO_V,n_ch_dishes); //(2^N)
+				/* ------------------------------------------------------------	--
+				 * The maximum number of processor cycles is determined to 	--
+				 * solve the Towers of Hanoi with 'n' discs.			--
+				 * ------------------------------------------------------------	*/
+				szt_num_maximum_cycles_HanoiTowers = szt_potency(V_TWO, szt_num_dishes); //(2^N)
 
-				/* Se avisa en pantalla la cantidad de movimientos que se
-				   requieren para trasladar los distintos discos. */
-				printf("Se necesitan [%ld] movimientos para resolver\n"
-					"el enigma de las Torres de Hanoi con [%d] discos.\n",
-					lng_n_maximum_cycles_HanoiTowers-ONE_V,
-					n_ch_dishes);
-				getpause("Presione la tecla ENTRAR para comenzar el programa...");
+				/* ------------------------------------------------------------	--
+				 * The number of movements that have been made is displayed on	--
+				 * the screen required to move the different disks.		--
+				 * ------------------------------------------------------------	*/
+				printf("\n[%10.ld] Necessary movements with [%2.ld] discs.\n", szt_num_maximum_cycles_HanoiTowers + V_MINUS_ONE, szt_num_dishes);
 
-				/* ---------------------------------------------------- */
-				/* Se llama a la función 'HanoiTowers' y se devuelve	*/
-				/* como resultado el número de ciclos para resolverlas. */
-				/* -----------------------------------------------------*/
-				lng_n_loops_HanoiTowers=lng_HanoiTowers(n_ch_dishes,
-									FIRST_TOWER,
-									SECOND_TOWER,
-									THIRD_TOWER,
-									lng_n_maximum_cycles_HanoiTowers);
+				/* ------------------------------------------------------------	--
+				 * The 'HanoiTowers' function is called and returns as a result	--
+				 * the number of cycles to solve them.				--
+				 * ------------------------------------------------------------	*/
+				szt_num_loops_HanoiTowers = szt_HanoiTowers(szt_num_dishes, FIRST_TOWER, SECOND_TOWER, THIRD_TOWER, szt_num_maximum_cycles_HanoiTowers);
 
-				/* Se visualiza en pantalla cuántos movimientos efectivos
-				   se realizaron con el número de discos especificado. */
-				printf("\n[%ld] movimientos realizados para resolver\n"
-					"el enigma de las Torres de Hanoi y\n"
-					"[%d] discos desplazados.\n",
-					lng_n_loops_HanoiTowers,
-					n_ch_dishes);
+				/* ------------------------------------------------------------	--
+				 * The screen displays how many effective movements were made	--
+				 * with the specified number of disks.				--
+				 * ------------------------------------------------------------	*/
+				printf("\n[%10.ld] Movements performed with [%2.ld] displaced discs.\n", szt_num_loops_HanoiTowers, szt_num_dishes);
 			}
 		else
-			/* El valor introducido está fuera del rango permitido.
-			   Mientras más discos, mayor es la probabilidad
-			   de que nunca termine el proceso del algoritmo. */
-			printf("El valor introducido [%d] está fuera del rango permitido.\n"
-				"Sólo se permiten desde [%d] hasta [%d] discos. \n",
-					 n_ch_dishes, ONE_V, MAX_DISHES);
+			/* ------------------------------------------------------------	--
+			 * The entered value is outside the allowed range.		--
+			 * The more disks, the greater the probability that the		--
+			 * algorithm process never ends.				--
+			 * ------------------------------------------------------------	*/
+			printf("Mistake! Value [%2.ld] is not in the range of [%2.d] and [%2.d].\n", szt_num_dishes, V_MIN_DISHES, V_MAX_DISHES);
 
-		/* Pausa para terminar el programa. */
-		getpause("\nEste programa ha terminado con éxito.\n"
-			"Presione la tecla ENTRAR para finalizarlo...");
-
-    	return(lng_n_maximum_cycles_HanoiTowers);
+    	return V_ZERO;
     }
