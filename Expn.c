28a87817221a4d933fec4f971f3929f715f6d8c9
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-08 16:34:50 -0600

    Euler exponential natural number program
    For this program that generates the Neperian Euler number raised to a certain power, some functions were refactored, output messages were modified and the terms it must execute were optimized in order to offer a good approximation.

--- Expn.c
+++ Expn.c
@@ -1,7 +1,7 @@
 /************* Euler exponential natural number 'e'. *************
  ** Source Code:        Expn.c					**
  ** Author:             Gustavo Islas Gálvez.			**
- ** Creation Date:      Thursday, August 31, 2023.		**
+ ** Creation Date:      Saturday, December 30, 2023.		**
  ** Purpose:		This program is in charge of raising	**
  **			Euler's natural number 'e' to a		**
  **			determined power 'x', and it is solved	**
@@ -11,93 +11,68 @@
 #include <stdio.h>
 
 //Numeric Symbolic Constants.
-#define V_NUM_TERMS	20
+#define	V_MINUS_ONE	-1
+#define V_NUM_TERMS	256
 #define V_ONE		1
 #define V_ZERO		0
 
 /*****************************************************************
- ** Function:           absolute.                               **
- ** Explanation:        Returns as a result the positive or     **
- **                     unsigned value of a certain value,      **
- **                     as long as it is subjected to a series  **
- **                     of conditions such as being greater     **
- **                     than zero, to be positive; be zero,     **
- **                     to be zero, and less than zero,         **
- **                     to become positive by sign              **
- **                     neutralization.                         **
- ** Input Parms:        double int_value.                       **
- ** Output Parms:       None.                                   **
- ** Result:             the base value (int_value) is           **
- **                     sign-neutralized.                       **
- ****************************************************************/
-double absolute(int int_value)
-	{
-		return  (int_value==V_ZERO) ? V_ZERO
-		: (int_value>V_ZERO) ? int_value : -int_value;
-	}
-
-/*****************************************************************
- ** Function:           factorial.                              **
+ ** Function:		static double dbl_factorial.		**
  ** Explanation:        Gets the value of the factorial of any  **
  **                     integer by recursively multiplying it by**
  **                     its descendant surrogate numbers.       **
- ** Input Parms:        integer int_num.                        **
+ ** Input Parms:        const size_t szt_num.			**
  ** Output Parms:       None.                                   **
  ** Result:             Factorial from 'i' to 'n'.              **
  ****************************************************************/
-double factorial(int int_num)
+static double dbl_factorial(const size_t szt_num)
 	{
-		return (int_num <= V_ZERO) ? V_ONE
-			: (double) int_num * factorial(int_num - V_ONE);
+		return	(szt_num < V_ONE) ? V_ONE : szt_num * dbl_factorial(szt_num + V_MINUS_ONE);
 	}
 
 /*****************************************************************
- ** Function:           potency.                                **
+ ** Function:		static double dbl_potency.		**
  ** Explanation:        Returns a base coefficient raised to the**
  **                     specified power recursively by means of **
  **                     successive multiplications or divisions.**
  **                                                             **
  **                     This function can also be programmed    **
  **                     recursively in the form:                **
- **                     (int_exp == V_ZERO) ? V_ONE : dbl_base  **
- **                      * potency(dbl_base, int_exp - V_ONE);  **
+ **                     (szt_exp == V_ZERO) ? V_ONE : dbl_base  **
+ **                      * dbl_potency(dbl_base,		**
+ **					szt_exp - V_ONE);	**
  **                                                             **
- ** Input Parms:        double dbl_base,                        **
- **                     integer int_exp.                        **
+ ** Input Parms:        const double dbl_base,			**
+ **                     const size_t szt_exp.			**
  ** Output Parms:       None.                                   **
  ** Result:             dbl_base raised to int_exp.             **
  ****************************************************************/
-double potency(double dbl_base, int int_exp)
+static double dbl_potency(const double dbl_base, const size_t szt_exp)
 	{
-		double dbl_ret=V_ONE;
-		int int_exp_pow=absolute(int_exp);
-
-		for (int int_i=V_ZERO; int_i<int_exp_pow; int_i++)
-			dbl_ret = (int_exp > V_ZERO) ? dbl_ret * dbl_base :
-				  (int_exp < V_ZERO) ? dbl_ret / dbl_base : V_ONE;
-
-		return dbl_ret;
+		return	(szt_exp > V_ZERO) ? dbl_base * dbl_potency(dbl_base, szt_exp + V_MINUS_ONE) :
+			(szt_exp == V_ZERO) ? V_ONE :
+			(szt_exp < V_ZERO) ? dbl_base / dbl_potency(dbl_base, szt_exp + V_ONE) : V_ONE;
 	}
 
 /*****************************************************************
- ** Function:           expn.					**
+ ** Function:           static double expn.			**
  ** Explanation:	Computes Euler's natural number 'e'	**
  **			through power series given a specified	**
  **			number of terms, in such a way that the	**
  **			series is obtained: (x^0 / 0!) +	**
  **			(x^1 / 1!) + (x^2 / 2!) + (x^3 / 3!)...	**
- ** Input Parms:        double dbl_x.				**
+ ** Input Parms:        const double dbl_x.			**
  ** Output Parms:       None.					**
  ** Result:             'e' raised to dbl_x.			**
  ****************************************************************/
-double expn(double dbl_x)
+static double expn(const double dbl_x)
 	{
-		double dbl_ret=V_ZERO;
+		double dbl_outcome = V_ZERO;
 
-		for (int int_i=V_ZERO; int_i<V_NUM_TERMS; int_i++)
-			dbl_ret+=potency(dbl_x, int_i)/factorial(int_i);
+		for (size_t szt_idx = V_ZERO; szt_idx < V_NUM_TERMS; szt_idx++)
+			dbl_outcome += dbl_potency(dbl_x, szt_idx) / dbl_factorial(szt_idx);
 
-		return dbl_ret;
+		return dbl_outcome;
 	}
 
 /*****************************************************************
@@ -115,20 +90,24 @@ double expn(double dbl_x)
  ****************************************************************/
 int main()
 	{
-		double dbl_x=V_ZERO;
+		double dbl_expn = V_ZERO;
+		double dbl_x = V_ZERO;
 
 		printf("+---|----+---|----+---|----+---|--\n");
-		printf("| Número neperiano 'e' de Euler. |\n");
+		printf("| Neperian Euler number. |\n");
 		printf("+---|----+---|----+---|----+---|--\n");
-		printf("Potencia  'x' : ");
+		printf("Exponent : ");
 		scanf("%lf", &dbl_x);
 
+		dbl_expn = expn(dbl_x);
+
 		printf("\n");
 		printf("+---|----+---|----+---|----+---|--\n");
-		printf("| Potencia  'x' : [%lf].\n", dbl_x);
-		printf("| Euler 'e'^'x' : [%lf].\n", expn(dbl_x));
+		printf("| Exponent\t: [%lf].\n", dbl_x);
+		printf("| Terms\t\t: [%d].\n", V_NUM_TERMS);
+		printf("| Result\t: [%lf].\n", dbl_expn);
 		printf("+---|----+---|----+---|----+---|--\n");
 		printf("\n");
 
-		return dbl_x;
+		return V_ZERO;
 	}
