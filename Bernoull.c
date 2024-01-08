6e7cc60a80309b303f38014172aad6b5e9e1cdcd
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-08 08:18:47 -0600

    Bernoulli Number Generator Program
    Some portions of the code of this program that generates the Bernoulli series numbers were refactored, some variable types were changed, and some comments in the program's internal documentation in this regard were modified.

--- Bernoull.c
+++ Bernoull.c
@@ -1,7 +1,7 @@
 /****************** Bernoulli Number Generator. ******************
  ** Source Code:	Bernoull.cc	               		**
  ** Author:		Gustavo Islas Gálvez.			**
- ** Creation Date:	Saturday, September 30, 2023.		**
+ ** Creation Date:	Saturday, December 30, 2023.		**
  ** Purpose:		This program generates the sequence of	**
  **			Bernoulli numbers attending to		**
  **			iterations and recursion.		**
@@ -17,76 +17,74 @@
 //C Standard Libraries.
 #include <stdio.h>
 
-//Working Macros
+//Working Macros.
 #define	DENOMINATOR(v_n, v_k)	((v_n) - (v_k) + V_ONE)
 
+//Limit constants for Bernoulli numbers.
+#define	V_UPPER_LIMIT_BERN	28
+#define	V_LOWER_LIMIT_BERN	0
+
 //Numeric Symbolic Constants.
-#define	V_LIM_MAX		26
-#define	V_LIM_MIN		0
 #define V_MINUS_ONE		-1
 #define V_ONE			1
 #define V_ZERO			0
 
 /*****************************************************************
- ** Function:		factorial.				**
+ ** Function:		static size_t factorial.		**
  ** Explanation:	Gets the value of the factorial of any	**
  **			integer by iterativity multiplying	**
  **			it by its descendant surrogate numbers.	**
- ** Input Parms:	int int_num.				**
+ ** Input Parms:	size_t szt_num.				**
  ** Output Parms:	None.					**
  ** Result:		Factorial from 'i' to 'n'.		**
 *****************************************************************/
-long factorial(int int_num)
+static size_t factorial(size_t szt_num)
 	{
-		long lng_fact=V_ONE;
-
-		for (int int_idx=V_ONE; int_idx<=int_num; int_idx++)
-			lng_fact*=int_idx;
-
-		return lng_fact;
+		return (szt_num < V_ONE) ? V_ONE : szt_num * factorial(szt_num + V_MINUS_ONE);
 	}
 
 /*****************************************************************
- ** Function:		combinations.				**
+ ** Function:		static size_t combinations.		**
  ** Explanation:	Gets the total number of combinations	**
  **			arising from the mathematical formula:	**
  **			nCr = n! / [(n - r)! * r!].		**
  **			In such a way that it also resorts to	**
  **			the previously predefined and		**
  **			pre-established factorial formula.	**
- ** Input Parms:	int int_num.				**
+ ** Input Parms:	size_t szt_num,				**
+ **			size_t szt_r.				**
  ** Output Parms:	None.					**
  ** Result:		nCr = n! / [(n - r)! * r!].		**
 *****************************************************************/
-long combinations(int int_num, int int_r)
+static size_t combinations(size_t szt_num, size_t szt_r)
 	{
-		return factorial(int_num) / (factorial(int_num - int_r) * factorial(int_r));
+		return factorial(szt_num) / (factorial(szt_num - szt_r) * factorial(szt_r));
 	}
 
 /*****************************************************************
- ** Function:		Bernoulli.				**
+ ** Function:		static double Bernoulli.		**
  ** Explanation:	Returns as the result itself the	**
  **			Bernoulli number requested by the	**
  **			mathematical formula:			**
  **			-Sum(from j=0 to m):                    **
  **                             mCj * (B[j] / (m + 1 - j)).	**
- ** Input Parms:	int int_num.				**
+ ** Input Parms:	size_t szt_num.				**
  ** Output Parms:	None.					**
  ** Result:		Repetitive loops & recursive iterations.**
  **			-Sum(from j=0 to m):			**
  **				(m!/((m!-j!)*j!) * (B[j] /	**
  **					(m + 1 - j)).		**
 *****************************************************************/
-double Bernoulli(int int_num)
+static double Bernoulli(size_t szt_num)
 	{
-		double dbl_Bernoulli=V_ZERO;
+		double dbl_Bernoulli = V_ZERO;
 
-		if (int_num>V_ZERO)
-			for (int int_idx=V_ZERO; int_idx<=(int_num+V_MINUS_ONE); int_idx++)
-				dbl_Bernoulli += (double) (combinations(int_num, int_idx)
-						* Bernoulli(int_idx)) / DENOMINATOR(int_num, int_idx);
+		if (szt_num > V_ZERO)
+			for (size_t szt_idx = V_ZERO; szt_idx <= (szt_num + V_MINUS_ONE); szt_idx++)
+				dbl_Bernoulli += (double) (combinations(szt_num, szt_idx)
+						* Bernoulli(szt_idx)) / DENOMINATOR(szt_num, szt_idx);
 		else
-			dbl_Bernoulli=V_MINUS_ONE;
+			dbl_Bernoulli = V_MINUS_ONE;
 
 		return -dbl_Bernoulli;
 	}
@@ -113,20 +111,20 @@ double Bernoulli(int int_num)
 *****************************************************************/
 int main()
 	{
-		int int_nums=V_ZERO;
+		size_t szt_nums = V_ZERO;
 
-		printf("Bernoulli numbers between [%d] and [%d] : ", V_LIM_MIN, V_LIM_MAX);
-		scanf("%d", &int_nums);
+		printf("Bernoulli numbers between [%d] and [%d] : ", V_LOWER_LIMIT_BERN, V_UPPER_LIMIT_BERN);
+		scanf("%ld", &szt_nums);
 
-		if (int_nums>=V_LIM_MIN && int_nums<=V_LIM_MAX)
+		if (szt_nums >= V_LOWER_LIMIT_BERN && szt_nums <= V_UPPER_LIMIT_BERN)
 			{
-				printf("[%d] Bernoulli numbers:\n", int_nums);
+				printf("[%ld] Bernoulli numbers:\n", szt_nums);
 
-				for (int int_idx=V_ZERO; int_idx<=int_nums; int_idx++)
-					printf("B:[%2.d] = [%12.6lf].\n", int_idx, Bernoulli(int_idx));
+				for (size_t szt_idx = V_ZERO; szt_idx <= szt_nums; szt_idx++)
+					printf("B:[%2.ld] = [%12.6lf].\n", szt_idx, Bernoulli(szt_idx));
 			}
 		else
-			printf("Error! The number [%d] is out of range from [%d] to [%d].\n", int_nums, V_LIM_MIN, V_LIM_MAX);
+			printf("Error! The number [%ld] is out of range from [%d] to [%d].\n", szt_nums, V_LOWER_LIMIT_BERN, V_UPPER_LIMIT_BERN);
 
 		return V_ZERO;
 	}
