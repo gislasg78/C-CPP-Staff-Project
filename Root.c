a6ea4d31ac89327816d8206dcfbca468348087cf
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-01-08 19:48:14 -0600

    Kth root number program
    This program is responsible for obtaining a kth root of a given base, and some of its intrinsic functions were refactored, such as those responsible for powers and absolute values. Likewise, some output messages of the results generated on the screen were changed.

--- Root.c
+++ Root.c
@@ -1,7 +1,7 @@
 /***************** K-th root by Newton's Method. *****************
  ** Source Code:	Root.c					**
  ** Author:		Gustavo Islas Gálvez.			**
- ** Creation Date:	Thursday, August 31, 2023.          	**
+ ** Creation Date:	Saturday, December 30, 2023.          	**
  ** Purpose:		This program obtains the k-th root of a	**
  **			given numerical coefficient by Newton's	**
  **			method through iterations of successive	**
@@ -12,8 +12,9 @@
 #include <stdio.h>
 
 //Numeric Symbolic Constants.
+#define	V_MINUS_ONE	-1
 #define V_ONE		1
-#define V_PR_TOL	0.000001;	//Base Precision tolerance.
+#define V_PR_TOL	0.000001	//Base Precision tolerance.
 #define V_ZERO		0
 
 /*****************************************************************
@@ -31,7 +32,7 @@ struct strct_root_drvtv
 	} sd = {V_ZERO};
 
 /*****************************************************************
- ** Function:           absolute.                               **
+ ** Function:           static double absolute.			**
  ** Explanation:        Returns as a result the positive or     **
  **                     unsigned value of a certain value,      **
  **                     as long as it is subjected to a series  **
@@ -40,56 +41,53 @@ struct strct_root_drvtv
  **                     to be zero, and less than zero,         **
  **                     to become positive by sign              **
  **                     neutralization.                         **
- ** Input Parms:        double dbl_value.                       **
+ ** Input Parms:        const double dbl_value.			**
  ** Output Parms:       None.                                   **
- ** Result:             the base value (int_value) is           **
+ ** Result:             the base value (dbl_value) is           **
  **                     sign-neutralized.                       **
  ****************************************************************/
-double absolute(double dbl_value)
+static double absolute(const double dbl_value)
 	{
-		return  (dbl_value==V_ZERO) ? V_ZERO
-		: (dbl_value>V_ZERO) ? dbl_value : -dbl_value;
+		return  (dbl_value > V_ZERO) ? dbl_value :
+			(dbl_value == V_ZERO) ? V_ZERO :
+			(dbl_value < V_ZERO) ? -dbl_value : V_ZERO;
 	}
 
 /*****************************************************************
- ** Function:           potency.                                **
+ ** Function:           static double dbl_potency.              **
  ** Explanation:        Returns a base coefficient raised to the**
  **                     specified power recursively by means of **
  **                     successive multiplications or divisions.**
  **                                                             **
  **                     This function can also be programmed    **
  **                     recursively in the form:                **
- **                     (int_exp == V_ZERO) ? V_ONE : dbl_base  **
- **                      * potency(dbl_base, int_exp - V_ONE);  **
  **                                                             **
- ** Input Parms:        double dbl_base,                        **
- **                     integer int_exp.                        **
+ **                     (szt_exp == V_ZERO) ? V_ONE : dbl_base  **
+ **                      * dbl_potency                          **
+ **                             (dbl_base, szt_exp - V_ONE);    **
+ **                                                             **
+ ** Input Parms:        const double dbl_base,                  **
+ **                     const size_t szt_exp.                   **
  ** Output Parms:       None.                                   **
- ** Result:             dbl_base raised to int_exp.             **
- ****************************************************************/
-double potency(double dbl_base, int int_exp)
-	{
-		double dbl_ret=V_ONE;
-		int int_exp_pow=absolute(int_exp);
-
-		for (int int_i=V_ZERO; int_i<int_exp_pow; int_i++)
-			{
-		        	dbl_ret = (int_exp > V_ZERO) ? dbl_ret * dbl_base :
-		                	  (int_exp < V_ZERO) ? dbl_ret / dbl_base : V_ONE;
-			}
-
-		return dbl_ret;
+ ** Result:             dbl_base raised to szt_exp.             **
+*****************************************************************/
+static double dbl_potency(const double dbl_base, const size_t szt_exp)
+        {
+                return  (szt_exp > V_ZERO) ? dbl_base * dbl_potency(dbl_base, szt_exp + V_MINUS_ONE) :
+                        (szt_exp == V_ZERO) ? V_ONE :
+                        (szt_exp < V_ZERO) ? dbl_base / dbl_potency(dbl_base, szt_exp + V_ONE) : V_ONE;
         }
 
 /*****************************************************************
- ** Function:		root_derivative.			**
+ ** Function:		static struct strct_root_drvtv		**
+ **				root_derivative.		**
  ** Explanation:	Returns all the previous results needed	**
  **			for the series of divisions that make up**
  **			the Newton's method series to get the	**
  **			'root' of a specified coefficient.	**
- ** Input Parms:	double dbl_initial_x,			**
- **			double dbl_base,			**
- **			integer int_exp.			**
+ ** Input Parms:	const double dbl_initial_x,		**
+ **			const double dbl_base,			**
+ **			const size_t szt_exp.			**
  ** Output Parms:	None.					**
  ** Result:		The numerator "f(x)", the denominator	**
  **			"f'(x)", the quotient and the remainder	**
@@ -122,10 +120,7 @@ double potency(double dbl_base, int int_exp)
  **			approximation, repeating the process	**
  **			until the desired precision is reached.	**
  ****************************************************************/
-struct strct_root_drvtv root_derivative(
-					double dbl_initial_x,
-					double dbl_base,
-					int int_exp)
+static struct strct_root_drvtv root_derivative(const double dbl_initial_x, const double dbl_base, size_t szt_exp)
 	{
 		/*------------------------------------------------
 		 ** Results obtained from the series of		--
@@ -143,14 +138,14 @@ struct strct_root_drvtv root_derivative(
 		 ** The Function f(x) = [x ^ k] is obtained for	--
 		 ** the numerator.				--
 		 **---------------------------------------------*/
-		sd.dbl_numerator_fx = potency(dbl_initial_x, int_exp) - dbl_base;
+		sd.dbl_numerator_fx = dbl_potency(dbl_initial_x, szt_exp) - dbl_base;
 
 		/*------------------------------------------------
 		 ** The derivative of the Function f(x) is	--
 		 ** obtained for the denominator as f'(x).	--
 		 ** Said derivative: f'(x) = [k * x ^ (k - 1)]. --
 		 **---------------------------------------------*/
-		sd.dbl_denominator_fpx = int_exp * potency(dbl_initial_x, int_exp - V_ONE);
+		sd.dbl_denominator_fpx = szt_exp * dbl_potency(dbl_initial_x, szt_exp + V_MINUS_ONE);
 
 		/*------------------------------------------------
 		 ** The quotient is obtained by dividing:	--
@@ -176,17 +171,17 @@ struct strct_root_drvtv root_derivative(
 	}
 
 /*****************************************************************
- ** Function:		root.					**
+ ** Function:		static double root.			**
  ** Explanation:	Returns the kth root of a given base	**
  **			value through a series of iterations of	**
  **			Newton's method.			**
- ** Input Parms:	double dbl_base,			**
- **			integer int_exp.			**
+ ** Input Parms:	const double dbl_base,			**
+ **			const size_t szt_exp.			**
  ** Output Parms:	None.					**
- ** Result:		the kth root (int_exp) of the given	**
+ ** Result:		The kth root (int_exp) of the given	**
  **			base (dbl_base).			**
  ****************************************************************/
-double root(double dbl_base, int int_exp)
+static double root(const double dbl_base, const size_t szt_exp)
 	{
 		//Structure with the results of the derivative.
 		struct strct_root_drvtv sdv = {V_ZERO};
@@ -202,7 +197,7 @@ double root(double dbl_base, int int_exp)
 				 ** structure with all the result values of the --
 				 ** applied derivative.				--
 				 *----------------------------------------------*/
-				sdv = root_derivative(dbl_initial_x, dbl_base, int_exp);
+				sdv = root_derivative(dbl_initial_x, dbl_base, szt_exp);
 
 				/*------------------------------------------------
 				 ** The initial value of 'x' now receives the	--
@@ -231,30 +226,30 @@ double root(double dbl_base, int int_exp)
  ****************************************************************/
 int main()
 	{
-		double dbl_base=V_ZERO;
-		double dbl_root=V_ZERO;
-		int int_exp=V_ZERO;
+		double dbl_base = V_ZERO;
+		double dbl_root = V_ZERO;
+		size_t szt_exp = V_ZERO;
 
-		printf("+---|----+---|----+---|----+---|----+---|----+\n");
-		printf("|  Raíces k-ésimas por el Método de Newton.  |\n");
-		printf("+---|----+---|----+---|----+---|----+---|----+\n");
+		printf("+---|----+---|----+---|----+---|----+\n");
+		printf("|   Kth roots by Newton's Method.   |\n");
+		printf("+---|----+---|----+---|----+---|----+\n");
 		printf("Base: ");
 		scanf("%lf", &dbl_base);
-		printf("Raíz: ");
-		scanf("%d", &int_exp);
+		printf("Root: ");
+		scanf("%ld", &szt_exp);
 
-		dbl_root=root(dbl_base, int_exp);
+		dbl_root = root(dbl_base, szt_exp);
 
 		printf("\n");
-		printf("+---|----+---|----+---|----+---|----+---|----+-\n");
-		printf("| Resultados obtenidos para una raíz k-ésima. |\n");
-		printf("+---|----+---|----+---|----+---|----+---|----+.\n");
-		printf("| Base      : [%lf].\n", dbl_base);
-		printf("| Raíz      : [%d].\n", int_exp);
-		printf("+---|----+---|----+---|----+---|----+---|----+\n");
-		printf("| Resultado : [%lf].\n", dbl_root);
-		printf("+---|----+---|----+---|----+---|----+---|----+\n");
+		printf("+---|----+---|----+---|----+---|----+\n");
+		printf("| Results obtained for a kth root.  |\n");
+		printf("+---|----+---|----+---|----+---|----+\n");
+		printf("| Base\t: [%lf].\n", dbl_base);
+		printf("| Root\t: [%ld].\n", szt_exp);
+		printf("+---|----+---|----+---|----+---|----+\n");
+		printf("| Result: [%lf].\n", dbl_root);
+		printf("+---|----+---|----+---|----+---|----+\n");
 		printf("\n");
 
-		return int_exp;
+		return V_ZERO;
 	}
