226f8c9f0ac87beee7263f657eaf0d9e7a21d77b
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-03-10 19:16:33 -0600

    Mathematical Functions General Project
    The purpose of this program is to show the dynamic creation of a rectangular matrix given a certain number of rows and for each of the rows, a certain number of columns, with the purpose of storing values given by the user in said assigned areas of memory in execution time.

---
+++ MemAsig.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#define	V_ZERO	0
+
+int main()
+	{
+		int items = V_ZERO, rows = V_ZERO;
+		int **matrix = NULL, *colsxrow = NULL;;
+
+		std::cout << "Creating a dynamic rectangular array." << std::endl;
+		std::cout << "Rows: ";
+		std::cin >> rows;
+
+		std::cout << std::endl << "Memory assigning spaces..." << std::endl;
+
+		matrix = new int* [rows];
+		colsxrow = new int [rows];
+
+		for (int row = V_ZERO; row < rows; row++)
+			{
+				std::cout << "Row # " << row + 1 << " of " << rows << "." << std::endl;
+				std::cout << "Columns for this row: ";
+
+				std::cin >> colsxrow[row];
+
+				matrix[row] = new int[colsxrow[row]];
+			}
+
+		std::cout << std::endl << "Capturing values..." << std::endl;
+		for (int row = V_ZERO; row < rows; row++)
+			for (int col = V_ZERO; col < colsxrow[row]; col++)
+				{
+					std::cout << "Enter value #" << items++ << ": ("<< row+1 << ", "<< col+1 << "): ";
+					std::cin >> *(*(matrix+row)+col);
+				}
+		std::cout << "[" << items << "] Captured input values." << std::endl;
+
+		std::cout << std::endl << "Displaying values..." << std::endl;
+		for (int row = V_ZERO, items = V_ZERO; row < rows; row++)
+			{
+				for (int col = V_ZERO; col < colsxrow[row]; col++, items++)
+					std::cout << "(" << row+1 << ", " << col+1 << "):\t"<< "[" << *(*(matrix+row)+col) << "].\t";
+
+				std::cout << std::endl;
+			}
+		std::cout << "[" << items << "] Generated output results." << std::endl;
+
+		std::cout << std::endl << "Deleting values..." << std::endl;
+		for (int row = V_ZERO; row < rows; row++)
+			delete matrix[row];
+
+		delete colsxrow;
+		delete matrix;
+
+		return V_ZERO;
+	}
