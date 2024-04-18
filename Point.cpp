727d1892f41b9c09fc53575c6eb38ada467f2f4f
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-04-17 18:01:26 -0600

    Mathematical Functions General Project
    Small modifications were made to this program to incorporate a virtual method that prints the values of the attributes of a 2D point and that are reflected in the inheritance that the 3D point receives from it, and exemplifies the use of template classes.

--- Point.cpp
+++ Point.cpp
@@ -11,7 +11,7 @@ class Point2D
 
 		protected:
 			static int c;
-			T x=V_ZERO, y=V_ZERO;
+			T x = V_ZERO, y = V_ZERO;
 
 		public:
 			Point2D (T x, T y) : x(x), y(y) {this->c++;};
@@ -19,8 +19,14 @@ class Point2D
 			T getX() const {return this->x;};
 			T getY() const {return this->y;};
 
-			void setX(const T x) {this->x=x;};
-			void setY(const T y) {this->y=y;};
+			void setX(const T x) {this->x = x;};
+			void setY(const T y) {this->y = y;};
+
+			virtual void print()
+				{
+					std::cout << "2D Point #" << this->c << ": (" << this->x << ", " << this->y << ")" << std::endl;
+				};
+
 	};
 
 template <typename T>
@@ -33,20 +39,27 @@ class Point3D : public Point2D<T>
 			template <typename U>
 			friend std::ostream &operator<< (std::ostream &os, const Point3D<U> &p3D);
 
-			T z=V_ZERO;
+			T z = V_ZERO;
 
 		public:
-			Point3D(T x, T y, T z) : Point2D<T>(x, y) {this->z=z;};
+			Point3D(T x, T y, T z) : Point2D<T>(x, y), z(z) {};
 
 			T getZ() const {return this->z;};
 
-			void setZ(const T z) {this->z=z;};
+			void setZ(const T z) {this->z = z;};
+
+			void print()
+				{
+					Point2D<T>::print();
+					std::cout << "3D Point #" << this->c << ": (" << this->z << ")" << std::endl;
+				};
 	};
+
 
 template <typename U>
 inline std::ostream &operator<< (std::ostream &os, const Point2D<U> &p2D)
 	{
-		os << "2D Point #" << p2D.c << ": (" << p2D.x << "," << p2D.y << ")" << std::endl;
+		os << "2D Point #" << p2D.c << ": (" << p2D.x << ", " << p2D.y << ")" << std::endl;
 
 		return os;
 	};
@@ -54,15 +67,16 @@ inline std::ostream &operator<< (std::ostream &os, const Point2D<U> &p2D)
 template <typename U>
 inline std::ostream &operator<< (std::ostream &os, const Point3D<U> &p3D)
 	{
-		os << "3D Point #" << p3D.c << ": (" << p3D.getX() << "," << p3D.getY() << "," << p3D.getZ() << ")" << std::endl;
+		os << "3D Point #" << p3D.c << ": (" << p3D.getX() << ", " << p3D.getY() << ", " << p3D.getZ() << ")" << std::endl;
 
 		return os;
 	};
 
+
 int main()
 	{
-		int int_2D_x=V_ZERO, int_2D_y=V_ZERO;
-		int int_3D_x=V_ZERO, int_3D_y=V_ZERO, int_3D_z=V_ZERO;
+		int int_2D_x = V_ZERO, int_2D_y = V_ZERO;
+		int int_3D_x = V_ZERO, int_3D_y = V_ZERO, int_3D_z = V_ZERO;
 
 		std::cout << "Coordinates of two points in the 2D Cartesian plane & 3D plane." << std::endl;
 
@@ -88,24 +102,29 @@ int main()
 
 		std::cout << "Generating an instance of a class template of type 2D Point." << std::endl;
 		Point2D<int> p2D = Point2D<int>(int_2D_x, int_2D_y);
+		p2D.print();
 		std::cout << p2D << std::endl;
 
 		std::cout << "Generating an instance of a class template of type 3D Point." << std::endl;
 		Point3D<int> p3D = Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
+		p3D.print();
 		std::cout << p3D << std::endl;
 
 		std::cout << "Change of 'x' & 'y' coordinates of the 2D Point." << std::endl;
 		p2D.setX(int_3D_x);
 		p2D.setY(int_3D_y);
+		p2D.print();
 		std::cout << p2D << std::endl;
 
 		std::cout << "Change of 'x' & 'y' coordinates of the 3D Point." << std::endl;
 		p3D.setX(int_2D_x);
 		p3D.setY(int_2D_y);
+		p3D.print();
 		std::cout << p3D << std::endl;
 
 		std::cout << "Polymorphism by assigning a 3D Point to a 2D Point type." << std::endl;
 		Point2D<int> p2x3D = Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
+		p2x3D.print();
 		std::cout << p2x3D << std::endl;
 
 		return V_ZERO;
