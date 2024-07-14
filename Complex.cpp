2ccef7fd77d5b9a74f65834dca58d70f3854220e
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-07-14 13:06:12 -0600

    Mathematical Functions General Project
    This program receives a string of characters with the typical format of a complex number and extracts the corresponding values ​​converted to decimal numerics for further processing. Includes overloading of the std::cin class and inheritance with exceptions.

---
+++ Complex.cpp
@@ -0,0 +1,99 @@
+#include <exception>
+#include <iostream>
+#include <sstream>
+
+#define V_MINUS_TWO -2
+#define V_ONE       1
+#define V_PLUS_SIGN 0x2b
+#define V_ZERO      0
+
+
+//Class to handle custom exceptions.
+class ComplexNumberFormatErrorException : public std::exception
+	{
+		public:
+			const char* what() const noexcept override
+				{
+					return "Damn! Invalid complex number format.";
+				}
+	};
+
+
+//Definition of the 'Complex' class.
+class Complex
+	{
+		public:
+			double dbl_real = V_ZERO;
+			double dbl_imaginary = V_ZERO;
+
+			//Default constructor.
+			Complex() : dbl_real(V_ZERO), dbl_imaginary(V_ZERO) {}
+
+			//Constructor with parameters.
+			Complex(double dbl_real, double dbl_imaginary) : dbl_real(dbl_real), dbl_imaginary(dbl_imaginary) {}
+
+			//Overloading of the '>>' operator.
+			friend std::istream& operator>>(std::istream& console_input, Complex& complex);
+	};
+
+
+//Defining the '>>' operator overload for std::cin.
+std::istream& operator>> (std::istream& input, Complex& complex)
+	{
+		std::string inputStr;
+		input >> inputStr;
+
+		//Find the position of the '+' symbol.
+		size_t plusPos = inputStr.find(V_PLUS_SIGN);
+
+		if (plusPos == std::string::npos)
+			{
+				//Handle error if format is not correct.
+				input.setstate(std::ios::failbit);
+				throw ComplexNumberFormatErrorException();
+			}
+		else
+			{
+				//Extract the real part and the imaginary part of the complex number from the character string as decimals.
+				try
+					{
+						complex.dbl_real = std::stod(inputStr.substr(V_ZERO, plusPos));
+						complex.dbl_imaginary = std::stod(inputStr.substr(plusPos + V_ONE, inputStr.length() - plusPos + V_MINUS_TWO)); //-2 to remove 'i'.
+					}
+				catch (std::exception& ex)
+					{
+						std::cerr << '\x5b' << ex.what() << '\x5d' << std::endl;
+					}
+			}
+
+		return input;
+	}
+
+
+int main()
+	{
+		Complex c;
+		std::cout << "Enter a complex number of the form: 'a+bi': ";
+
+		//Testing and obtaining the complex number of the form: 'a+bi'.
+		try
+			{
+				std::cin >> c;
+			}
+		catch (std::exception& ex)
+			{
+				std::cerr << '\x5b' << ex.what() << '\x5d' << std::endl;
+			}
+
+		//Sets flag on input as invalid complex number format.
+		if (std::cin.fail())
+			{
+				std::cerr << "Mistake! Incorrect input complex number format." << std::endl;
+			}
+		else
+			{
+				std::cout << "Complex number entered: [" << c.dbl_real << "] + [" << c.dbl_imaginary << "]i." << std::endl;
+			}
+
+		return V_ZERO;
+	}
