a4f22d4ae3f5e84fc484647c2be378ebc5ab993e
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-10-08 21:57:25 -0600

    Mathematical Functions General Project
    This program was segmented into more functions to give greater readability to the code that allows multiplying two matrices of variable size in their rows and for each of their rows a certain number of columns, in such a way that the resulting product matrix is ​​a weighted product of the rows and columns of each generated matrix.

--- Matprod.c
+++ Matprod.c
@@ -4,12 +4,51 @@
 #define	V_ONE	1
 #define	V_ZERO	0
 
+int int_capture_matrix(int **int_matrix, const int int_rows, const int int_cols)
+	{
+		int int_captured_items = V_ZERO;
+
+		if (int_matrix)
+			{
+				printf("Capture of the created and assigned matrix...\n");
+
+				for (int int_row = V_ZERO; int_row < int_rows; int_row++)
+					{
+						if (int_matrix[int_row])
+							{
+								for (int int_col = V_ZERO; int_col < int_cols; int_col++)
+									{
+										int_captured_items++;
+										printf("Enter a value #[%d]: (Row: [%d] of [%d], Column: [%d] of [%d]) : ", int_captured_items, int_row + V_ONE, int_rows, int_col + V_ONE, int_cols);
+										scanf("%d", &int_matrix[int_row][int_col]);
+									}
+
+								printf("\n");
+							}
+						else
+							{
+								perror("There is no memory space allocated to hold a row of the matrix...");
+								exit(EXIT_FAILURE);
+							}
+					}
+			}
+		else
+			{
+				perror("Empty array or not generated correctly to be captured...");
+				exit(EXIT_FAILURE);
+			}
+
+		printf("[%d] Captured output elements...\n", int_captured_items);
+
+		return int_captured_items;
+	}
+
+
 int **int_create_matrix(const int int_rows, const int int_cols)
 	{
 		int int_captured_items = V_ZERO;
 		int **int_matrix = NULL;
 
-		printf("\n");
 		printf("Direct creation and initialization of an array...\n");
 
 		if ((int_matrix = (int **) calloc(int_rows, sizeof(int *))))
@@ -18,12 +57,7 @@ int **int_create_matrix(const int int_rows, const int int_cols)
 					{
 						if ((int_matrix[int_row] = (int *) calloc(int_cols, sizeof(int))))
 							{
-								for (int int_col = V_ZERO; int_col < int_cols; int_col++)
-									{
-										int_captured_items++;
-										printf("Enter a value #[%d]: (Row: [%d] of [%d], Column: [%d] of [%d]) : ", int_captured_items, int_row + V_ONE, int_rows, int_col + V_ONE, int_cols);
-										scanf("%d", &int_matrix[int_row][int_col]);
-									}
+								int_captured_items++;
 							}
 						else
 							{
@@ -38,8 +72,7 @@ int **int_create_matrix(const int int_rows, const int int_cols)
 				exit(EXIT_FAILURE);
 			}
 
-		printf("\n");
-		printf("[%d] Captured output elements...\n", int_captured_items);
+		printf("[%d] Output results generated...\n", int_captured_items);
 
 		return int_matrix;
 	}
@@ -50,7 +83,6 @@ int int_deallocate_matrix(int **int_matrix, const int int_rows)
 
 		if (int_matrix)
 			{
-				printf("\n");
 				printf("Deallocating rows from a dynamically created array...\n");
 
 				for (int int_row = V_ZERO; int_row < int_rows; int_row++)
@@ -77,10 +109,44 @@ int int_deallocate_matrix(int **int_matrix, const int int_rows)
 				exit(EXIT_FAILURE);
 			}
 
-		printf("\n");
 		printf("[%d] Freed rows of memory from the allocated array...\n", int_deallocated_items);
 
 		return int_deallocated_items;
+	}
+
+int **int_matrix_product(int **int_f_matrix, int **int_s_matrix, const int int_f_rows, const int int_f_cols, const int int_s_rows, const int int_s_cols, int *int_max_rows, int *int_max_cols)
+	{
+		int **int_matrix_prod = NULL;
+
+		*int_max_rows = int_f_rows;
+		*int_max_cols = int_s_cols;
+
+		printf("Creation and initialization of the matrix that will serve as the product of the two matrices...\n");
+
+		if (int_matrix_prod = int_create_matrix(*int_max_rows, *int_max_cols))
+			{
+				printf("The product matrix element is obtained by multiplying each element of the matrix row by each element of the matrix column and summing them...\n");
+
+				for (int int_f_row = V_ZERO; int_f_row < int_f_rows; int_f_row++)
+					{
+						for (int int_s_col = V_ZERO; int_s_col < int_s_cols; int_s_col++)
+							{
+								for (int int_f_col = V_ZERO; int_f_col < int_f_cols; int_f_col++)
+									{
+										int_matrix_prod[int_f_row][int_s_col] += int_f_matrix[int_f_row][int_f_col] * int_s_matrix[int_f_col][int_s_col];
+										printf("i=[%d],j=[%d],k=[%d]:\t{(i=[%d], k=[%d])=[%d], (k=[%d],j=[%d]) = [%d]} = [%d]:\t[%d].\n", int_f_row, int_s_col, int_f_col, int_f_row, int_f_col, int_f_matrix[int_f_row][int_f_col], int_f_col, int_s_col, int_s_matrix[int_f_col][int_s_col], int_f_matrix[int_f_row][int_f_col] * int_s_matrix[int_f_col][int_s_col], int_matrix_prod[int_f_row][int_s_col]);
+									}
+
+								printf("\n");
+							}
+
+						printf("\n");
+					}
+			}
+		else
+			perror("There is not enough space in memory to allocate a matrix product array...");
+
+		return int_matrix_prod;
 	}
 
 int int_view_matrix(int **int_matrix, const int int_rows, const int int_cols)
@@ -89,7 +155,6 @@ int int_view_matrix(int **int_matrix, const int int_rows, const int int_cols)
 
 		if (int_matrix)
 			{
-				printf("\n");
 				printf("Visualization of the captured matrix...\n");
 
 				for (int int_row = V_ZERO; int_row < int_rows; int_row++)
@@ -123,44 +188,6 @@ int int_view_matrix(int **int_matrix, const int int_rows, const int int_cols)
 	}
 
 
-int **int_matrix_product(int **int_f_matrix, int **int_s_matrix, const int int_f_rows, const int int_f_cols, const int int_s_rows, const int int_s_cols, int *int_max_rows, int *int_max_cols)
-	{
-		int **int_matrix_prod = NULL;
-
-		*int_max_rows = (int_f_rows > int_s_rows) ? int_f_rows : int_s_rows;
-		*int_max_cols = (int_f_cols > int_s_cols) ? int_f_cols : int_s_cols;
-
-		printf("\n");
-		printf("Creation and initialization of the matrix that will serve as the product of the two matrices...");
-
-		if (int_matrix_prod = int_create_matrix(*int_max_rows, *int_max_cols))
-			{
-				printf("\n");
-				printf("The product matrix element is obtained by multiplying each element of the matrix row by each element of the matrix column and summing them...\n");
-
-				for (int int_f_row = V_ZERO; int_f_row < int_f_rows; int_f_row++)
-					{
-						for (int int_s_col = V_ZERO; int_s_col < int_s_cols; int_s_col++)
-							{
-								for (int int_f_col = V_ZERO; int_f_col < int_f_cols; int_f_col++)
-									{
-										int_matrix_prod[int_f_row][int_s_col] += int_f_matrix[int_f_row][int_f_col] * int_s_matrix[int_f_col][int_s_col];
-										printf("i=[%d],j=[%d],k=[%d]:\t{(i=[%d], k=[%d])=[%d], (k=[%d],j=[%d]) = [%d]} = [%d]:\t[%d].\n", int_f_row, int_s_col, int_f_col, int_f_row, int_f_col, int_f_matrix[int_f_row][int_f_col], int_f_col, int_s_col, int_s_matrix[int_f_col][int_s_col], int_f_matrix[int_f_row][int_f_col] * int_s_matrix[int_f_col][int_s_col], int_matrix_prod[int_f_row][int_s_col]);
-									}
-
-								printf("\n");
-							}
-
-						printf("\n");
-					}
-			}
-		else
-			perror("There is not enough space in memory to allocate a matrix product array...");
-
-		return int_matrix_prod;
-	}
-
-
 int main()
 	{
 		int int_f_captured_items = V_ZERO, int_s_captured_items = V_ZERO;
@@ -177,23 +204,28 @@ int main()
 
 
 		printf("Program to perform the product of two rectangular matrices.\n");
-		printf("\n");
 
+		printf("\n");
 		printf("Number of rows in the first matrix: ");
 		scanf("%d", &int_f_rows);
 		printf("Number of columns in the first matrix: ");
 		scanf("%d", &int_f_cols);
 
 		int_f_matrix = int_create_matrix(int_f_rows, int_f_cols);
+		int_f_captured_items = int_capture_matrix(int_f_matrix, int_f_rows, int_f_cols);
 		int_f_captured_items = int_view_matrix(int_f_matrix, int_f_rows, int_f_cols);
 
+
+		printf("\n");
 		printf("Number of rows in the second matrix: ");
 		scanf("%d", &int_s_rows);
 		printf("Number of columns in the second matrix: ");
 		scanf("%d", &int_s_cols);
 
 		int_s_matrix = int_create_matrix(int_s_rows, int_s_cols);
+		int_s_captured_items = int_capture_matrix(int_s_matrix, int_s_rows, int_s_cols);
 		int_s_captured_items = int_view_matrix(int_s_matrix, int_s_rows, int_s_cols);
+
 
 		int_matrix_prod = int_matrix_product(int_f_matrix, int_s_matrix, int_f_rows, int_f_cols, int_s_rows, int_s_cols, &int_max_rows, &int_max_cols);
 
@@ -201,9 +233,12 @@ int main()
 		printf("The complete product matrix is ​​displayed...\n");
 		int_view_matrix(int_matrix_prod, int_max_rows, int_max_cols);
 
+
+		printf("\n");
+		printf("Evicting arrays from allocated memory areas...\n");
 		int_deallocate_matrix(int_f_matrix, int_f_rows);
 		int_deallocate_matrix(int_s_matrix, int_s_rows);
 		int_deallocate_matrix(int_matrix_prod, int_max_rows);
 
-		return V_ZERO;
+		return EXIT_SUCCESS;
 	}
