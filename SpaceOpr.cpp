1eec35f813832a87a89571414b633dfcdea493d5
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-03-03 12:12:27 -0600

    Mathematical Functions General Project
    Program that uses the C++20 'spaceship' operator.
    This program must be compiled in C++20 with the directive "g++ --std=c++20" to test the 'spaceship' type operator.

---
+++ SpaceOpr.cpp
@@ -0,0 +1,27 @@
+#include <compare>
+#include <iostream>
+
+#define V_ZERO  0
+
+int main()
+	{
+		int l_value = V_ZERO, r_value = V_ZERO;
+		std::cout << "Spaceship comparator." << std::endl;
+
+		std::cout << "First value: ";
+		std::cin >> l_value;
+
+		std::cout << "First value: ";
+		std::cin >> r_value;
+
+		auto result = (l_value <=> r_value);
+
+		if (result < V_ZERO)
+			std::cout << std::endl << "(" << l_value << " < " << r_value << ")" << std::endl;
+		else if (result > V_ZERO)
+			std::cout << std::endl << "(" << l_value << " > " << r_value << ")" << std::endl;
+		else if (result == V_ZERO)
+			std::cout << std::endl << "(" << l_value << " = " << r_value << ")" << std::endl;
+
+		return V_ZERO;
+	}
