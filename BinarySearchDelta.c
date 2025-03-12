41976e6594306ba9617ee002e1c4b38e982ab8fe
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-03-12 17:15:17 -0600

    Mathematical Functions General Project
    The purpose of this program is to search for a given value as a key within an array of integers using traditional binary search, recursive binary search, binary search with a 'delta' adjustment factor and approximate interpolation locating through a menu of options. If said key value is found, it returns its relative position or entry number (displacement index) within said numeric array.

--- BinarySearchDelta.c
+++ BinarySearchDelta.c
@@ -17,6 +17,16 @@
 #define	V_ONE		1
 #define	V_ZERO		0
 
+//Enumeration with menu options.
+enum enm_options
+	{
+		opt_bin_srch_normal = V_ONE,
+		opt_bin_srch_recursive,
+		opt_bin_srch_delta,
+		opt_interpolation_srch,
+		opt_exit
+	} enm_option;
+
 //Global static array of prime numbers.
 static const int array[] = {
 			2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
@@ -31,6 +41,11 @@ static const int array[] = {
 			829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
 			947, 953, 967, 971, 977, 983, 991, 997
 			};
+
+//Prototype functions.
+char getPause(const char *str_Message);
+int SelectedOption(int *value);
+
 
 //Improved traditional binary search function.
 int BinarySearch(const int array[], const int size, const int target_key, int *pos, int *iters)
@@ -113,17 +128,17 @@ int BinarySearchRecursive(const int array[], const int target_key, const int bot
 int getElement(const char *str_Message, const int array[], const int size, const int target_key, const int position, const int iterations)
 	{
 		//Preliminary working variables.
+		char stop_key = NULL_CHARACTER;
 		int item = V_ZERO;
 
 		//Validates if the position is in a correct range.
 		if (position >= V_ZERO && position < size)
 			{
-				printf("\n");
 				printf("[%s].\n", str_Message);
 				printf("+===+====+===+====+===+\n");
 				printf("|    Binary Search.   |\n");
 				printf("+===+====+===+====+===+\n");
-				printf("| * Element  : [%d].\n", target_key);
+				printf("| * Element  : <%d>.\n", target_key);
 				printf("| + Cycles   : {%d}.\n", iterations);
 				printf("+-=-+----+---+----+-=-+\n");
 				printf("|      Item Info.     |\n");
@@ -131,8 +146,8 @@ int getElement(const char *str_Message, const int array[], const int size, const
 				printf("| * Position : [%d].\n", position);
 				printf("| * Content  : [%d].\n", array[position]);
 				printf("+===+====+===+====+===+\n");
-				printf("\n");
 
+				stop_key = getPause("Press the ENTER key to continue...");
 				item = array[position];
 			}
 		else
@@ -141,6 +156,7 @@ int getElement(const char *str_Message, const int array[], const int size, const
 		return item;
 	}
 
+//Get a correct integer value.
 int getEntry(int *target_key)
 	{
 		//Preliminary working variables.
@@ -162,6 +178,24 @@ int getEntry(int *target_key)
 		return value_key;
 	}
 
+//Make a pause.
+char getPause(const char *str_Message)
+	{
+		//Preliminary working variables.
+		char chr_key = NULL_CHARACTER;
+
+		printf("%s", str_Message);
+
+		//Validate data entry as correct.
+		if (scanf("%c", &chr_key))
+			{
+				scanf("%*[^\n]%*c");
+				while ((chr_key = getchar()) != CARRIAGE_RETURN && chr_key != EOF);
+			};
+
+		return chr_key;
+	}
+
 //Function to locate a certain value by means of interpolations.
 int InterpolationLocate(const int array[], const int bottom, const int top, const int target_key, int *middle_bottom, int *middle_top, int *iters)
 	{
@@ -174,8 +208,34 @@ int InterpolationLocate(const int array[], const int bottom, const int top, cons
 		return (*middle_top >= bottom && *middle_top <= top) && (*middle_bottom >= bottom && *middle_bottom <= top);
 	}
 
-//Main function.
-int main()
+//Options menu.
+int Menu(int *option)
+	{
+		int value = V_ZERO;
+
+		while (value != opt_exit)
+			{
+				printf("\n");
+				printf("+===+====+===+====+===+====+===+\n");
+				printf("|    Binary search services.   |\n");
+				printf("+===+====+===+====+===+====+===+\n");
+				printf("| [1]. Binary search in loop.  |\n");
+				printf("| [2]. Recursive binary search.|\n");
+				printf("| [3]. Binary delta search.    |\n");
+				printf("| [4]. Interpolation search.   |\n");
+				printf("| [5]. Exit this program.      |\n");
+				printf("+===+====+===+====+===+====+===+\n");
+				printf("Choose an option: ");
+
+				*option = value = getEntry(&value);
+				*option = value = SelectedOption(&value);
+			}
+
+		return value;
+	}
+
+//Function to execute a function given an option.
+int SelectedOption(int *value)
 	{
 		//Preliminary working variables.
 		int delta_factor = V_ZERO;	//Delta factor that adjusts the position of the medium.
@@ -186,38 +246,67 @@ int main()
 		int target_key = V_ZERO;	//Numeric search key.
 
 		//Presentation and indication headers.
-		printf("Search for a prime number within the first thousand.\n");
-		printf("Prime number to find: ");
-		target_key = getEntry(&target_key);
-
-		//We call the traditional binary search function.
-		position = BinarySearch(array, size, target_key, &position, &iterations);
-		getElement("Traditional Loop Binary Search", array, size, target_key, position, iterations);
-
-		//We call the binary search function with a delta factor.
-		position = BinarySearchDelta(array, size, target_key, &delta_factor, &position, &iterations);
-		getElement("Binary Search with delta factor", array, size, target_key, position, iterations);
-		printf("  * Delta Fx : [%d].\n", delta_factor);
-
-		//We call the traditional recursive and cyclic binary search function.
-		iterations = V_ZERO;
-		position = BinarySearchRecursive(array, target_key, V_ZERO, size + V_MINUS_ONE, &position, &iterations, &iteratives);
-		getElement("Recursive Loop Binary Search", array, size, target_key, position, iterations);
-		printf("[%d] calls made to the recursive binary search function.\n", iteratives);
-
-		//We call a function that obtains the position of the searched element by means of interpolations.
-		if (InterpolationLocate(array, V_ZERO, size - V_ONE, target_key, &middle_bottom, &middle_top, &iterations))
+		if (*value >= opt_bin_srch_normal && *value < opt_exit)
 			{
-				printf("\n** [Approximate values ​​obtained] **.\n");
-				getElement("Interpolated lower value obtained", array, size, target_key, middle_bottom, iterations);
-				getElement("Interpolated upper value obtained", array, size, target_key, middle_top, iterations);
+				printf("Search for a prime number within the first thousand.\n");
+				printf("Prime number to find: ");
+				target_key = getEntry(&target_key);
 			}
 
-		//We call the traditional recursive and cyclic binary search function.
-		iterations = V_ZERO;
-		position = BinarySearchRecursive(array, target_key, V_ZERO, size + V_MINUS_ONE, &position, &iterations, &iteratives);
-		getElement("Recursive Loop Binary Search", array, size, target_key, position, iterations);
-		printf("[%d] calls made to the recursive binary search function.\n", iteratives);
+		enm_option = (enum enm_options) *value;
+
+		switch (enm_option)
+			{
+				//We call the traditional binary search function.
+				case opt_bin_srch_normal:
+					position = BinarySearch(array, size, target_key, &position, &iterations);
+					getElement("Traditional Loop Binary Search", array, size, target_key, position, iterations);
+					break;
+
+				//We call the traditional recursive and cyclic binary search function.
+				case opt_bin_srch_recursive:
+					iterations = V_ZERO;
+					position = BinarySearchRecursive(array, target_key, V_ZERO, size + V_MINUS_ONE, &position, &iterations, &iteratives);
+					getElement("Recursive Loop Binary Search", array, size, target_key, position, iterations);
+					printf("[%d] calls made to the recursive binary search function.\n", iteratives);
+					break;
+
+				//We call the binary search function with a delta factor.
+				case opt_bin_srch_delta:
+					position = BinarySearchDelta(array, size, target_key, &delta_factor, &position, &iterations);
+					getElement("Binary Search with delta factor", array, size, target_key, position, iterations);
+					printf("  * Delta Fx : [%d].\n", delta_factor);
+					break;
+
+				//We call a function that obtains the position of the searched element by means of interpolations.
+				case opt_interpolation_srch:
+					if (InterpolationLocate(array, V_ZERO, size - V_ONE, target_key, &middle_bottom, &middle_top, &iterations))
+						{
+							printf("\n** [Approximate values ​​obtained] **.\n");
+							getElement("Interpolated lower value obtained", array, size, target_key, middle_bottom, iterations);
+							getElement("Interpolated upper value obtained", array, size, target_key, middle_top, iterations);
+						}
+					break;
+
+				//Program exit case.
+				case opt_exit:
+					printf("\nLeaving this program...\n");
+					break;
+
+				//Case of failed option.
+				default:
+					printf("The selected option: [%d] is not valid.\n", *value);
+					break;
+			}
+
+		return *value;
+	}
+
+
+//Main function.
+int main()
+	{
+		int option = Menu(&option);
 
 		return V_ZERO;
 	}
