67e358709844d65aed94536b53ce14c3122c21e8
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-03-26 11:39:34 -0600

    Mathematical Functions General Project
    The purpose of this program is to implement a program that creates a sequential formatted text file and retrieves and copies it to another file of the same type, even setting limits on the number of records to be copied. This program is ideal if you need to copy a certain number of records from one file to another, or simply create a blank one.

--- CopyFile.c
+++ CopyFile.c
@@ -4,38 +4,47 @@
 #include "stdlib.h"
 #include "string.h"
 
+//Symbolic work constants.
 #define NULL_CHARACTER		'\0'
 #define	CARRIAGE_RETURN		'\n'
 
 #define	_READ_ONLY_FILE_	"\x72\x2b"
 #define	_WRITE_ONLY_FILE_	"\x77\x2b"
 
+#define V_ONE			1
 #define	V_TWELVE		12
 #define	V_ZERO			0
 
+/* General global variable. */
 int key = V_ZERO;
 
+/* Prototypes of the functions of this program. */
 int CheckErrorFile(const char *strFile, FILE *pFile);
-int CopyFile(const char *str_SourceFile, const char *str_TargetFile);
+int CopyFile(const char *str_SourceFile, const char *str_TargetFile, const int numRecords);
 char *GetFileName(const char *strMessage, char *strFile);
-int DumpFile(const char *str_File);
+int DumpFile(const char *str_File, const int numRecords);
 int CreateFile(const char *str_File);
 
-int main()	/* Main function */
+/* Main function */
+int main()
 	{
+		/* Preliminary working variables. */
 		char *strFIn = (char *) malloc(V_TWELVE * sizeof(char));
 		char *strFOut = (char *) malloc(V_TWELVE * sizeof(char));
+		int numRecords = V_ZERO;
 
 		/* Data entry */
 		printf("File Copy.\n");
 		GetFileName("Source file : ", strFIn);
 		GetFileName("Target file : ", strFOut);
 
-		CreateFile(strFIn);
-		DumpFile(strFIn);
-		CopyFile(strFIn, strFOut);
-		DumpFile(strFOut);
+		//Calls to special functions.
+		numRecords = CreateFile(strFIn);
+		numRecords = DumpFile(strFIn, numRecords);
+		numRecords = CopyFile(strFIn, strFOut, numRecords);
+		numRecords = DumpFile(strFOut, numRecords);
 
+		//Freeing memory pointers.
 		free(strFIn);
 		free(strFOut);
 	}
@@ -49,6 +58,7 @@ int CheckErrorFile(const char *strFile, FILE *pFile)
 				printf("| I/O Error detection |\n");
 				printf("+---+----+---+----+---+\n");
 				printf("| - File:\t\t[%s].\n", strFile);
+				printf("| - Address:\t[%p].\n", pFile);
 				printf("| - Error number:\t[%d].\n", errno);
 				printf("| - Description:\t[%s].\n", strerror(errno));
 				printf("+---+----+---+----+---+\n");
@@ -58,14 +68,15 @@ int CheckErrorFile(const char *strFile, FILE *pFile)
 			}
 
 		if (feof(pFile))
-			printf("End of file: [%s] reached.\n", strFile);
+			printf("End of file: [%s] in address: [%p] reached.\n", strFile, pFile);
 
 		return (ferror(pFile) || feof(pFile));
 	}
 
-int CopyFile(const char *strSource, const char *strTarget)
+int CopyFile(const char *strSource, const char *strTarget, const int numRecords)
 	{
 		FILE *pFSource = NULL, *pFTarget = NULL;
+		int inpBytes = V_ZERO, inpKey = V_ZERO, outBytes = V_ZERO, outKey = V_ZERO;
 		int idx = V_ZERO;
 
 		printf("\nCopy Files.\n");
@@ -79,18 +90,26 @@ int CopyFile(const char *strSource, const char *strTarget)
 						printf("File: [%s] is copied to file: [%s].\n", strSource, strTarget);
 
 						while ((!feof(pFSource) && !ferror(pFSource))
-						    && (!feof(pFTarget) && !ferror(pFTarget)))
+						    && (!feof(pFTarget) && !ferror(pFTarget))
+						    && (idx < numRecords))
 							{
-								if (fscanf(pFSource, "%*c%d%*c%*c", &key) && !feof(pFSource) && !ferror(pFSource))
+								if (fscanf(pFSource, "%*c%d%*c%*c%n", &inpKey, &inpBytes) && !feof(pFSource) && !ferror(pFSource) && idx < numRecords)
 									{
-										if (fprintf(pFTarget, "{%d}.\n", key))
-											printf("#: [%d]\t:\t{%d}.\n", idx++, key);
+										if (fprintf(pFTarget, "{%d}.%n\n", outKey = inpKey, &outBytes))
+											printf("#: [%d]\t:\t(%d)\t=\t{%d}.\t<%d>\t=\t<%d>.\n", idx++, inpKey, outKey, inpBytes, outBytes);
 									}
 							}
 
 						CheckErrorFile(strSource, pFSource);
 						CheckErrorFile(strTarget, pFTarget);
-						printf("File: [%s] was written with: [%d] records.\n", strTarget, idx);
+
+						printf("\nSize of the source and destination copy files.\n");
+						fseek(pFSource, V_ZERO, SEEK_END);
+						printf("File size: [%s] with: [%ld] bytes.\n", strSource, ftell(pFSource));
+						fseek(pFTarget, V_ZERO, SEEK_END);
+						printf("File size: [%s] with: [%ld] bytes.\n", strTarget, ftell(pFTarget));
+
+						printf("File: [%s] was written with: [%d] records out of a total of: [%d].\n", strTarget, idx, numRecords);
 
 						fclose(pFSource);
 						fclose(pFTarget);
@@ -108,7 +127,7 @@ int CreateFile(const char *strFile)
 	{
 		char c = NULL_CHARACTER;
 		FILE *pFile = NULL;
-		int idx = V_ZERO;
+		int idx = V_ZERO, outBytes = V_ZERO;
 
 		printf("\nCreate file.\n");
 		printf("Loading File: [%s]. Opening mode: [%s].\n", strFile, _WRITE_ONLY_FILE_);
@@ -121,7 +140,7 @@ int CreateFile(const char *strFile)
 					{
 						printf("Code: ");
 
-						if (scanf("%d", &key))
+						if (scanf("%d%*c", &key) == V_ONE)
 							printf("Correct entry: [%d]. OK!\n", key);
 						else
 							{
@@ -131,11 +150,15 @@ int CreateFile(const char *strFile)
 
 						if (key == V_ZERO) break;
 
-						if (fprintf(pFile, "[%d].\n", key))
-							printf("#: [%d]\t:\t[%d].\n", idx++, key);
+						if (fprintf(pFile, "(%d).\n%n", key, &outBytes))
+							printf("#: [%d]\t:\t[%d].\t<%d>.\n", idx++, key, outBytes);
 					}
 
 				CheckErrorFile(strFile, pFile);
+
+				printf("\nSize of the created file.\n");
+				fseek(pFile, V_ZERO, SEEK_END);
+				printf("File size: [%s] with: [%ld] bytes.\n", strFile, ftell(pFile));
 				printf("File saved: [%s] with: [%d] records.\n", strFile, idx);
 
 				fclose(pFile);
@@ -146,10 +169,10 @@ int CreateFile(const char *strFile)
 		return idx;
 	}
 
-int DumpFile(const char *strFile)
+int DumpFile(const char *strFile, const int numRecords)
 	{
 		FILE *pFile = NULL;
-		int idx = V_ZERO;
+		int inpBytes = V_ZERO, idx = V_ZERO;
 
 		printf("\nDump file.\n");
 		printf("Unloading File: [%s]. Opening mode: [%s].\n", strFile, _READ_ONLY_FILE_);
@@ -158,17 +181,18 @@ int DumpFile(const char *strFile)
 			{
 				printf("Viewing file content...\n");
 
-				while (!feof(pFile) && !ferror(pFile))
+				while (!feof(pFile) && !ferror(pFile) && idx < numRecords)
 					{
-						if (fscanf(pFile, "%*c%d%*c%*c", &key) && !feof(pFile) && !ferror(pFile))
-							printf("#: [%d]\t:\t[%d].\n", idx++, key);
+						if (fscanf(pFile, "%*c%d%*c%*c%n", &key, &inpBytes) && !feof(pFile) && !ferror(pFile) && idx < numRecords)
+							printf("#: [%d]\t:\t[%d].\t<%d>.\n", idx++, key, inpBytes);
 					}
 
 				CheckErrorFile(strFile, pFile);
-				printf("File recovered: [%s] with: [%d] records.\n", strFile, idx);
 
+				printf("\nSize of the dump file.\n");
 				fseek(pFile, V_ZERO, SEEK_END);
 				printf("File size: [%s] with: [%ld] bytes.\n", strFile, ftell(pFile));
+				printf("File recovered: [%s] with: [%d] records out of a total of: [%d].\n", strFile, idx, numRecords);
 
 				fclose(pFile);
 			}
@@ -186,7 +210,7 @@ char *GetFileName(const char *strMessage, char *strFile)
 			{
 				printf("\n%s", strMessage);
 
-				if (scanf("%s", strFile))
+				if (scanf("%s%*c", strFile) == V_ONE)
 					printf("Correct entry: [%s]. OK!\n", strFile);
 				else
 					{
@@ -195,7 +219,7 @@ char *GetFileName(const char *strMessage, char *strFile)
 					}
 			}
 		else
-			printf("No valid memory address allocated for the file name.");
+			printf("No valid memory address allocated for the file name: [%s].\n", strFile);
 
 		return strFile;
 	}
