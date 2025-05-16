0708f29cb142c7edcc5e200701c76a98814d9210
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-05-16 09:30:55 -0600

    Mathematical Functions General Project
    The purpose of this program is to exemplify the use of pointers to functions and to compare the addresses in which they are housed with respect to the variables of the main function, and to exemplify their advantages and uses.

--- FnsPtrsa.c
+++ FnsPtrsa.c
@@ -1,60 +1,80 @@
+/* Program that uses pointers to functions of a certain result type. */
 #include <stdio.h>
 
-#define	V_FIVE	5
-#define	V_TEN	10
+/* Symbolic work constants. */
 #define	V_ZERO	0
 
-
 //Function prototipes.
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b);
-
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
 
 //Function to perform arithmetic operation using function pointer.
-int performArithmetic(int a, int b, int (*operation)(int, int))
+float performArithmetic(float a, float b, float (*operation)(float, float))
 	{
 		return operation(a, b);
 	}
 
-
+//Main function.
 int main()
 	{
-		int a = V_TEN, b = V_FIVE;
+		/* Preliminary working variables. */
+		float a = V_ZERO, b = V_ZERO;
+
+		//Request for input data.
+		printf("Function Pointers in \'C\'.\n");
+		printf("Value 'a' : ");
+		scanf("%f", &a);
+		printf("Value 'b' : ");
+		scanf("%f", &b);
 
 		//Call the performArithmetic function using function pointers.
-		int sum = performArithmetic(a, b, add);
-		int difference = performArithmetic(a, b, subtract);
-		int product = performArithmetic(a, b, multiply);
-		int quotient = performArithmetic(a, b, divide);
+		float sum = performArithmetic(a, b, add);
+		float difference = performArithmetic(a, b, subtract);
+		float product = performArithmetic(a, b, multiply);
+		float quotient = performArithmetic(a, b, divide);
 
 		//Deployment of results.
-		printf("Sum of [%d] and [%d]:\t\t\t[%d]:\t[%p].\tSum address:\t\t[%p].\n", a, b, sum, &sum, add);
-		printf("Difference between [%d] and [%d]:\t[%d]:\t[%p].\tDifference address:\t[%p].\n", a, b, difference, &difference, subtract);
-		printf("Product of [%d] and [%d]:\t\t[%d]:\t[%p].\tMultiply address:\t[%p].\n", a, b, product, &product, multiply);
-		printf("Quotient of [%d] and [%d]:\t\t[%d]:\t[%p].\tDivide address:\t\t[%p].\n", a, b, quotient, &quotient, divide);
+		printf("\nResults of calls to pointers.\n");
+		printf("Perform Arithmetic.\n");
+		printf("(%p).\n\n", performArithmetic);
+
+		printf("Add:\t\t(%p).\n", add);
+		printf("{(%p) = [%f]} + {(%p) = [%f]} = {(%p) = [%f]}.\n\n", &a, a, &b, b, &sum, sum);
+
+		printf("Subtract:\t(%p).\n", subtract);
+		printf("{(%p) = [%f]} - {(%p) = [%f]} = {(%p) = [%f]}.\n\n", &a, a, &b, b, &difference, difference);
+
+		printf("Multiply:\t(%p).\n", multiply);
+		printf("{(%p) = [%f]} * {(%p) = [%f]} = {(%p) = [%f]}.\n\n", &a, a, &b, b, &product, product);
+
+		printf("Divide:\t\t(%p).\n", divide);
+		printf("{(%p) = [%f]} / {(%p) = [%f]} = {(%p) = [%f]}.\n\n", &a, a, &b, b, &quotient, quotient);
 
 		return V_ZERO;
 	}
 
-
-int add(int a, int b)
+//Adition.
+float add(float a, float b)
 	{
 		return a + b;
 	}
 
-int subtract(int a, int b)
+//Subtraction.
+float subtract(float a, float b)
 	{
 		return a - b;
 	}
 
-int multiply(int a, int b)
+//Multiplication.
+float multiply(float a, float b)
 	{
 		return a * b;
 	}
 
-int divide(int a, int b)
+//Division.
+float divide(float a, float b)
 	{
 		return a / b;
 	}
