098771e9e97c197c8ef9f17812b1a2999d0d29be
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-05-19 08:49:41 -0600

    Mathematical Functions General Project
    This program exemplifies different techniques of Object Oriented Programming in C++ language given a class of type Point2D that has 2 attributes 'x' and 'y' and its setter and getter methods, which inherits another class called Point3D with its attribute 'z' and its own setter and getter methods. Different classes of the 'std' namespace are overloaded and templates and friend classes are used, as well as everything is contained in a given namespace.

--- Point.cpp
+++ Point.cpp
@@ -1,166 +1,206 @@
 //General Work Libraries.
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 //Symbolic constant of work.
 #define V_ZERO  0
 
-//Namespace to hold both types of 2D and 3D Points.
-namespace SpacePoints
+//First '2D Point' template type class.
+template <typename T>
+class Point2D
 	{
-		//First '2D Point' template type class.
-		template <typename T>
-		class Point2D
-			{
-				private:
-					template <typename U>
-					friend std::istream& operator>> (std::istream& in, SpacePoints::Point2D<U> &Sp2D);
-					template <typename U>
-					friend std::ostream &operator<< (std::ostream &os, const SpacePoints::Point2D<U> &Sp2D);
+		private:
+			T x = V_ZERO, y = V_ZERO;
 
-				protected:
-					static int c;
-					T x = V_ZERO, y = V_ZERO;
+			template <typename U>
+			friend std::istream& operator>> (std::istream& in, Point2D<U> &Sp2D);
+			template <typename U>
+			friend std::ostream &operator<< (std::ostream &os, const Point2D<U> &Sp2D);
 
-				public:
-					Point2D (T x, T y) : x(x), y(y) {this->c++;};
+		protected:
+			static int c;
 
-					T getX() const {return this->x;};
-					T getY() const {return this->y;};
+		public:
+			Point2D()
+				{
+					this->c++;
+					this->capture();
+				}
 
-					void setX(const T x) {this->x = x;};
-					void setY(const T y) {this->y = y;};
+			Point2D (T &x, T &y) : x(x), y(y) {this->c++;};
 
-					virtual void print() const
+			virtual void capture()
+				{
+					std::cout << "Capturing coordinates of a 'Point2D' object..." << std::endl;
+					std::cout << "x = ";
+					std::cin >> this->x;
+					std::cout << "y = ";
+					std::cin >> this->y;
+				}
+
+			T getX() const {return this->x;};
+			T getY() const {return this->y;};
+
+			virtual void print() const
+				{
+					std::cout << "Displaying values ​​of a 'Point2D' type object..." << std::endl;
+					std::cout << "2D Point # " << this->c << std::endl;
+					std::cout << "(x = [" << this->x << "], y = [" << this->y << "])." << std::endl;
+				};
+
+			void setX(const T &x) {this->x = x;};
+			void setY(const T &y) {this->y = y;};
+
+			virtual ~Point2D() = default;
+	};
+
+//Initialization of the static count value of created 2D points.
+template <typename T>
+int Point2D<T>::c = V_ZERO;
+
+//Second '3D Point' template type class inherits its attributes and methods from the '2D Point' class.
+template <typename T>
+class Point3D : public Point2D<T>
+	{
+		private:
+			T z = V_ZERO;
+
+			template <typename U>
+			friend std::istream &operator>> (std::istream& in, Point3D<U> &Sp3D);
+			template <typename U>
+			friend std::ostream &operator<< (std::ostream &os, const Point3D<U> &Sp3D);
+
+		public:
+			Point3D()	{ this->capture(); }
+
+			Point3D(T &x, T &y, T &z) : Point2D<T>(x, y), z(z) {};
+
+			virtual void capture()
+				{
+					Point2D<T>::capture();
+					std::cout << "Capturing coordinates of a 'Point3D' object..." << std::endl;
+					std::cout << "z = ";
+					std::cin >> this->z;
+				}
+
+			T getZ() const {return this->z;};
+
+			void print() const
+				{
+					Point2D<T>::print();
+					std::cout << "Displaying values ​​of a 'Point3D' type object..." << std::endl;
+					std::cout << "3D Point # " << this->c << std::endl;
+					std::cout << "(x = [" << this->getX() << "], y = [" << this->getY() << "], z = [" << this->z << "])." << std::endl;
+				};
+
+			void setZ(const T &z) {this->z = z;};
+
+			virtual ~Point3D() = default;
+	};
+
+//Line class.
+template <typename T>
+class Line
+	{
+
+		private:
+			/* Storing a vector of 'Point2D' objects. */
+			std::vector<Point2D<T>> vSp2D{};
+
+		public:
+			/* Add a 'Point2D' class to the 'Point2D' point vector. */
+			void addPoint2D(const  ::Point2D<T> &Sp2D)
+				{
+					std::cout << "2D Point:\t(x = [" << Sp2D.getX() << "], y = [" << Sp2D.getY() << "])." << std::endl;
+					vSp2D.push_back(Sp2D);
+				};
+
+			/* Clear 'Point2D' point vector. */
+			void clearVPoint2D()
+				{
+					std::cout << "All the points that made up the line have been cleaned." << std::endl;
+					vSp2D.clear();
+				};
+
+			/* Print saved 'Point2D' classes. */
+			virtual void print() const
+				{
+					std::cout << "'Point2D' classes added to the 'Line' class." << std::endl;
+
+					for (auto it_vSp2D = std::begin(vSp2D); it_vSp2D != std::end(vSp2D); it_vSp2D++)
 						{
-							std::cout << "2D Point #" << this->c << ":\t(" << this->x << ", " << this->y << ")." << std::endl;
-						};
+							std::cout << "2D Point:\t(x = [" << it_vSp2D->getX() << "], y = [" << it_vSp2D->getY() << "])." << std::endl;
+						}
 
-			};
+					std::cout << "[" << vSp2D.size() << "] 'Point2D' classes contained." << std::endl;
+				};
+	};
 
-		//Initialization of the static count value of created 2D points.
-		template <typename T>
-		int Point2D<T>::c = V_ZERO;
+//Overloading the >> operator to read 'x' and 'y' values ​​from std::cin for 'Point2D' object.
+template <typename T>
+inline std::istream &operator>> (std::istream &in, Point2D<T> &Sp2D)
+	{
+		T x = V_ZERO, y = V_ZERO;
 
-		//Second '3D Point' template type class inherits its attributes and methods from the '2D Point' class.
-		template <typename T>
-		class Point3D : public SpacePoints::Point2D<T>
-			{
-				private:
-					template <typename U>
-					friend std::istream &operator>> (std::istream& in, SpacePoints::Point3D<U> &Sp3D);
-					template <typename U>
-					friend std::ostream &operator<< (std::ostream &os, const SpacePoints::Point3D<U> &Sp3D);
+		std::cout << "Setting coordinates of a 'Point2D' type object..." << std::endl;
+		std::cout << "x = ";
+		std::cin >> x;  //Read the value of 'x'.
+		std::cout << "y = ";
+		std::cin >> y;  //Read the value of 'y'.
 
-					T z = V_ZERO;
+		Sp2D.setX(x);
+		Sp2D.setY(y);
 
-				public:
-					Point3D(T x, T y, T z) : SpacePoints::Point2D<T>(x, y), z(z) {};
+		std::cout << "New 'Point2D' coordinates established." << std::endl << std::endl;
 
-					T getZ() const {return this->z;};
-					void setZ(const T z) {this->z = z;};
+		return in;
+	}
 
-					void print() const
-						{
-							SpacePoints::Point2D<T>::print();
-							std::cout << "3D Point #" << this->c << ":\t(" << this->z << ")." << std::endl;
-						};
-			};
+//Overloading the std::cout operator to receive a '2D Point' object type.
+template <typename T>
+inline std::ostream &operator<< (std::ostream &os, const Point2D<T> &Sp2D)
+	{
+		std::cout << "Showing values ​​of a 'Point2D' type object..." << std::endl;
+		std::cout << "2D Point # " << Sp2D.c << std::endl;
+		std::cout << "(x = [" << Sp2D.getX() << "], y = [" << Sp2D.getY() << "])." << std::endl;
 
-		//Line class.
-		template <typename T>
-		class Line
-			{
+		return os;
+	};
 
-				private:
-					/* Storing a vector of 'Point2D' objects. */
-					std::vector<SpacePoints::Point2D<T>> vSp2D;
+//Overloading the >> operator to read 'x', 'y' and 'z' values ​​from std::cin for 'Point3D' object.
+template <typename T>
+inline std::istream &operator>> (std::istream& in, Point3D<T> &Sp3D)
+	{
+		T x = V_ZERO, y = V_ZERO, z = V_ZERO;
 
-				public:
-					/* Add a 'Point2D' class to the 'Point2D' point vector. */
-					void addPoint2D(const SpacePoints::Point2D<T> &Sp2D)
-						{
-							std::cout << "2D Point:\t(x = [" << Sp2D.getX() << "], y = [" << Sp2D.getY() << "])." << std::endl;
-							vSp2D.push_back(Sp2D);
-						};
+		std::cout << "Setting coordinates of a 'Point3D' type object..." << std::endl;
+		std::cout << "x = ";
+		std::cin >> x;  //Read the value of 'x'.
+		std::cout << "y = ";
+		std::cin >> y;  //Read the value of 'y'.
+		std::cout << "z = ";
+		std::cin >> z;  //Read the value of 'z'.
 
-					/* Clear 'Point2D' point vector. */
-					void clearVPoint2D()
-						{
-							std::cout << "All the points that made up the line have been cleaned." << std::endl;
-							vSp2D.clear();
-						};
+		Sp3D.setX(x);
+		Sp3D.setY(y);
+		Sp3D.setZ(z);
 
-					/* Print saved 'Point2D' classes. */
-					virtual void print() const
-						{
-							std::cout << "'Point2D' classes added to the 'Line' class." << std::endl;
+		std::cout << "New 'Point3D' coordinates established." << std::endl << std::endl;
 
-							for (auto it_vSp2D = std::begin(vSp2D); it_vSp2D != std::end(vSp2D); it_vSp2D++)
-								{
-									std::cout << "2D Point:\t(x = [" << it_vSp2D->getX() << "], y = [" << it_vSp2D->getY() << "])." << std::endl;
-								}
+		return in;
+	};
 
-							std::cout << "[" << vSp2D.size() << "] 'Point2D' classes contained." << std::endl;
-						};
-			};
+//Overloading the std::cout operator to receive a '3D Point' object type.
+template <typename T>
+inline std::ostream &operator<< (std::ostream &os, const Point3D<T> &Sp3D)
+	{
+		std::cout << "Showing values ​​of a 'Point3D' type object..." << std::endl;
+		std::cout << "3D Point # " << Sp3D.c << std::endl;
+		std::cout << "(x = [" << Sp3D.getX() << "], y = [" << Sp3D.getY() << "], z = [" << Sp3D.getZ() << "])." << std::endl;
 
-	//Overloading the >> operator to read 'x' and 'y' values ​​from std::cin for 'Point2D' object.
-	template <typename T>
-	inline std::istream &operator>> (std::istream &in, SpacePoints::Point2D<T> &Sp2D)
-		{
-			std::cout << "New 2D Point Coordinates." << std::endl;
-
-        		std::cout << "* X Coord: ";
-        		in >> Sp2D.x;  //Read the value of 'x'.
-
-        		std::cout << "* Y Coord: ";
-        		in >> Sp2D.y;  //Read the value of 'y'.
-
-			std::cout << "New coordinates established." << std::endl << std::endl;
-
-        		return in;
-    		}
-
-	//Overloading the std::cout operator to receive a '2D Point' object type.
-	template <typename T>
-	inline std::ostream &operator<< (std::ostream &os, const SpacePoints::Point2D<T> &Sp2D)
-		{
-			os << "* 2D Point #" << Sp2D.c << ":\t(" << Sp2D.x << ", " << Sp2D.y << "). *" << std::endl;
-
-			return os;
-		};
-
-	//Overloading the >> operator to read 'x', 'y' and 'z' values ​​from std::cin for 'Point3D' object.
-	template <typename T>
-	inline std::istream &operator>> (std::istream& in, SpacePoints::Point3D<T> &Sp3D)
-		{
-			std::cout << "New 3D Point Coordinates." << std::endl;
-
-        		std::cout << "* X Coord: ";
-        		in >> Sp3D.x;  //Read the value of 'x'.
-
-        		std::cout << "* Y Coord: ";
-        		in >> Sp3D.y;  //Read the value of 'y'.
-
-        		std::cout << "* Z Coord: ";
-        		in >> Sp3D.z;  //Read the value of 'z'.
-
-			std::cout << "New coordinates established." << std::endl << std::endl;
-
-        		return in;
-		};
-
-	//Overloading the std::cout operator to receive a '3D Point' object type.
-	template <typename T>
-	inline std::ostream &operator<< (std::ostream &os, const SpacePoints::Point3D<T> &Sp3D)
-		{
-			os << "* 3D Point #" << Sp3D.c << ":\t(" << Sp3D.getX() << ", " << Sp3D.getY() << ", " << Sp3D.getZ() << "). *" << std::endl;
-
-			return os;
-		};
-};
-
+		return os;
+	};
 
 int main()
 	{
@@ -190,24 +230,22 @@ int main()
 		std::cout << "--------" << std::endl;
 
 		std::cout << "Generating an instance of a class template of type '2D Point'." << std::endl;
-		SpacePoints::Point2D<int> Sp2D = SpacePoints::Point2D<int>(int_2D_x, int_2D_y);
+		Point2D<int> Sp2D = Point2D<int>(int_2D_x, int_2D_y);
 		Sp2D.print();
 		std::cout << Sp2D << std::endl;
 
 		std::cout << "Generating an instance of a class template of type '3D Point'." << std::endl;
-		SpacePoints::Point3D<int> Sp3D = SpacePoints::Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
+		Point3D<int> Sp3D = Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
 		Sp3D.print();
 		std::cout << Sp3D << std::endl;
 
 		std::cout << "Change of 'x' & 'y' coordinates of the '2D Point' object." << std::endl;
-		Sp2D.setX(int_3D_x);
-		Sp2D.setY(int_3D_y);
+		Sp2D.capture();
 		Sp2D.print();
 		std::cout << Sp2D << std::endl;
 
 		std::cout << "Change of 'x' & 'y' coordinates of the '3D Point' object." << std::endl;
-		Sp3D.setX(int_2D_x);
-		Sp3D.setY(int_2D_y);
+		Sp3D.capture();
 		Sp3D.print();
 		std::cout << Sp3D << std::endl;
 
@@ -222,23 +260,26 @@ int main()
 		std::cout << Sp3D << std::endl;
 
 		std::cout << "Polymorphism by assigning a '3D Point' class to a '2D Point' object." << std::endl;
-		SpacePoints::Point2D<int> Sp2x3D = SpacePoints::Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
+		Point2D<int> Sp2x3D = Point3D<int>(int_3D_x, int_3D_y, int_3D_z);
 		Sp2x3D.print();
 		std::cout << Sp2x3D << std::endl;
 
 		std::cout << "Setting new coordinates for '2D Point' in '3D Point' class." << std::endl;
+		Sp2x3D.capture();
+		Sp2x3D.print();
+		std::cout << Sp2x3D;
+
 		std::cin >> Sp2x3D;
 		Sp2x3D.print();
 		std::cout << Sp2x3D << std::endl;
 
 		std::cout << "The necessary points are added and displayed in a 'Line' class." << std::endl;
-		SpacePoints::Line<int> line;
+		Line<int> line;
 		line.addPoint2D(Sp2D);
 		line.addPoint2D(Sp3D);
 		line.addPoint2D(Sp2x3D);
 		line.print();
 		line.clearVPoint2D();
-
 		std::cout << "Program finished..." << std::endl;
 
 		return V_ZERO;
