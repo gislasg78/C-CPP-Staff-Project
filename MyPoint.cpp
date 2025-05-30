d2a7b3bdfa459736828dc0658d48fd8995181578
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-05-30 15:19:46 -0600

    Add files via upload
    The purpose of this program is to exemplify for me various characteristics of object-oriented programming in C++, in order to subsequently support a program that allows the creation of 'Point2D' objects with various attributes and methods and to throw different practice operations.

--- MyPoint.cpp
+++ MyPoint.cpp
@@ -1,4 +1,4 @@
-/* This program creates a parent or base class called 'MyPoint' 
+/* This program creates a parent or base class called 'Point2D'
    and exemplifies different features of Object Oriented Programming in C++.
 */
 #include <iostream>
@@ -7,37 +7,24 @@
 #define	V_ONE	1
 #define V_ZERO  0
 
-/* Base or parent class of the 'MyPoint' type. */
+/* Base or parent class of the 'Point2D' type. */
 template <typename T>
-class MyPoint
+class Point2D
 	{
 		private:
 			T x = V_ZERO, y = V_ZERO;
 
 			template <typename U>
-			friend std::istream &operator>> (std::istream& in, MyPoint<U> &myPoint)
+			friend std::istream &operator>> (std::istream& in, Point2D<U> &Point2D)
 				{
-					U x = V_ZERO, y = V_ZERO;
-					std::cout << "Set new coordinates to the 'MyPoint' object." << std::endl;
-					std::cout << "Base coordinates." << std::endl;
-					std::cout << "x = ";
-					std::cin >> x;
-					std::cout << "y = ";
-					std::cin >> y;
-
-					myPoint.setX(x);
-					myPoint.setY(y);
-
+					Point2D.capture();
 					return in;
 				}
 
 			template <typename U>
-			friend std::ostream &operator<< (std::ostream& out, const MyPoint<U> &myPoint)
+			friend std::ostream &operator<< (std::ostream& out, const Point2D<U> &Point2D)
 				{
-					std::cout << "Display properties of the 'My Point' object." << std::endl;
-					myPoint.viewCounter();
-					std::cout << "(x = [" << myPoint.getX() << "]," << " y = [" << myPoint.getY() << "])." << std::endl;
-
+					Point2D.print();
 					return out;
 				}
 
@@ -45,25 +32,24 @@ class MyPoint
 			static int c;
 
 		public:
-			MyPoint()
+			Point2D()
 				{
 					this->c++;
-					std::cout << "Capture a new object 'MyPoint'." << std::endl;
 					this->capture();
 				};
 
-			MyPoint(const MyPoint& myPoint)
+			Point2D(const Point2D& Point2D)
 				{
 					this->c++;
-					this->x = myPoint.getX();
-					this->y = myPoint.getY();
+					this->x = Point2D.getX();
+					this->y = Point2D.getY();
 				};
 
-			MyPoint(const T &x, const T &y) : x(x), y(y)	{this->c++;};
+			Point2D(const T &x, const T &y) : x(x), y(y)	{this->c++;};
 
 			virtual void capture()
 				{
-					std::cout << "Base coordinates." << std::endl;
+					std::cout << "Capture the coordinates of a '2D Point'." << std::endl;
 					std::cout << "x = ";
 					std::cin >> this->x;
 					std::cout << "y = ";
@@ -75,88 +61,81 @@ class MyPoint
 
 			virtual void print() const
 				{
-					std::cout << "Show properties of the 'My Point' object." << std::endl;
-					this->viewCounter();
-					std::cout << "(x = [" << this->x << "], y = [" << this->y << "])." << std::endl;
+					std::cout << "Display the current values ​​of a '2D Point'." << std::endl;
+					std::cout << "c = [" << this->c << "].\t(x = [" << this->x << "], y = [" << this->y << "])." << std::endl;
 				}
 
 			void setX(const T &x)	{this->x = x;}
 			void setY(const T &y)	{this->y = y;}
 
-			void viewCounter() const
-				{
-					std::cout << "'MyPoint's objects created: " << this->c << std::endl;
-				}
-
-			virtual ~MyPoint() = default;
+			virtual ~Point2D() = default;
 	};
 
-/* Initialization of the static accounting variable of the 'MyPoint' class. */
+/* Initialization of the static accounting variable of the 'Point2D' class. */
 template <typename T>
-int MyPoint<T>::c = V_ZERO;
+int Point2D<T>::c = V_ZERO;
 
 //Main function.
 int main()
 	{
 		/* Preliminary working variables. */
-		int qty = V_ZERO;
+		int quantity = V_ZERO;
 		int x = V_ZERO, y = V_ZERO;
 
 		/* Initial header messages. */
-		std::cout << "Creating 'MyPoint' objects on an array." << std::endl;
-		std::cout << "How many 'MyPoints' do you want to create? : ";
-		std::cin >> qty;
+		std::cout << "Creating 'Point2D' objects on an array." << std::endl;
+		std::cout << "How many 'Point2D' do you want to create? : ";
+		std::cin >> quantity;
 
-		//The dynamic array of pointers to objects of type 'MyPoint' is created.
-		MyPoint<int> **mypoints = new MyPoint<int>* [qty];
+		//The dynamic array of pointers to objects of type 'Point2D' is created.
+		Point2D<int> **array_Point2D = new Point2D<int>* [quantity];
 
-		/* Each object of type 'MyPoint' is created and stored in an array of pointers of the same type. */
-		for (int idx = V_ZERO; idx < qty; idx++)
+		/* Each object of type 'Point2D' is created and stored in an array of pointers of the same type. */
+		for (int idx = V_ZERO; idx < quantity; idx++)
 			{
-				std::cout << std::endl << "'MyPoint' #: " << idx + V_ONE << std::endl;
-				std::cout << "+ 'X' coordinate: ";
+				std::cout << std::endl << "'Point2D' #: [" << idx + V_ONE << "] of: [" << quantity << "]." << std::endl;
+				std::cout << "x = ";
 				std::cin >> x;
-				std::cout << "+ 'Y' coordinate: ";
+				std::cout << "y = ";
 				std::cin >> y;
 
-				mypoints[idx] = new MyPoint<int>(x, y);
+				array_Point2D[idx] = new Point2D<int>(x, y);
 			}
 
-		/* An internal method of the 'MyPoint' object is used to display the assigned values. */
+		/* An internal method of the 'Point2D' object is used to display the assigned values. */
+		for (int idx = V_ZERO; idx < quantity; idx++)
+			{
+				std::cout << std::endl << "'Point2D' #: [" << idx + V_ONE << "] of: [" << quantity << "]." << std::endl;
+				array_Point2D[idx]->print();
+			}
+
+		/* New values ​​are reassigned and captured to created objects of type 'Point2D' */
 		std::cout << std::endl;
-		for (int idx = V_ZERO; idx < qty; idx++)
+		for (int idx = V_ZERO; idx < quantity; idx++)
 			{
-				std::cout << std::endl << "'MyPoint' #: " << idx + V_ONE << std::endl;
-				mypoints[idx]->print();
+				std::cout << std::endl << "'Point2D' #: [" << idx + V_ONE << "] of: [" << quantity << "]." << std::endl;
+				std::cin >> *(*(array_Point2D + idx));
 			}
 
-		/* New values ​​are reassigned and captured to created objects of type 'MyPoint' */
+		/* The reassigned values ​​of each instantiated object of type 'Point2D' are displayed again. */
 		std::cout << std::endl;
-		for (int idx = V_ZERO; idx < qty; idx++)
+		for (int idx = V_ZERO; idx < quantity; idx++)
 			{
-				std::cout << std::endl << "'MyPoint' #: " << idx + V_ONE << std::endl;
-				std::cin >> *mypoints[idx];
+				std::cout << std::endl << "'Point2D' #: [" << idx + V_ONE << "] of: [" << quantity << "]." << std::endl;
+				std::cout << *array_Point2D[idx];
 			}
 
-		/* The reassigned values ​​of each instantiated object of type 'MyPoint' are displayed again. */
-		std::cout << std::endl;
-		for (int idx = V_ZERO; idx < qty; idx++)
+		/* All dynamically created instances of objects of type 'Point2D' are purged. */
+		std::cout << std::endl << "Clearing 'Point2D' objects..." << std::endl;
+		for (int idx = V_ZERO; idx < quantity; idx++)
 			{
-				std::cout << std::endl << "'MyPoint' #: " << idx + V_ONE << std::endl;
-				std::cout << *mypoints[idx];
+				std::cout << "Deleting object 'Point2D' #: [" << idx + V_ONE << "] of: [" << quantity << "]." << std::endl;
+				delete *(array_Point2D + idx);
 			}
 
-		/* All dynamically created instances of objects of type 'MyPoint' are purged. */
-		std::cout << std::endl << "Clearing 'MyPoint' objects..." << std::endl;
-		for (int idx = V_ZERO; idx < qty; idx++)
-			{
-				std::cout << "Deleting object 'MyPoint' #: " << idx + V_ONE << std::endl;
-				delete *(mypoints + idx);
-			}
-
-		/* Deleting the array of pointer objects of type 'MyPoint'. */
-		std::cout << "Deleting the array of pointers of type 'MyPoint'..." << std::endl;
-		delete mypoints;
+		/* Deleting the array of pointer objects of type 'Point2D'. */
+		std::cout << "Deleting the array of pointers of type 'Point2D'..." << std::endl;
+		delete array_Point2D;
 
 		return V_ZERO;
 	}
