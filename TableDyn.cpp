f1d3bb71149b55365911d529af3e8376e72a5fe1
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-05-31 12:47:32 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to generate several three-dimensional and two-dimensional matrices, and a one-dimensional array. The first is to store a series of matrices, each with a variable number of rows, and each with a variable number of columns or cells. The two-dimensional matrix stores the number of rows that each three-dimensional matrix number has individually per entry. Finally, the one-dimensional array stores the number of columns that each row has, individually determined from each of the matrices determined by the user. This is a great example of highly variable dynamic memory allocation for each content captured by the user.

--- TableDyn.cpp
+++ TableDyn.cpp
@@ -1,10 +1,19 @@
+/* Program that dynamically allocates memory in C++
+   to store a series of given matrices,
+   each with a variable number of rows,
+   and each of its respective rows with a variable number of columns.*/
+
+/* Standard work libraries. */
 #include <iostream>
 
+/* Symbolic work constants. */
 #define	V_ONE	1
 #define	V_ZERO	0
 
+//Main function.
 int main()
 	{
+		/* Preliminary working variables. */
 		int items = V_ZERO;
 
 		/* Three, two and one dimension dynamic pointers. */
@@ -14,6 +23,7 @@ int main()
 
 		int n_matrix = V_ZERO;	//Number of dynamic matrices to generate.
 
+		/* Header messages. */
 		std::cout << "Highly dynamic pointer generator." << std::endl;
 		std::cout << "How many rectangular arrangements do you want to create? : ";
 		std::cin >> n_matrix;
@@ -21,36 +31,38 @@ int main()
 		std::cout << std::endl << "Memory assigning spaces..." << std::endl;
 
 		/* The initial space is first allocated for the number of two-dimensional arrays to be stored. */
-		matrix = new int** [n_matrix];
-		matrix_rxc = new int* [n_matrix];
-		matrix_r = new int [n_matrix];
+		matrix = new int** [n_matrix];		//Space is generated for the first dimension of a three-dimensional array.
+		matrix_rxc = new int* [n_matrix];	//Space is generated for the first dimension of a two-dimensional array.
+		matrix_r = new int [n_matrix];		//Space is generated for the only dimension of a one-dimensional array.
 
 		/* General loading cycles of dynamic pointer pointers. */
 		std::cout << std::endl << "Capturing values..." << std::endl;
 
-		for (int m = V_ZERO; m < n_matrix; m++)
+		/* Dynamic memory is allocated to know the number of matrices to be generated. */
+		for (int m = V_ZERO; m < n_matrix; m++)		//Cycle by matrix.
 			{
-				std::cout << "For the matrix #[" << m + V_ONE << "] of [" << n_matrix << "]." << std::endl;
+				std::cout << "For the matrix #: [" << m + V_ONE << "] of [" << n_matrix << "]." << std::endl;
 				std::cout << "How many lines will it generate for it? : ";
-				std::cin >> matrix_r[m];
+				std::cin >> matrix_r[m];	//Number of matrices to store.
 
 				/* Dynamic memory is allocated for each matrix to know the number of rows it will have. */
-				matrix[m] = new int* [matrix_r[m]];
+				matrix[m] = new int* [matrix_r[m]];	//Number of rows for each matrix.
 				matrix_rxc[m] = new int [matrix_r[m]];
 
-				for (int r = V_ZERO; r < matrix_r[m]; r++)
+				for (int r = V_ZERO; r < matrix_r[m]; r++)	//Cycle through each row of each matrix.
 					{
-						std::cout << "For the matrix #[" << m + V_ONE << "] of [" << n_matrix << "], and its row #[" << r + V_ONE << "] of [" << matrix_r[m] << "]." << std::endl;
+						std::cout << "For the matrix #: [" << m + V_ONE << "] of [" << n_matrix << "], and its row #: [" << r + V_ONE << "] of [" << matrix_r[m] << "]." << std::endl;
 						std::cout << "How many columns will it generate for it and its respective row? : ";
-						std::cin >> matrix_rxc[m][r];
+						std::cin >> matrix_rxc[m][r];	//Number of rows for each matrix created.
 
 						/* Dynamic memory is allocated for each row of the matrix in concurrent turn to know the number of columns it will have. */
-						matrix[m][r] = new int [matrix_rxc[m][r]];
+						matrix[m][r] = new int [matrix_rxc[m][r]];	//Number of columns per row of each matrix.
 
-						for (int c = V_ZERO; c < matrix_rxc[m][r]; c++)
+						for (int c = V_ZERO; c < matrix_rxc[m][r]; c++)	//Cycle through each column of each row of each matrix.
 							{
-								std::cout << "Enter a value #" << items++ << " for Matrix #[" << m + V_ONE << "] of [" << n_matrix << "] -> (Row: [" << r + V_ONE << "] of [" << matrix_r[m] << "], Column: [" << c + V_ONE << "] of [" << matrix_rxc[m][r] << "]) : ";
-								std::cin >> matrix[m][r][c];
+								std::cout << "Enter a value #: " << items++ << " for Matrix #: [" << m + V_ONE << "] of [" << n_matrix << "] -> (Row: [" << r + V_ONE << "] of [" << matrix_r[m] << "], Column: [" << c + V_ONE << "] of [" << matrix_rxc[m][r] << "]) : ";
+								std::cin >> matrix[m][r][c];	//Value according to number of column per row of each matrix created.
+
 								std::cout << "(Matrix: [" << m + V_ONE << "] of [" << n_matrix << "], Row: [" << r + V_ONE << "] of [" << matrix_r[m] << "], Column: [" << c + V_ONE << "] of [" << matrix_rxc[m][r] << "]) = [" << matrix[m][r][c] << "]." << std::endl;
 							}
 
@@ -61,15 +73,20 @@ int main()
 			}
 		std::cout << "[" << items << "] Captured input values." << std::endl;
 
+		/* Process of dumping each column, each row, each matrix. */
 		std::cout << std::endl << "Displaying captured values..." << std::endl;
+
+		/* Dump of each matrix. */
 		for (int m = V_ZERO; m < n_matrix; m++)
 			{
 				std::cout << "Matrix: [" << m + V_ONE <<"] of [" << n_matrix << "]." << std::endl;
 
+				/* Dump of each row of each matrix. */
 				for (int r = V_ZERO; r < *(matrix_r + m); r++)
 					{
 						std::cout << "\tRow: [" << r + V_ONE << "] of [" << *(matrix_r + m) << "]." << std::endl;
 
+						/* Dump of each column of each row of each matrix. */
 						for (int c = V_ZERO; c < *(*(matrix_rxc + m) + r); c++)
 							{
 								std::cout << "\t\tColumn: [" << c + V_ONE << "] of [" << *(*(matrix_rxc + m) + r) << "].\t";
@@ -83,21 +100,36 @@ int main()
 			}
 		std::cout << "[" << items << "] Generated output results." << std::endl;
 
-		std::cout << "Freeing the memory of all dynamically allocated pointers..." << std::endl;
+		/* The allocated memory is freed from each allocated row of each created array. */
+		std::cout << std::endl << "Freeing the memory of all dynamically allocated pointers..." << std::endl;
+
+		/* Freeing each memory space allocated to each array. */
 		for (int m = V_ZERO; m < n_matrix; m++)
 			{
+				std::cout << "Deallocating memory for array #: [" << m + V_ONE << "] of [" << n_matrix << "]." << std::endl;
+
+				/* Freeing each memory space allocated to each row of each matrix. */
 				for (int r = V_ZERO; r < matrix_r[m]; r++)
 					{
-						delete matrix[m][r];
+						std::cout << "\tDeallocating memory for its row #: [" << r + V_ONE << "] of [" << matrix_r[m] << "]." << std::endl;
+
+						delete matrix[m][r];	//Deallocate each row of each matrix.
 					}
 
-				delete matrix[m];
+				delete matrix[m];	//Deallocate each array.
 				delete matrix_rxc[m];
-			}
-		delete matrix_rxc;
-		delete matrix_r;
-		delete matrix;
 
+				std::cout << std::endl;
+			}
+
+		/* Higher-order pointers are deallocated. */
+		std::cout << "Evicting main memory addresses..." << std::endl;
+
+		delete [] matrix_rxc;
+		delete [] matrix_r;
+		delete [] matrix;
+
+		/* Program closure notice. */
 		std::cout << std::endl << "[End of the assigment!]." << std::endl;
 
 		return V_ZERO;
