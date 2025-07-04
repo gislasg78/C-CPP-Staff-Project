e17a82df42b886ec47562f2bad9fbfd1e125cbe4
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-07-04 15:52:31 -0600

    Mathematical Functions General Project
    The sole purpose of this program is to exemplify the use of derived classes in which there are methods with identical names but different parameters and they need to be converted to access the methods of the derived class and not of the base class.

--- MyPolygons.cpp
+++ MyPolygons.cpp
@@ -3,8 +3,9 @@
 
 #define V_PI    3.1415926
 
-#define V_THREE 3
-#define V_TWO   2
+#define	V_FOUR	4.0
+#define V_THREE 3.0
+#define V_TWO   2.0
 #define V_ZERO  0
 
 class Polygon
@@ -13,80 +14,121 @@ class Polygon
 			int width = V_ZERO, height = V_ZERO;
 
 		public:
-			virtual double area () const       {return V_ZERO;}
-			virtual double perimeter() const   {return V_ZERO;}
+			virtual double area ()			{return V_ZERO;}
+			virtual double perimeter()		{return V_ZERO;}
 
-			void set_values (const double& width, const double& height)
+			void set_values (double width, double height)
 				{this->width = width; this->height = height;}
 	};
 
-class Circle : public Polygon
+class Circle: public Polygon
 	{
 		public:
-			Circle() : Polygon() {}
+			double area(double radius)		{return V_PI * radius * radius;}
+			double perimeter(double radius)		{return V_TWO * V_PI * radius;}
+	};
 
-			virtual double area(const double &radius) const        {return V_PI * radius * radius;}
-			virtual double perimeter(const double &radius) const   {return V_TWO * V_PI * radius;}
+class Diamond: public Polygon
+	{
+		public:
+			double area()				{return (this->width * this->height) / V_TWO;}
+			double perimeter(double side)		{return V_FOUR * side;}
 	};
 
 class Rectangle: public Polygon
 	{
 		public:
-			virtual double area () const       {return this->width * this->height;}
-			virtual double perimeter() const   {return V_TWO * this->width + V_TWO * this->height;}
+			double area ()				{return this->width * this->height;}
+			double perimeter()			{return V_TWO * this->width + V_TWO * this->height;}
+	};
+
+class Square: public Polygon
+	{
+		public:
+			double area(double side)		{return side * side;}
+			double perimeter(double side)		{return V_FOUR * side;}
 	};
 
 class Triangle: public Polygon
 	{
 		public:
-			virtual double area () const       {return (this->width * this->height / V_TWO);}
-			virtual double perimeter(const int& side) const {return V_THREE * side;}
+			double area ()				{return (this->width * this->height / V_TWO);}
+			double perimeter(double side)		{return V_THREE * side;}
 	};
 
 int main()
 	{
 		Polygon poly;
 		Circle circ;
+		Diamond diam;
 		Rectangle rect;
+		Square sqr;
 		Triangle trgl;
 
 		Polygon * ppoly1 = &poly;
 		Polygon * ppoly2 = &circ;
-		Polygon * ppoly3 = &rect;
-		Polygon * ppoly4 = &trgl;
+		Polygon * ppoly3 = &diam;
+		Polygon * ppoly4 = &rect;
+		Polygon * ppoly5 = &sqr;
+		Polygon * ppoly6 = &trgl;
 
 		ppoly1->set_values (V_TWO, V_THREE);
 		ppoly2->set_values (V_THREE, V_TWO);
 		ppoly3->set_values (V_TWO, V_THREE);
+		ppoly4->set_values (V_TWO, V_THREE);
+		ppoly5->set_values (V_THREE, V_TWO);
+		ppoly6->set_values (V_THREE, V_TWO);
 
 		std::cout << "Polygon." << std::endl;
-		std::cout << "Area:\t\t[" << ppoly1->area() << "]." << std::endl;
-		std::cout << "Perimeter:\t[" << ppoly1->perimeter() << "]." << std::endl << std::endl;
+		std::cout << "+ Area:\t\t[" << ppoly1->area() << "]." << std::endl;
+		std::cout << "+ Perimeter:\t[" << ppoly1->perimeter() << "]." << std::endl << std::endl;
 
 		std::cout << "Circle." << std::endl;
-
 		if (Circle *c = dynamic_cast<Circle*>(ppoly2))
 			{
-				std::cout << "Area:\t\t[" << c->area(V_TWO) << "]." << std::endl;
-				std::cout << "Perimeter:\t[" << c->perimeter(V_TWO) << "]." << std::endl << std::endl;
+				std::cout << "+ Area:\t\t[" << c->area(V_TWO) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << c->perimeter(V_TWO) << "]." << std::endl << std::endl;
 			}
 		else
 			{
-				std::cout << "Area:\t\t[" << ppoly2->area() << "]." << std::endl;
-				std::cout << "Perimeter:\t[" << ppoly2->perimeter() << "]." << std::endl << std::endl;
+				std::cout << "- Area:\t\t[" << ppoly2->area() << "]." << std::endl;
+				std::cout << "- Perimeter:\t[" << ppoly2->perimeter() << "]." << std::endl << std::endl;
+			}
+
+		std::cout << "Diamond." << std::endl;
+		if (Diamond *d = dynamic_cast<Diamond*>(ppoly3))
+			{
+				std::cout << "+ Area:\t\t[" << d->area() << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << d->perimeter(V_TWO) << "]." << std::endl << std::endl;
+			}
+		else
+			{
+				std::cout << "- Area:\t\t[" << ppoly3->area() << "]." << std::endl;
+				std::cout << "- Perimeter:\t[" << ppoly3->perimeter() << "]." << std::endl << std::endl;
 			}
 
 		std::cout << "Rectangle." << std::endl;
-		std::cout << "Area:\t\t[" << ppoly3->area() << "]." << std::endl;
-		std::cout << "Perimeter:\t[" << ppoly3->perimeter() << "]." << std::endl << std::endl;
+		std::cout << "+ Area:\t\t[" << ppoly4->area() << "]." << std::endl;
+		std::cout << "+ Perimeter:\t[" << ppoly4->perimeter() << "]." << std::endl << std::endl;
+
+		std::cout << "Square." << std::endl;
+		if (Square *s = dynamic_cast<Square*>(ppoly5))
+			{
+				std::cout << "+ Area:\t\t[" << s->area(V_TWO) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << s->perimeter(V_TWO) << "]." << std::endl << std::endl;
+			}
+		else
+			{
+				std::cout << "- Area:\t\t[" << ppoly5->area() << "]." << std::endl;
+				std::cout << "- Perimeter:\t[" << ppoly5->perimeter() << "]." << std::endl << std::endl;
+			}
 
 		std::cout << "Triangle." << std::endl;
-		std::cout << "Area:\t\t[" << ppoly3->area() << "]." << std::endl;
-
-		if (Triangle *t = dynamic_cast<Triangle*>(ppoly4))
-			std::cout << "Perimeter:\t[" << t->perimeter(V_TWO) << "]." << std::endl << std::endl;
+		std::cout << "+ Area:\t\t[" << ppoly6->area() << "]." << std::endl;
+		if (Triangle *t = dynamic_cast<Triangle*>(ppoly6))
+			std::cout << "+ Perimeter:\t[" << t->perimeter(V_TWO) << "]." << std::endl << std::endl;
 		else
-			std::cout << "Perimeter:\t[" << ppoly4->perimeter() << "]." << std::endl << std::endl;
+			std::cout << "- Perimeter:\t[" << ppoly6->perimeter() << "]." << std::endl << std::endl;
 
 		return V_ZERO;
 	}
