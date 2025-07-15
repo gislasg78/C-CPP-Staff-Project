0f649935af2663c34714c53b742058879f5ec570
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-07-15 16:35:54 -0600

    Mathematical Functions General Project
    This program is intended to exemplify the use of files in C++ and returns as a result the size of a file that is passed to it by user input through the keyboard.

--- FileSize.cpp
+++ FileSize.cpp
@@ -6,14 +6,15 @@
 
 int main()
 	{
+		char *memblock = nullptr;
 		std::string strFileName {};
-		std::streampos begin, end;
+		std::streampos begin{}, end{}, size{};
 
 		std::cout << "Program to determine the size of a given file." << std::endl;
 		std::cout << "Enter a file name: ";
 		std::cin >> strFileName;
 
-		std::ifstream myfile(strFileName, std::ios::binary);
+		std::ifstream myfile(strFileName, std::ios::in | std::ios::binary);
 
 		if (myfile.is_open())
 			{
@@ -23,11 +24,21 @@ int main()
 				myfile.seekg (V_ZERO, std::ios::end);
 				end = myfile.tellg();
 
+				myfile.seekg(V_ZERO, std::ios::end);
+				size = myfile.tellg();
+
+				memblock = new char [size];
+
+				myfile.seekg(V_ZERO, std::ios::beg);
+				myfile.read(memblock, size);
 				myfile.close();
 
 				std::cout << std::endl << "Requested file information." << std::endl;
 				std::cout << "File: [" << strFileName << "]." << std::endl;
-				std::cout << "Size: [" << (end - begin) << "] bytes." << std::endl;
+				std::cout << "Size: [" << (end - begin) << "] = [" << size << "] bytes." << std::endl;
+				std::cout << "The entire file content is in memory." << std::endl;
+
+				delete[] memblock;
 			}
 
 		return V_ZERO;
