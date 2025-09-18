1309c42a52c2184d979534fbc1909b91c7b4e396
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-09-17 21:12:45 -0600

    Mathematical Functions General Project
    This program calculates the perimeter and area of a circle given a radius.

--- Circles.cpp
+++ Circles.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 #define V_PI    3.1415926
+
+#define	V_FIVE	5
+#define	V_FOUR	4
+#define	V_THREE	3
 #define V_TWO   2
 #define V_ZERO  0
 
@@ -26,11 +30,20 @@ class Circle
 			T radius = V_ZERO;
 
 		public:
-			const T& getRadius()	const	{return (*this).radius;}
-			T& getRadius()			{return this->radius;}
+			Circle() : radius(V_ZERO)
+				{
+					std::cout << std::endl << "Data capture for a 'circle'." << std::endl;
+					std::cout << "Enter the radius: ";
+					std::cin >> this->radius;
+				}
 
-			const T getArea()		{return pi() * this->radius * this->radius;}
-			const T getPerimeter()		{return V_TWO * pi.getPi() * this->radius;}
+			Circle(const T radius)				{this->radius = radius;}
+
+			const T& getRadius()	const			{return (*this).radius;}
+			T& getRadius()					{return this->radius;}
+
+			const T getArea()				{return pi() * this->radius * this->radius;}
+			const T getPerimeter()				{return V_TWO * pi.getPi() * this->radius;}
 
 			void setRadius(const T& radius = V_ZERO)	{this->radius = radius;}
 
@@ -48,8 +61,7 @@ int main()
 
 		Pi<double> pi;
 
-		Circle<double> circle;
-		circle.setRadius(radius);
+		Circle<double> circle(radius);
 		circle.getRadius() = radius;
 
 		std::cout << std::endl << "Results of the 'Circle' calculations." << std::endl;
@@ -57,6 +69,38 @@ int main()
 		std::cout << "+ Radius:\t[" << circle.getRadius() << "]." << std::endl;
 		std::cout << "+ Area:\t\t[" << circle.getArea() << "]." << std::endl;
 		std::cout << "+ Perimeter:\t[" << circle.getPerimeter() << "]." << std::endl;
+
+		Circle<float> foo;				//Functional form.
+		Circle<float> bar = V_TWO;			//Assignment init.
+		Circle<float> baz {V_THREE};			//Uniform init.
+		Circle<float> qux = {V_FOUR};			//POD-like
+		Circle<float> quz = Circle<float>(V_FIVE);	//Traditional assignment form.
+
+		std::cout << std::endl << "Outcomes." << std::endl;
+		std::cout << "** foo." << std::endl;
+		std::cout << " + Radius:\t[" << foo.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << foo.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << foo.getPerimeter() << "]." << std::endl;
+
+		std::cout << "** bar." << std::endl;
+		std::cout << " + Radius:\t[" << bar.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << bar.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << bar.getPerimeter() << "]." << std::endl;
+
+		std::cout << "** baz." << std::endl;
+		std::cout << " + Radius:\t[" << baz.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << baz.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << baz.getPerimeter() << "]." << std::endl;
+
+		std::cout << "** qux." << std::endl;
+		std::cout << " + Radius:\t[" << qux.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << qux.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << qux.getPerimeter() << "]." << std::endl;
+
+		std::cout << "** quz." << std::endl;
+		std::cout << " + Radius:\t[" << quz.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << quz.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << quz.getPerimeter() << "]." << std::endl;
 
 		return V_ZERO;
 	}
