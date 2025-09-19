eee3c2174612cff97b2d44f9963351ccd4609f1d
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-09-18 19:36:01 -0600

    Mathematical Functions General Project
    This program calculates the perimeter and area of a circle given a radius.

--- Circles.cpp
+++ Circles.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
 #define V_PI    3.1415926
-
-#define	V_FIVE	5
-#define	V_FOUR	4
-#define	V_THREE	3
-#define V_TWO   2
+#define	V_TWO	2
 #define V_ZERO  0
+
+
+template <typename T> T captureRadius();
 
 template <typename T>
 class Pi
@@ -32,13 +31,12 @@ class Circle
 		public:
 			Circle() : radius(V_ZERO)
 				{
-					std::cout << std::endl << "Data capture for a 'circle'." << std::endl;
-					std::cout << "Enter the radius: ";
-					std::cin >> this->radius;
+					(*this).radius = captureRadius<T>();
 				}
 
-			Circle(const T radius)				{this->radius = radius;}
+			Circle(const T& radius) : radius(radius)	{this->radius = radius;}
 
+			const T  getPi()	const			{return static_cast<T>(pi);}
 			const T& getRadius()	const			{return (*this).radius;}
 			T& getRadius()					{return this->radius;}
 
@@ -50,57 +48,51 @@ class Circle
 			virtual ~Circle() = default;
 	};
 
-
-int main()
+template <typename T>
+T captureRadius()
 	{
-		double radius = V_ZERO;
+		T radius = V_ZERO;
 
-		std::cout << "Perimeter and Area of a 'Circle'." << std::endl;
+		std::cout << std::endl << "Capture the 'Circle' object radius." << std::endl;
 		std::cout << "Enter the value for the radius: ";
 		std::cin >> radius;
 
-		Pi<double> pi;
+		return radius;
+	}
 
-		Circle<double> circle(radius);
-		circle.getRadius() = radius;
-
+template <typename T>
+void infoCircle(Circle<T> &circle)
+	{
 		std::cout << std::endl << "Results of the 'Circle' calculations." << std::endl;
-		std::cout << "+ PI:\t\t[" << static_cast<double>(pi) << "] = [" << pi() << "]." << std::endl;
-		std::cout << "+ Radius:\t[" << circle.getRadius() << "]." << std::endl;
-		std::cout << "+ Area:\t\t[" << circle.getArea() << "]." << std::endl;
-		std::cout << "+ Perimeter:\t[" << circle.getPerimeter() << "]." << std::endl;
+		std::cout << " + PI:\t\t[" << circle.getPi() << "]." << std::endl;
+		std::cout << " + Radius:\t[" << circle.getRadius() << "]." << std::endl;
+		std::cout << " + Area:\t[" << circle.getArea() << "]." << std::endl;
+		std::cout << " + Perimeter:\t[" << circle.getPerimeter() << "]." << std::endl;
+	};
 
-		Circle<float> foo;				//Functional form.
-		Circle<float> bar = V_TWO;			//Assignment init.
-		Circle<float> baz {V_THREE};			//Uniform init.
-		Circle<float> qux = {V_FOUR};			//POD-like
-		Circle<float> quz = Circle<float>(V_FIVE);	//Traditional assignment form.
 
-		std::cout << std::endl << "Outcomes." << std::endl;
-		std::cout << "** foo." << std::endl;
-		std::cout << " + Radius:\t[" << foo.getRadius() << "]." << std::endl;
-		std::cout << " + Area:\t[" << foo.getArea() << "]." << std::endl;
-		std::cout << " + Perimeter:\t[" << foo.getPerimeter() << "]." << std::endl;
+int main()
+	{
+		std::cout << "Creating objects of type 'Circle'." << std::endl;
 
-		std::cout << "** bar." << std::endl;
-		std::cout << " + Radius:\t[" << bar.getRadius() << "]." << std::endl;
-		std::cout << " + Area:\t[" << bar.getArea() << "]." << std::endl;
-		std::cout << " + Perimeter:\t[" << bar.getPerimeter() << "]." << std::endl;
+		Pi<double> pi;
+		Circle<double> circle(captureRadius<double>());
+		infoCircle(circle);
 
-		std::cout << "** baz." << std::endl;
-		std::cout << " + Radius:\t[" << baz.getRadius() << "]." << std::endl;
-		std::cout << " + Area:\t[" << baz.getArea() << "]." << std::endl;
-		std::cout << " + Perimeter:\t[" << baz.getPerimeter() << "]." << std::endl;
+		Circle<double> foo;						//Functional form.
+		infoCircle(foo);
 
-		std::cout << "** qux." << std::endl;
-		std::cout << " + Radius:\t[" << qux.getRadius() << "]." << std::endl;
-		std::cout << " + Area:\t[" << qux.getArea() << "]." << std::endl;
-		std::cout << " + Perimeter:\t[" << qux.getPerimeter() << "]." << std::endl;
+		Circle<double> bar = captureRadius<double>();			//Assignment init.
+		infoCircle(bar);
 
-		std::cout << "** quz." << std::endl;
-		std::cout << " + Radius:\t[" << quz.getRadius() << "]." << std::endl;
-		std::cout << " + Area:\t[" << quz.getArea() << "]." << std::endl;
-		std::cout << " + Perimeter:\t[" << quz.getPerimeter() << "]." << std::endl;
+		Circle<double> baz {captureRadius<double>()};			//Uniform init.
+		infoCircle(baz);
+
+		Circle<double> qux = {captureRadius<double>()};			//POD-like
+		infoCircle(qux);
+
+		Circle<double> quz = Circle<double>(captureRadius<double>());	//Traditional assignment form.
+		infoCircle(quz);
 
 		return V_ZERO;
 	}
