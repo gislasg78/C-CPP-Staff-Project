5cbe73eae8c43aa46b318885b481c881cffffcbc
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-09-22 21:06:46 -0600

    Mathematical Functions General Project
    This program offers a simple on-screen menu for converting between degrees Celsius and Fahrenheit.

--- GradeCnv.cpp
+++ GradeCnv.cpp
@@ -1,44 +1,81 @@
 #include <iostream>
+#include <sstream>
 
+//Working symbolic variables.
 #define V_THIRTY_TWO		32.0
+#define	V_THREE			3
 #define	V_TWO			2
 #define	V_ONE			1
 #define	V_ONE_POINT_EIGHT	1.8
 #define V_ZERO  		0
 
+//Main function.
 int main()
 	{
+		/* Preliminary working variables. */
 		int int_option = V_ZERO;
 		double dbl_result = V_ZERO;
 		double dbl_value = V_ZERO;
 
-		std::cout << "Temperature Conversion." << std::endl;
-		std::cout << "[1]. Celsius to Farenheit." << std::endl;
-		std::cout << "[2]. Farenheit to Celsius." << std::endl;
-		std::cout << "Option: ";
-		std::cin >> int_option;
+		/* Receiving the values ​​as strings of characters. */
+		std::string str_option = {};
+		std::string str_value {};
 
-		switch (int_option)
+		/* Main loop that displays the conversion menu. */
+		while (int_option != V_THREE)
 			{
-				case V_ONE:
-					std::cout << std::endl << "Enter a value in degrees Celsius: ";
-					std::cin >> dbl_value;
+				std::cout << std::endl << "Temperature Conversion." << std::endl;
+				std::cout << "[1]. Celsius to Fahrenheit." << std::endl;
+				std::cout << "[2]. Fahrenheit to Celsius." << std::endl;
+				std::cout << "[3]. Quit this program." << std::endl;
+				std::cout << "Option: ";
+				std::getline(std::cin >> std::ws, str_option);
+				std::stringstream(str_option) >> int_option;
 
-					dbl_result = (V_ONE_POINT_EIGHT * dbl_value) + V_THIRTY_TWO;
-					std::cout << std::endl << "[" << dbl_value << "] Celsius = [" << dbl_result << "] Farenheit." << std::endl;
-					break;
+				std::cout << "Option captured: [" << int_option << "]. OK!" << std::endl;
 
-				case V_TWO:
-					std::cout << std::endl << "Enter a value in degrees Fahrenheit: ";
-					std::cin >> dbl_value;
+				/* Case selection based on the chosen option. */
+				switch (int_option)
+					{
+						case V_ONE:
+							/* Capture the value in degrees Celsius. */
+							std::cout << std::endl << "Enter a value in degrees Celsius: ";
+							std::getline(std::cin >> std::ws, str_value);
+							std::stringstream(str_value) >> dbl_value;
 
-					dbl_result = (dbl_value - V_THIRTY_TWO) / V_ONE_POINT_EIGHT;
-					std::cout << std::endl << "[" << dbl_value << "] Farenheit = [" << dbl_result << "] Celsius." << std::endl;
-					break;
+							/* Formula to convert from 'Celsius' to 'Fahrenheit'. */
+							dbl_result = (V_ONE_POINT_EIGHT * dbl_value) + V_THIRTY_TWO;
 
-				default:
-					std::cout << std::endl << "Invalid option: [" << int_option << "]!" << std::endl;
-					break;
+							/* Outgoing messages. */
+							std::cout << std::endl << "Calculation results' degrees." << std::endl;
+							std::cout << "+ Celsius:\t[" << dbl_value << "]." << std::endl;
+							std::cout << "* Fahrenheit:\t{" << dbl_result << "}." << std::endl;
+							break;
+
+						case V_TWO:
+							/* Capture the value in degrees Fahrenheit. */
+							std::cout << std::endl << "Enter a value in degrees Fahrenheit: ";
+							std::getline(std::cin >> std::ws, str_value);
+							std::stringstream(str_value) >> dbl_value;
+
+							/* Formula to convert from 'Fahrenheit' to 'Celsius'. */
+							dbl_result = (dbl_value - V_THIRTY_TWO) / V_ONE_POINT_EIGHT;
+
+							/* Outgoing messages. */
+							std::cout << std::endl << "Calculation results' degrees." << std::endl;
+							std::cout << "+ Fahrenheit:\t[" << dbl_value << "]." << std::endl;
+							std::cout << "* Celsius:\t{" << dbl_result << "}." << std::endl;
+							break;
+
+						case V_THREE:
+							std::cout << std::endl << "Leaving this program..." << std::endl;
+							break;
+
+						default:
+							std::cout << std::endl << "Invalid option: [" << int_option << "]!" << std::endl;
+							break;
+
+					}
 			}
 
 		return V_ZERO;
