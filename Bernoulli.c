b929c7f982a1a8e577318e9494b13a84f0ad72e2
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-09-25 13:28:11 -0600

    Mathematical Functions General Project
    This program is designed to perform the necessary calculations to obtain Bernoulli numbers up to 29, as larger numbers would overload the processor and require significantly more processing time. Currently, the program uses a self-contained recursive function for these calculations.

--- Bernoulli.c
+++ Bernoulli.c
@@ -1,15 +1,17 @@
 /****************** Bernoulli Number Generator. ******************
- ** Source Code:	Bernoull.c	               		**
+ ** Source Code:	Bernoulli.c				**
  ** Author:		Gustavo Islas Gálvez.			**
- ** Creation Date:	Saturday, December 30, 2023.		**
+ ** Creation Date:	Wednesday, December 31, 2025.		**
  ** Purpose:		This program generates the sequence of	**
  **			Bernoulli numbers attending to		**
  **			iterations and recursion.		**
+ **								**
  **			Up to a maximum of terms are calculated,**
  **			since the storage capacity of the	**
  **			variables is limited when such prominent**
  **			factorial numbers are present.		**
  **			The formula used is:			**
+ **								**
  **			-Sum(from j=0 to m):			**
  **				(m!/((m!-j!)*j!) * (B[j] /	**
  **					(m + 1 - j)).		**
@@ -18,20 +20,20 @@
 #include <stdio.h>
 
 //Working Macros.
-#define	DENOMINATOR(v_n, v_k)	((v_n) - (v_k) + V_ONE_1_0)
+#define	DENOMINATOR(v_n, v_k)	((v_n) - (v_k) + (V_ONE_POINT_ZERO))
 
 //Limit constants for Bernoulli numbers.
-#define	V_LOWER_LIMIT_BERN	1
-#define	V_UPPER_LIMIT_BERN	29
+#define	V_LOWER_LIMIT_BERNOULLI	1
+#define	V_UPPER_LIMIT_BERNOULLI	29
 
 //Numeric Symbolic Constants.
 #define V_MINUS_ONE		-1
 #define V_ONE			1
-#define	V_ONE_1_0		1.0
+#define	V_ONE_POINT_ZERO	1.0
 #define V_ZERO			0
 
 /*****************************************************************
- ** Function:		static double dbl_factorial		**
+ ** Function:		double dbl_factorial			**
  **				(const size_t szt_num).		**
  ** Explanation:	Gets the value of the factorial of any	**
  **			integer by iterativity multiplying	**
@@ -40,13 +42,13 @@
  ** Output Parms:	None.					**
  ** Result:		Factorial from 'i' to 'n'.		**
 *****************************************************************/
-static double dbl_factorial(const size_t szt_num)
+double dbl_factorial(const size_t szt_num)
 	{
 		return (szt_num < V_ONE) ? V_ONE : szt_num * dbl_factorial(szt_num + V_MINUS_ONE);
 	}
 
 /*****************************************************************
- ** Function:		static double dbl_combinations		**
+ ** Function:		double dbl_combinations			**
  **				(const size_t szt_num,		**
  **				 const size_t szt_r).		**
  ** Explanation:	Gets the total number of combinations	**
@@ -60,27 +62,30 @@ static double dbl_factorial(const size_t szt_num)
  ** Output Parms:	None.					**
  ** Result:		nCr = n! / [(n - r)! * r!].		**
 *****************************************************************/
-static double dbl_combinations(const size_t szt_num, const size_t szt_r)
+double dbl_combinations(const size_t szt_num, const size_t szt_r)
 	{
-		return dbl_factorial(szt_num) / (dbl_factorial(szt_num - szt_r) * dbl_factorial(szt_r));
+		return (dbl_factorial(szt_num) / (dbl_factorial(szt_num - szt_r) * dbl_factorial(szt_r)));
 	}
 
 /*****************************************************************
- ** Function:		static double Bernoulli			**
+ ** Function:		double Bernoulli			**
  **				(const size_t szt_num).		**
  ** Explanation:	Returns as the result itself the	**
  **			Bernoulli number requested by the	**
  **			mathematical formula:			**
+ **								**
  **			-Sum(from j=0 to m):                    **
  **                             mCj * (B[j] / (m + 1 - j)).	**
+ **								**
  ** Input Parms:	const size_t szt_num.			**
  ** Output Parms:	None.					**
  ** Result:		Repetitive loops & recursive iterations.**
+ **								**
  **			-Sum(from j=0 to m):			**
  **				(m!/((m!-j!)*j!) * (B[j] /	**
  **					(m + 1 - j)).		**
 *****************************************************************/
-static double Bernoulli(const size_t szt_num)
+double Bernoulli(const size_t szt_num)
 	{
 		double dbl_Bernoulli = V_ZERO;
 
@@ -110,6 +115,7 @@ static double Bernoulli(const size_t szt_num)
  ** Result:		Bernoulli numbers generated with the	**
  **			following iterative and recursive	**
  **			mathematical procedure:			**
+ **								**
  **			-Sum(from j=0 to m):			**
  **				(m!/((m!-j!)*j!) * (B[j] /	**
  **					(m + 1 - j)).		**
@@ -117,28 +123,35 @@ static double Bernoulli(const size_t szt_num)
 int main()
 	{
 		/* Initial declaration of work variables. */
-		size_t szt_nums = V_ZERO;
+		size_t szt_ind = V_ZERO, szt_nums = V_ZERO;
 
-		printf("+---|----+---|----+---|----+---|----+\n");
+		/* Initial header messages. */
+		printf("+===|====+===|====+===|====+===|====+\n");
 		printf("+    Bernoulli Number Generator.    +\n");
-		printf("+---|----+---|----+---|----+---|----+\n");
-		printf("Bernoulli numbers between [%d] and [%d]: ", V_LOWER_LIMIT_BERN, V_UPPER_LIMIT_BERN);
+		printf("+===|====+===|====+===|====+===|====+\n");
+		printf("Bernoulli numbers between [%d] and [%d]: ", V_LOWER_LIMIT_BERNOULLI, V_UPPER_LIMIT_BERNOULLI);
 		scanf("%ld", &szt_nums);
 
-		if (szt_nums >= V_LOWER_LIMIT_BERN && szt_nums <= V_UPPER_LIMIT_BERN)
+		/* Validation condition. */
+		if (szt_nums >= V_LOWER_LIMIT_BERNOULLI && szt_nums <= V_UPPER_LIMIT_BERNOULLI)
 			{
+				/* Final results messages. */
 				printf("\n");
-				printf("+---|----+---|----+---|----+---|----+\n");
+				printf("+===|====+===|====+===|====+===|====+\n");
 				printf("+Bernoulli Number Generator Results.+\n");
+				printf("+===|====+===|====+===|====+===|====+\n");
+				printf("| [%ld] Bernoulli numbers to generate now:\n", szt_nums);
 				printf("+---|----+---|----+---|----+---|----+\n");
-				printf("| [%ld] Bernoulli numbers: \n", szt_nums);
 
 				/* Obtaining preliminary variables. */
 				for (size_t szt_idx = V_ZERO; szt_idx <= szt_nums; szt_idx++)
-					printf("| B# [%2.ld]\t=\t[%16.6lf].\n", szt_idx, Bernoulli(szt_idx));
+					printf("| B# [%2.ld]\t=\t{%16.6lf}.\n", szt_ind++, Bernoulli(szt_idx));
+
+				printf("+---|----+---|----+---|----+---|----+\n");
+				printf("[%ld] Output results generated.\n", szt_ind);
 			}
 		else
-			printf("Error! The number [%ld] is out of range from [%d] to [%d].\n", szt_nums, V_LOWER_LIMIT_BERN, V_UPPER_LIMIT_BERN);
+			printf("Error! The number [%ld] is out of range from: [%d] to [%d].\n", szt_nums, V_LOWER_LIMIT_BERNOULLI, V_UPPER_LIMIT_BERNOULLI);
 
 		return V_ZERO;
 	}
