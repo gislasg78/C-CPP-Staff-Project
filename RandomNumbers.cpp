c9e385d30ea59667e468b936a6a5f22111972551
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-09-27 13:53:51 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to create a base class that can generate random numbers as requested by the programmer or user, returning them either as integers or as floating-point numbers (between 0 and 1), so that they can be used for various purposes.

--- RandomNumbers.cpp
+++ RandomNumbers.cpp
@@ -1,73 +1,90 @@
 /* The purpose of this program is to create a class that returns
    the next element in a given range created by the user. */
 #include <iostream>
+#include <sstream>
 
 /* Symbolic work constants. */
 #define V_ONE   1
 #define V_ZERO  0
 
-//Work Numeric Symbolic Constants.
+/* Random number generator. */
+#define RANDOM_GENERATOR(s)	(((V_MULTIPLIER) * (s) + (V_INCREMENTER)) % (V_MODULUS))
 #define	V_INCREMENTER		13849
 #define V_MODULUS		65536
 #define V_MULTIPLIER		25173
 #define V_RESIDUAL_MODULUS	65535
 
-#define RANDOM_GENERATOR(ml, s, i, md)		((ml * (s) + i) % md)
-
-
+//Base class that generates random numbers based on an initial seed value.
 template <typename T>
 class RandomNumber
 	{
 		private:
-			T incrementer = V_ZERO;
-			T modulus = V_ZERO;
-			T multiplier = V_ZERO;
+			T random_counter = V_ZERO;
 			T random_seed = V_ZERO;
-			T last_number = V_ZERO;
+			T random_number = V_ZERO;
 
 		protected:
 			static int counter;
 
 		public:
-			RandomNumber() : multiplier(V_MULTIPLIER), incrementer(V_INCREMENTER), modulus(V_MODULUS), random_seed(V_ZERO), last_number(random_seed)		{this->counter++; this->capture();}
-			RandomNumber(T &random_seed) : multiplier(V_MULTIPLIER), incrementer(V_INCREMENTER), modulus(V_MODULUS), random_seed(random_seed), last_number(random_seed) {this->counter++;}
-			RandomNumber(T &multiplier, T &incrementer, T modulus, T random_seed) : multiplier(multiplier), incrementer(incrementer), modulus(modulus), random_seed(random_seed), last_number(random_seed) {(*this).counter++;}
-			RandomNumber(RandomNumber &random_number) : multiplier(random_number.getMultiplier()), incrementer(random_number.getIncrementer()), modulus(random_number.getModulus()), random_seed(random_number.getRandomSeed()), last_number(random_number.getRandomSeed()) {this->counter++;}
+			RandomNumber()					{this->counter++; this->capture();}
+			RandomNumber(const T& random_seed) : random_seed(random_seed), random_number(random_seed)
+				{this->counter++; this->random_number = this->newRandom();}
+
+			T operator()()					{return (this->newRandom());}
+			operator T()					{return this->random_number;}
 
 			void capture()
 				{
-					std::cout << std::endl << "Generator of random number series." << std::endl;
-					std::cout << "Multiplier  : "; std::cin >> this->multiplier;
-					std::cout << "Incrementer : "; std::cin >> this->incrementer;
-					std::cout << "Modulus     : "; std::cin >> this->modulus;
-					std::cout << "Random seed : "; std::cin >> this->random_seed;
+					std::string str_random_seed;
+
+					std::cout << std::endl << "Random Number Generator." << std::endl;
+					std::cout << "Enter a random initial seed value: ";
+
+					std::getline(std::cin >> std::ws, str_random_seed);
+					std::stringstream(str_random_seed) >> this->random_seed;
+
+					this->restore();
+					this->random_number = this->newRandom();
 				}
 
-			T getMultiplier()	const		{return this->multiplier;}
-			T getIncrementer()	const		{return this->incrementer;}
-			T getModulus()		const		{return this->modulus;}
-			T getRandomSeed()	const		{return this->random_seed;}
-			T getLastNumber()	const		{return this->last_number;}
+			const T& getNumber()	const			{return (*this).random_number;}
+			const T& getSeed()	const			{return (*this).random_seed;}
+			T& getSeed()					{return this->random_seed;}
 
-			T operator()()				{return this->last_number = RANDOM_GENERATOR(this->multiplier, this->last_number, this->incrementer, this->modulus);}
+			template <typename U = T>
+			typename std::enable_if<std::is_floating_point<T>::value, U>::type
+			getValue()					{return (this->random_number / (T) V_RESIDUAL_MODULUS);}
+
+			template <typename U = T>
+			typename std::enable_if<std::is_integral<T>::value, U>::type
+			&getValue()					{return this->random_number;}
+
+			const T getWithin(const T& minimum, const T& maximum)
+				{
+					return ((static_cast<int>(this->random_number) % (static_cast<int>(maximum) - static_cast<int>(minimum) + V_ONE)) + static_cast<int>(minimum));
+				}
+
+			const T& newRandom()
+				{
+					(*this).random_counter++;
+					return (this->random_number = static_cast<T>(RANDOM_GENERATOR(static_cast<int>(this->random_number))));
+				}
 
 			void print()
 				{
-					std::cout << std::endl << "Current values ​​of the 'RandomNumber' class." << std::endl;
-					std::cout << "Multiplier  :\t[" << this->multiplier << "]." << std::endl;
-					std::cout << "Incrementer :\t[" << this->incrementer << "]." << std::endl;
-					std::cout << "Modulus     :\t[" << this->modulus << "]." << std::endl;
-					std::cout << "Random Seed :\t[" << this->random_seed << "]." << std::endl;
-					std::cout << "Last Number :\t[" << this->last_number << "]." << std::endl;
+					std::cout << std::endl << "Random Number Information." << std::endl;
+					std::cout << "+ Qty Objects:\t[" << this->counter << "]." << std::endl;
+					std::cout << "+ Qty Numbers:\t[" << (*this).random_counter << "]." << std::endl;
+					std::cout << "+ Random Seed:\t[" << (*this).random_seed << "]." << std::endl;
+					std::cout << "+ Last Number:\t[" << (*this).random_number << "] = [" << this->getValue() << "]." << std::endl;
 					std::cout << std::endl;
 				}
 
-			void reset()					{this->multiplier = this->incrementer = this->modulus = this->last_number = V_ZERO;}
+			void reset()					{this->random_counter = V_ZERO; this->random_number = V_ZERO; this->random_seed = V_ZERO;}
+			void restore()					{(*this).random_number = this->random_seed;}
 
-			void setMultiplier(const T &multiplier)		{this->multiplier = multiplier;}
-			void setIncrementer(const T &incrementer)	{this->incrementer = incrementer;}
-			void setModulus(const T &modulus)		{this->modulus = modulus;}
-			void setRandomSeed(const T &random_seed)	{this->random_seed = random_seed;}
+			void setSeed(const T &random_seed = V_ZERO)	{this->random_number = V_ZERO; this->random_seed = random_seed;}
 
 			virtual ~RandomNumber() = default;
 	};
@@ -79,28 +96,27 @@ int RandomNumber<T>::counter = V_ZERO;
 int main()
 	{
 		/* Preliminary working variables. */
-		int idx = V_ZERO, nums = V_ZERO, random_seed = V_ZERO;
+		int counter = V_ZERO, numbers = V_ZERO, random_seed = V_ZERO;
 
 		/* Generate a range of infinite series of numbers. */
 		std::cout << "Generator a range of infinite series of numbers." << std::endl;
 		std::cout << "Number of elements: ";
-		std::cin >> nums;
-		std::cout << "Random Seed Value: ";
+		std::cin >> numbers;
+		std::cout << "Random seed value: ";
 		std::cin >> random_seed;
 
 		/* Create a class with the overloaded 'int' operator to generate series of numbers. */
-		RandomNumber<int> my_random_number(random_seed);
-
-		/* We use the generator to take 'n' given values. */
-		std::cout << "Generator of series of consecutive random numbers." << std::endl;
+		RandomNumber<double> my_random_number(random_seed);
 		my_random_number.print();
 
+		/* We use the generator to take 'n' given values. */
 		std::cout << "List of generated random numbers." << std::endl;
-		for (idx = V_ZERO; idx < nums; idx++)
+		for (int idx = V_ZERO; idx < numbers; idx++)
 			{
-				std::cout << "#: " << idx << "\t[" << my_random_number() << "]." << std::endl;
+				std::cout << "#: [" << counter++ << "]\t:\t[" << my_random_number.getNumber() << "]\t=\t[" << my_random_number.getValue() << "]." << std::endl;
+				my_random_number();	//Activate the generator to get the next number.
 			}
-		std::cout << "[" << idx << "] Output results generated." << std::endl;
+		std::cout << "[" << counter << "] Output results generated." << std::endl;
 
 		my_random_number.print();
 
