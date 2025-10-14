d20f29762d8db393115e1d4f5cce0160f6a8d179
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-10-14 16:13:49 -0600

    Mathematical Functions General Project
    The purpose of this program is to exemplify the creation of 'lambda' functions as well as their calls for a program that calculates polygonal numbers.

--- PointPly.cpp
+++ PointPly.cpp
@@ -10,10 +10,12 @@
 int main()
 	{
 		//Definition of preliminary variables.
+		int int_count = V_ZERO;
 		int int_dimensions = V_ZERO;
 		int int_rings = V_ZERO;
-		std::vector<int> vec_int_numbers;
+		std::vector<int> vect_int_numbers {};
 
+		//Initial headers.
 		std::cout << "Polygonal numbers." << std::endl;
 		std::cout << "Dimensions: ";
 		std::cin >> int_dimensions;
@@ -21,40 +23,39 @@ int main()
 		std::cin >> int_rings;
 
 		//Define a 'lambda' function that calculates each of the polygonal numbers.
-		auto fnPoly = [] (int int_rings, int int_dimensions, std::vector<int> &vec_int_numbers) -> int
+		auto fnPoly = [&] (int int_rings, int int_dimensions, std::vector<int>& vect_int_numbers) -> int
 			{
 				int int_total = V_ONE;
-				vec_int_numbers.clear();
+				vect_int_numbers.clear();
 
-				for (int int_idx = int_rings + V_MINUS_ONE; int_idx >= V_ONE; int_idx-- )
+				for (int int_idx = int_rings + V_MINUS_ONE; int_idx >= V_ONE; int_idx--)
 					{
 						int_total += int_idx * int_dimensions;
-						vec_int_numbers.push_back(int_total);
+						vect_int_numbers.push_back(int_total);
 					}
 
 				return int_total;
 			};
 
 		//Shows the final result of calculating polygonal numbers.
-		std::cout << std::endl << "Final value: [" << fnPoly(int_rings, int_dimensions, vec_int_numbers) << "]." << std::endl;
-
+		std::cout << std::endl << "Final value:\t[" << fnPoly(int_rings, int_dimensions, vect_int_numbers) << "]." << std::endl;
 
 		//Displays on the screen each of the values calculated to reach the last polygonal number.
-		auto printPoly = [] (std::vector<int> &vec_int_numbers)
-			{
-				int int_count = V_ZERO;
+		std::cout << std::endl << "Sequential calculation of polygonal numbers." << std::endl;
 
-				for (std::vector<int>::iterator it_vec_int_numbers = std::begin(vec_int_numbers); it_vec_int_numbers != std::end(vec_int_numbers); it_vec_int_numbers++)
-					std::cout << '\x23' << int_count++ << '\x3a' << '\t' <<'\x5b' << *it_vec_int_numbers << '\x5d' << '\x2e' << '\n';
+		auto printPoly = [&int_count] (const std::vector<int>& vect_int_numbers)
+			{
+				for (std::vector<int>::const_iterator it_vect_int_numbers = std::cbegin(vect_int_numbers); it_vect_int_numbers != std::cend(vect_int_numbers); it_vect_int_numbers++)
+					std::cout << '\x23' << int_count++ << '\x3a' << '\t' <<'\x5b' << *it_vect_int_numbers << '\x5d' << '\x2e' << '\n';
 
 				std::cout << std::endl;
 			};
 
-		printPoly(vec_int_numbers);
-
+		printPoly(vect_int_numbers);
 
 		//Lambda function to display only one element at a time from polygonal number calculation.
-		std::for_each(vec_int_numbers.begin(), vec_int_numbers.end(), [&](int int_Item) { std::cout << '\x5b' << int_Item << '\x5d' << '\x2e' << '\t';});
+		std::cout << "Generated series of polygonal numbers." << std::endl;
+		std::for_each(vect_int_numbers.begin(), vect_int_numbers.end(), [&](const int& int_Item) {std::cout << '\x5b' << int_Item << '\x5d' << '\x2e' << '\t';});
 		std::cout << std::endl;
 
 		return V_ZERO;
