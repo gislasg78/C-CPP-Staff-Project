529b4eb136dca1164e80eb3f6bafb1a74c60c0dc
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-10-21 16:48:16 -0600

    Mathematical Functions General Project
    This program exemplifies the use of pointers whose value is constant and unmodifiable, although pointers can be reassigned or incremented.
    
    The purpose of this program is to create dynamic and static arrays in a generic way with prime numbers in C++ and subsequently add and count their intrinsic elements, using both a static array and a dynamic pointer.

--- PrimeNumberArray.cpp
+++ PrimeNumberArray.cpp
@@ -1,11 +1,47 @@
-//Pointers as arguments.
-#include <iostream>
+/* Generator and counter of dynamic and
+   static arrays of prime numbers. */
 
+/* Standard work libraries. */
+#include <iostream>
+#include <sstream>
+
+//Symbolic work constants.
+#define	V_FIVE	5
+#define	V_SEVEN	7
+#define V_THREE	3
+#define V_TWO	2
 #define V_ZERO  0
+
+
+//Determine whether a given number is prime or not.
+template <typename T>
+bool IsPrime(const T& number)
+	{
+		return (((number == V_TWO) || (number == V_THREE) || (number == V_FIVE) || (number == V_SEVEN)) ||
+			((number % V_TWO) && (number % V_THREE) && (number % V_FIVE) && (number % V_SEVEN)));
+	}
+
+//Generates a dynamic array with prime numbers.
+template <typename T>
+T* create_array(const T& size, const T& last_number, T** array)
+	{
+		T *dynamic_array = new T[size]();
+
+		if (dynamic_array)
+			{
+				for (T idx = V_ZERO, number = last_number; idx < size; number++)
+					if (IsPrime<T>(number))	*(dynamic_array + idx++) = number;
+			}
+		else
+			std::cerr << std::endl << "The reserved memory area could not be properly allocated." << std::endl;
+
+		*array = dynamic_array;
+		return dynamic_array;
+	}
 
 //Cumulative sum of the increased and shifted value.
 template <typename T>
-auto sum_all(const T *start, const T *stop)
+auto sum_array(const T* start, const T* stop)
 	{
 		T sum = V_ZERO;
 
@@ -17,11 +53,11 @@ auto sum_all(const T *start, const T *stop)
 
 //Counting of the values.
 template <typename T>
-T view_all(const T *start, const T *stop)
+T view_array(const T *start, const T *stop)
 	{
 		T count = V_ZERO;
 
-		std::cout << "Dumping the array elements." << std::endl;
+		std::cout << std::endl << "Dumping the array elements." << std::endl;
 
 		while (start != stop)
 			{
@@ -38,16 +74,51 @@ T view_all(const T *start, const T *stop)
 //Main function.
 int main()
 	{
-		int count = V_ZERO, sum = V_ZERO;
+		/* Preliminary working variables. */
 		int array_numbers[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 		const int num_elements = sizeof(array_numbers) / sizeof(*array_numbers);
 
-		count = view_all<int>(array_numbers, array_numbers + num_elements);
-		sum = sum_all<int>(array_numbers, array_numbers + num_elements);
+		int count = V_ZERO, size {V_ZERO}, sum = V_ZERO;
+		std::string str_size {};
 
-		std::cout << std::endl << "Final Statistics." << std::endl;
+		/* Header messages. */
+		std::cout << "Generator of an array with prime numbers." << std::endl;
+		std::cout << "Enter the number of items: ";
+		std::getline(std::cin >> std::ws, str_size);
+		std::stringstream(str_size) >> size;
+
+		/* Code block to create a dynamic array with prime numbers. */
+		int *ptr_array_numbers = create_array<int>(size, V_TWO, &ptr_array_numbers);
+
+		if (ptr_array_numbers)
+			{
+				count = view_array<int>(ptr_array_numbers, ptr_array_numbers + size);
+				sum = sum_array<int>(ptr_array_numbers, ptr_array_numbers + size);
+
+				std::cout << std::endl << "Dynamic Final Statistics." << std::endl;
+				std::cout << "+ Counter:\t[" << count << "]." << std::endl;
+				std::cout << "+ Summation:\t[" << sum << "]." << std::endl;
+
+				delete ptr_array_numbers;
+			}
+		else
+			std::cerr << std::endl << "Error allocating memory for a dynamic array of prime numbers." << std::endl;
+
+		std::cout << std::endl << "Press the ENTER key to continue...";
+		std::cin.get();
+		std::cin.clear();
+
+		/* Code block to read a static array with predefined prime numbers. */
+		count = view_array<int>(array_numbers, array_numbers + num_elements);
+		sum = sum_array<int>(array_numbers, array_numbers + num_elements);
+
+		std::cout << std::endl << "Static Final Statistics." << std::endl;
 		std::cout << "+ Counter:\t[" << count << "]." << std::endl;
 		std::cout << "+ Summation:\t[" << sum << "]." << std::endl;
+
+		std::cout << std::endl << "Press the ENTER key to continue...";
+		std::cin.get();
+		std::cin.clear();
 
 		return V_ZERO;
 	}
