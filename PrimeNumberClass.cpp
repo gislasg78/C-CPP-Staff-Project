b09dfb9d6a6a9ffefa752d0cc0967909832406eb
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-10-20 19:03:13 -0600

    Mathematical Functions General Project
    This program demonstrates the use of a dynamic pointer with deep copying for a class that generates prime numbers from 1 to n. When the size of the pointer is adjusted, the elements from the old array are copied to the new one, and if the new array is larger than the old one, it is filled with the remaining elements.

--- PrimeNumberClass.cpp
+++ PrimeNumberClass.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <sstream>
 
 #define	V_FIVE		5
 #define	V_MINUS_ONE	-1
@@ -10,66 +11,100 @@
 #define	V_ONE		1
 #define V_ZERO  	0
 
+template <class T>
 class DynamicArray
 	{
 		private:
-			size_t *array = nullptr;
-			size_t size = V_ZERO;
+			T *array = nullptr;
+			T size = V_ZERO;
 
 		public:
-			DynamicArray(const size_t& size) : array(new size_t[size]()), size(size)
+			DynamicArray(const T& size) : array(new T[size]()), size(size)
 				{this->fillArray(V_ZERO, size + V_MINUS_ONE, V_TWO);}
 
-			const bool IsPrime(const size_t& number)
+			const bool IsPrime(const T& number)
 				{
 					return (((number == V_TWO) || (number == V_THREE) || (number == V_FIVE) || (number == V_SEVEN)) ||
 						((number % V_TWO) && (number % V_THREE) && (number % V_FIVE) && (number % V_SEVEN)));
 				}
 
-			void fillArray(const size_t& start, const size_t &finish, const size_t &last_number)
+			static const T& enter_a_value(T *const ptr_value)
 				{
-					for (size_t idx = start, number = last_number; idx <= finish; number++)
-						if ((*this).IsPrime(number)) *(this->array + idx++) = number;
+					if (ptr_value)
+						{
+							std::string str_value = {};
+							std::getline(std::cin >> std::ws, str_value);
+							std::stringstream(str_value) >> *ptr_value;
+
+							std::cout << "Value entered:\t[" << *ptr_value << "]. OK!" << std::endl;
+						}
+					else
+						std::cerr << std::endl << "A valid memory address was not provided." << std::endl;
+
+					return *ptr_value;
 				}
 
-			void resize(const size_t& size)
+			const T fillArray(const T& start, const T &finish, const T &last_number)
 				{
-					size_t *array = new size_t[size]();
-					std::copy((*this).array, (*this).array + std::min((*this).size, size), array);
+					T counter = V_ZERO;
+
+					for (T idx = start, number = last_number; idx <= finish; number++)
+						if ((*this).IsPrime(number))
+							{
+								counter++;
+								*(this->array + idx++) = number;
+							}
+
+					return counter;
+				}
+
+			void resize(const T& size)
+				{
+					T *array = new T[size]();
+					T counter = V_ZERO;
+					T maximum = std::max(this->size, size);
+					T minimum = std::min((*this).size, size);
+
+					std::copy((*this).array, (*this).array + minimum, array);
+
+					T last_value = array[minimum + V_MINUS_ONE];
+
 					delete[] this->array;
 
 					this->array = array;
-					(*this).fillArray(std::min(this->size, size), std::max(this->size, size) + V_MINUS_ONE, (*this).array[(*this).size + V_MINUS_ONE] + V_ONE);
 					this->size = size;
+
+					counter = (*this).fillArray(minimum, maximum + V_MINUS_ONE, last_value + V_ONE);
+					std::cout << "[" << counter << "] Output results generated." << std::endl;
 				}
 
 			void show() const
 				{
-					std::cout << std::endl << "Displaying the array elements." << std::endl;
-					std::copy(this->array, this->array + size, std::ostream_iterator<size_t>(std::cout, "\t"));
+					std::cout << std::endl << "Displaying the prime number array." << std::endl;
+					std::copy(this->array, this->array + this->size, std::ostream_iterator<T>(std::cout, "\t"));
 					std::cout << std::endl;
 				}
 
-			~DynamicArray()  {delete[] (*this).array;}
+			~DynamicArray()	{delete[] (*this).array;}
 	};
 
 
 int main()
 	{
-		size_t numbers = V_ZERO;
+		size_t szt_numbers = V_ZERO;
 
 		std::cout << "Automatic adjuster for dynamic settings." << std::endl;
 		std::cout << "Enter the number of items: ";
-		std::cin >> numbers;
+		szt_numbers = DynamicArray<size_t>::enter_a_value(&szt_numbers);
 
-		DynamicArray dyn_array(numbers);
-		dyn_array.show();
+		DynamicArray<size_t> my_dyn_array(szt_numbers);
+		my_dyn_array.show();
 
 		std::cout << std::endl << "Enter the new number of items: ";
-		std::cin >> numbers;
+		szt_numbers = DynamicArray<size_t>::enter_a_value(&szt_numbers);
 
-		dyn_array.resize(numbers);
-		dyn_array.show();
+		my_dyn_array.resize(szt_numbers);
+		my_dyn_array.show();
 
 		return V_ZERO;
 	}
