1a9ca076de0d7dbd83b4cdafe45731bae2eaec5d
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-10-27 21:56:18 -0600

    Mathematical Functions General Project
    The sole purpose of this program is to exemplify the use of derived classes in which there are methods with identical names but different parameters and they need to be converted to access the methods of the derived class and not of the base class.

--- MyPolygons.cpp
+++ MyPolygons.cpp
@@ -1,6 +1,10 @@
-//Virtual members.
+/* This program performs polymorphism of different geometric figures
+   to obtain their respective areas and perimeters. */
+
+/* Standard work libraries. */
 #include <iostream>
 
+/* Symbolic work constants. */
 #define V_PI    3.1415926
 
 #define	V_FOUR	4.0
@@ -8,127 +12,308 @@
 #define V_TWO   2.0
 #define V_ZERO  0
 
-class Polygon
+/* Shape Class. */
+template <class T>
+class Shape
 	{
 		protected:
-			int width = V_ZERO, height = V_ZERO;
+			T area = V_ZERO, perimeter = V_ZERO;
 
 		public:
-			virtual double area ()			{return V_ZERO;}
-			virtual double perimeter()		{return V_ZERO;}
-
-			void set_values (double width, double height)
-				{this->width = width; this->height = height;}
+			virtual const T& getAreaShape() const
+				{return (*this).area;}
+			virtual const T& getPerimeterShape() const
+				{return (*this).perimeter;}
 	};
 
-class Circle: public Polygon
+/* Circle class. */
+template <class T>
+class Circle: public Shape<T>
 	{
 		public:
-			double area(double radius)		{return V_PI * radius * radius;}
-			double perimeter(double radius)		{return V_TWO * V_PI * radius;}
+			const T getArea(const T& radius)
+				{return (*this).area = V_PI * radius * radius;}
+			const T getPerimeter(const T& radius)
+				{return (*this).perimeter = V_TWO * V_PI * radius;}
 	};
 
-class Diamond: public Polygon
+/* Diamond class. */
+template <class T>
+class Diamond: public Shape<T>
 	{
 		public:
-			double area()				{return (this->width * this->height) / V_TWO;}
-			double perimeter(double side)		{return V_FOUR * side;}
+			const T getArea(const T& major_diameter, const T& minor_diameter)
+				{return this->area = (major_diameter * minor_diameter) / V_TWO;}
+			const T getPerimeter(const T &side)
+				{return this->perimeter = V_FOUR * side;}
 	};
 
-class Rectangle: public Polygon
+/* Kite class. */
+template <class T>
+class Kite: public Shape<T>
 	{
 		public:
-			double area ()				{return this->width * this->height;}
-			double perimeter()			{return V_TWO * this->width + V_TWO * this->height;}
+			const T getArea(const T& major_diameter, const T& minor_diameter)
+				{return (*this).area = (major_diameter * minor_diameter) / V_TWO;}
+			const T getPerimeter(const T& top, const T& bottom)
+				{return (*this).perimeter = V_TWO * (bottom + top);}
 	};
 
-class Square: public Polygon
+/* Parallelogram class. */
+template <typename T>
+class Parallelogram: public Shape<T>
 	{
 		public:
-			double area(double side)		{return side * side;}
-			double perimeter(double side)		{return V_FOUR * side;}
+			const T getArea(const T& bottom, const T& height)
+				{return (*this).area = bottom * height;}
+			const T getPerimeter(const T& side, const T& bottom)
+				{return (*this).perimeter = V_TWO * side + V_TWO * bottom;}
 	};
 
-class Triangle: public Polygon
+/* Polygon class. */
+template <class T>
+class Polygon: public Shape<T>
 	{
 		public:
-			double area ()				{return (this->width * this->height / V_TWO);}
-			double perimeter(double side)		{return V_THREE * side;}
+			virtual const T& getArea(const T& number, const T& side, const T& apothegm)
+				{return this->area = ((number * side) * apothegm) / V_TWO;}
+			virtual const T& getPerimeter(const T &number, const T &side)
+				{return (*this).perimeter = number * side;}
 	};
 
+/* Rectangle class. */
+template <class T>
+class Rectangle: public Shape<T>
+	{
+		public:
+			const T getArea(const T& width, const T& height)
+				{return (*this).area = width * height;}
+			const T getPerimeter(const T &width, const T &height)
+				{return (*this).perimeter = V_TWO * height + V_TWO * width;}
+	};
+
+/* Square class. */
+template <class T>
+class Square: public Shape<T>
+	{
+		public:
+			const T getArea(const T &side)
+				{return this->area = side * side;}
+			const T getPerimeter(const T& side)
+				{return this->perimeter = V_FOUR * side;}
+	};
+
+/* Trapeze class. */
+template <class T>
+class Trapeze: public Shape<T>
+	{
+		public:
+			const T getArea(const T& top, const T& bottom, const T& height)
+				{return (*this).area = ((top * bottom) * height) / V_TWO;}
+			const T getPerimeter(const T &top, const T &bottom, const T &left, const T &right)
+				{return (*this).perimeter = top + bottom + left + right;}
+	};
+
+/* Triangle class. */
+template <class T>
+class Triangle: public Shape<T>
+	{
+		public:
+			const T getArea(const T& width, const T& height)
+				{return (*this).area = (width * height / V_TWO);}
+			const T getPerimeter(const T &side)
+				{return (*this).perimeter = V_THREE * side;}
+	};
+
+
+//Main function.
 int main()
 	{
-		Polygon poly;
-		Circle circ;
-		Diamond diam;
-		Rectangle rect;
-		Square sqr;
-		Triangle trgl;
+		/* Preliminary working variables. */
+		double apothegm = V_ZERO, number = V_ZERO;
+		double bottom = V_ZERO, left = V_ZERO, right = V_ZERO, top = V_ZERO;
+		double height = V_ZERO, width = V_ZERO;
+		double major_diameter = V_ZERO, minor_diameter = V_ZERO, side = V_ZERO;
+		double radius = V_ZERO;
 
-		Polygon * ppoly1 = &poly;
-		Polygon * ppoly2 = &circ;
-		Polygon * ppoly3 = &diam;
-		Polygon * ppoly4 = &rect;
-		Polygon * ppoly5 = &sqr;
-		Polygon * ppoly6 = &trgl;
+		/* Initial header message. */
+		std::cout << "Program that calculates areas and perimeters of flat figures." << std::endl << std::endl;
 
-		ppoly1->set_values (V_TWO, V_THREE);
-		ppoly2->set_values (V_THREE, V_TWO);
-		ppoly3->set_values (V_TWO, V_THREE);
-		ppoly4->set_values (V_TWO, V_THREE);
-		ppoly5->set_values (V_THREE, V_TWO);
-		ppoly6->set_values (V_THREE, V_TWO);
 
-		std::cout << "Polygon." << std::endl;
-		std::cout << "+ Area:\t\t[" << ppoly1->area() << "]." << std::endl;
-		std::cout << "+ Perimeter:\t[" << ppoly1->perimeter() << "]." << std::endl << std::endl;
-
+		/* Circle. */
+		Circle<double> circ;
+		Shape<double> *pcirc = &circ;
 		std::cout << "Circle." << std::endl;
-		if (Circle *c = dynamic_cast<Circle*>(ppoly2))
+
+		if (Circle<double> *c = dynamic_cast<Circle<double>*>(pcirc))
 			{
-				std::cout << "+ Area:\t\t[" << c->area(V_TWO) << "]." << std::endl;
-				std::cout << "+ Perimeter:\t[" << c->perimeter(V_TWO) << "]." << std::endl << std::endl;
-			}
-		else
-			{
-				std::cout << "- Area:\t\t[" << ppoly2->area() << "]." << std::endl;
-				std::cout << "- Perimeter:\t[" << ppoly2->perimeter() << "]." << std::endl << std::endl;
+				std::cout << "Enter the radius: ";
+				std::cin >> radius;
+
+				std::cout << std::endl << "[Circle]." << std::endl;
+				std::cout << "+ Area:\t\t[" << c->getArea(radius) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << c->getPerimeter(radius) << "]." << std::endl << std::endl;
 			}
 
+
+		/* Diamond. */
+		Diamond<double> diam;
+		Shape<double> *pdiam = &diam;
 		std::cout << "Diamond." << std::endl;
-		if (Diamond *d = dynamic_cast<Diamond*>(ppoly3))
+
+		if (Diamond<double> *d = dynamic_cast<Diamond<double>*>(pdiam))
 			{
-				std::cout << "+ Area:\t\t[" << d->area() << "]." << std::endl;
-				std::cout << "+ Perimeter:\t[" << d->perimeter(V_TWO) << "]." << std::endl << std::endl;
-			}
-		else
-			{
-				std::cout << "- Area:\t\t[" << ppoly3->area() << "]." << std::endl;
-				std::cout << "- Perimeter:\t[" << ppoly3->perimeter() << "]." << std::endl << std::endl;
+				std::cout << "Enter the major diameter: ";
+				std::cin >> major_diameter;
+				std::cout << "Enter the minor diameter: ";
+				std::cin >> minor_diameter;
+				std::cout << "Enter the side: ";
+				std::cin >> side;
+
+				std::cout << std::endl << "[Diamond]." << std::endl;
+				std::cout << "+ Area:\t\t[" << d->getArea(major_diameter, minor_diameter) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << d->getPerimeter(side) << "]." << std::endl << std::endl;
 			}
 
+
+		/* Kite. */
+		Kite<double> kite;
+		Shape<double> *pkite = &kite;
+		std::cout << "Kite." << std::endl;
+
+		if (Kite<double> *k = dynamic_cast<Kite<double>*>(pkite))
+			{
+				std::cout << "Enter the major diameter: ";
+				std::cin >> major_diameter;
+				std::cout << "Enter the minor diameter: ";
+				std::cin >> minor_diameter;
+				std::cout << "Enter the top: ";
+				std::cin >> top;
+				std::cout << "Enter the bottom: ";
+				std::cin >> bottom;
+
+				std::cout << std::endl << "[Kite]." << std::endl;
+				std::cout << "+ Area:\t\t[" << k->getArea(major_diameter, minor_diameter) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << k->getPerimeter(top, bottom) << "]." << std::endl << std::endl;
+			}
+
+
+		/* Parallelogram. */
+		Parallelogram<double> prll;
+		Shape<double> *pprll = &prll;
+		std::cout << "Parallelogram." << std::endl;
+
+		if (Parallelogram<double> *p = dynamic_cast<Parallelogram<double>*>(pprll))
+			{
+				std::cout << "Enter the bottom: ";
+				std::cin >> bottom;
+				std::cout << "Enter the height: ";
+				std::cin >> height;
+				std::cout << "Enter the side: ";
+				std::cin >> side;
+
+				std::cout << std::endl << "[Parallelogram]." << std::endl;
+				std::cout << "+ Area:\t\t[" << p->getArea(bottom, height) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << p->getPerimeter(side, bottom) << "]." << std::endl << std::endl;
+			}
+
+
+		/* Polygon. */
+		Polygon<double> poly;
+		Shape<double> *ppoly = &poly;
+		std::cout << "Polygon." << std::endl;
+
+		if (Polygon<double> *p = dynamic_cast<Polygon<double>*>(ppoly))
+			{
+				std::cout << "Enter the number sides: ";
+				std::cin >> number;
+				std::cout << "Enter the length of side: ";
+				std::cin >> side;
+				std::cout << "Enter the apothegm: ";
+				std::cin >> apothegm;
+
+				std::cout << std::endl << "[Polygon]." << std::endl;
+				std::cout << "+ Area:\t\t[" << p->getArea(number, side, apothegm) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << p->getPerimeter(number, side) << "]." << std::endl << std::endl;
+			}
+
+
+		/* Rectangle. */
+		Rectangle<double> rect;
+		Shape<double> *prect = &rect;
 		std::cout << "Rectangle." << std::endl;
-		std::cout << "+ Area:\t\t[" << ppoly4->area() << "]." << std::endl;
-		std::cout << "+ Perimeter:\t[" << ppoly4->perimeter() << "]." << std::endl << std::endl;
 
+		if (Rectangle<double> *r = dynamic_cast<Rectangle<double>*>(prect))
+			{
+				std::cout << "Enter the width: ";
+				std::cin >> width;
+				std::cout << "Enter the height: ";
+				std::cin >> height;
+
+				std::cout << std::endl << "[Rectangle]." << std::endl;
+				std::cout << "+ Area:\t\t[" << r->getArea(width, height) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << r->getPerimeter(width, height) << "]." << std::endl << std::endl;
+			}
+
+
+		/* Square. */
+		Square<double> sqr;
+		Shape<double> *psqr = &sqr;
 		std::cout << "Square." << std::endl;
-		if (Square *s = dynamic_cast<Square*>(ppoly5))
+
+		if (Square<double> *s = dynamic_cast<Square<double>*>(psqr))
 			{
-				std::cout << "+ Area:\t\t[" << s->area(V_TWO) << "]." << std::endl;
-				std::cout << "+ Perimeter:\t[" << s->perimeter(V_TWO) << "]." << std::endl << std::endl;
-			}
-		else
-			{
-				std::cout << "- Area:\t\t[" << ppoly5->area() << "]." << std::endl;
-				std::cout << "- Perimeter:\t[" << ppoly5->perimeter() << "]." << std::endl << std::endl;
+				std::cout << "Enter the side: ";
+				std::cin >> side;
+
+				std::cout << std::endl << "[Square]." << std::endl;
+				std::cout << "+ Area:\t\t[" << s->getArea(side) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << s->getPerimeter(side) << "]." << std::endl << std::endl;
 			}
 
+
+		/* Trapeze. */
+		Trapeze<double> trpz;
+		Shape<double> *ptrpz = &trpz;
+		std::cout << "Trapeze." << std::endl;
+
+		if (Trapeze<double> *t = dynamic_cast<Trapeze<double>*>(ptrpz))
+			{
+				std::cout << "Enter the top: ";
+				std::cin >> top;
+				std::cout << "Enter the bottom: ";
+				std::cin >> bottom;
+				std::cout << "Enter the left side: ";
+				std::cin >> left;
+				std::cout << "Enter the right side: ";
+				std::cin >> right;
+				std::cout << "Enter the height: ";
+				std::cin >> height;
+
+				std::cout << std::endl << "[Trapeze]." << std::endl;
+				std::cout << "+ Area:\t\t[" << t->getArea(top, bottom, height) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << t->getPerimeter(top, bottom, left, right) << "]." << std::endl << std::endl;
+			}
+
+
+		/* Triangle. */
+		Triangle<double> trgl;
+		Shape<double> *ptrgl = &trgl;
 		std::cout << "Triangle." << std::endl;
-		std::cout << "+ Area:\t\t[" << ppoly6->area() << "]." << std::endl;
-		if (Triangle *t = dynamic_cast<Triangle*>(ppoly6))
-			std::cout << "+ Perimeter:\t[" << t->perimeter(V_TWO) << "]." << std::endl << std::endl;
-		else
-			std::cout << "- Perimeter:\t[" << ppoly6->perimeter() << "]." << std::endl << std::endl;
+
+		if (Triangle<double> *t = dynamic_cast<Triangle<double>*>(ptrgl))
+			{
+				std::cout << "Enter the width: ";
+				std::cin >> width;
+				std::cout << "Enter the height: ";
+				std::cin >> height;
+				std::cout << "Enter the side: ";
+				std::cin >> side;
+
+				std::cout << std::endl << "[Triangle]." << std::endl;
+				std::cout << "+ Area:\t\t[" << t->getArea(width, height) << "]." << std::endl;
+				std::cout << "+ Perimeter:\t[" << t->getPerimeter(side) << "]." << std::endl;
+			}
 
 		return V_ZERO;
 	}
