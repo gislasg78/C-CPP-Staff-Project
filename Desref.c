fa8b4a8807561c3cdd68fba23d192862ad577d0f
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-10-30 18:08:52 -0600

    Mathematical Functions General Project
    This program simply exemplifies the ways to reference and dereference a structure with its respective members.

--- Desref.c
+++ Desref.c
@@ -1,41 +1,73 @@
 #include <stdio.h>
 
-#define	V_EIGHT	8
-#define	V_FIVE	5
-#define V_FOUR	4
 #define V_ONE	1
-#define	V_SEVEN	7
-#define V_SIX	6
-#define	V_THREE	3
-#define	V_TWO	2
 #define V_ZERO  0
 
-struct str_example
+struct Node
 	{
-		int x, y;
-	} str_ex, *ptr_str_ex = &str_ex;
+		int data;
+		struct Node* rightNode;
+		struct Node* leftNode;
+		struct Node* fatherNode;
+	} my_Node, *ptr_my_Node = &my_Node;
+
+
+void viewNode(struct Node* ptr_my_Node)
+	{
+		printf("\nPointer dereferencing nomenclature.\n");
+		printf("Variable: ptr_my_Node.\n");
+		printf("[%p] : [%p] = [%d], [%p], [%p], [%p].\n", &ptr_my_Node, ptr_my_Node, ptr_my_Node->data, ptr_my_Node->leftNode, ptr_my_Node->rightNode, ptr_my_Node->fatherNode);
+	}
+
+void viewDoubleNode(struct Node** ptr_ptr_my_Node)
+	{
+		printf("\nDouble Pointer Node dereferencing nomenclature.\n");
+		printf("Variable: ptr_ptr_my_Node.\n");
+		printf("[%p] : [%p] : [%p] = [%d], [%p], [%p], [%p].\n", &ptr_ptr_my_Node, ptr_ptr_my_Node, *ptr_ptr_my_Node, (*ptr_ptr_my_Node)->data, (*ptr_ptr_my_Node)->leftNode, (*ptr_ptr_my_Node)->rightNode, (*ptr_ptr_my_Node)->fatherNode);
+	}
+
 
 int main()
 	{
-		str_ex.x = V_ONE;
-		str_ex.y = V_TWO;
+		printf("Node dereferencing nomenclature.\n");
+		printf("Variable: my_Node.\n");
 
-		printf("[%d]\t:\t[%d].\n", ptr_str_ex->x, ptr_str_ex->y);
+		my_Node.data = V_ONE;
+		my_Node.leftNode = NULL;
+		my_Node.rightNode = NULL;
+		my_Node.fatherNode = NULL;
 
-		ptr_str_ex->x = V_THREE;
-		ptr_str_ex->y = V_FOUR;
+		viewNode(&my_Node);
 
-		printf("[%d]\t:\t[%d].\n", str_ex.x, str_ex.y);
+		ptr_my_Node->data = V_ZERO;
+		ptr_my_Node->leftNode = NULL;
+		ptr_my_Node->rightNode = NULL;
+		ptr_my_Node->fatherNode = NULL;
 
-		(&str_ex)->x = V_FIVE;
-		(&str_ex)->y = V_SIX;
+		printf("\nptr_my_Node: [%p] = [%p].\n", &ptr_my_Node, ptr_my_Node);
+		viewNode(ptr_my_Node);
 
-		printf("[%d]\t:\t[%d].\n", ptr_str_ex->x, ptr_str_ex->y);
+		(&my_Node)->data = V_ONE;
+		(&my_Node)->leftNode = NULL;
+		(&my_Node)->rightNode = NULL;
+		(&my_Node)->fatherNode = NULL;
 
-		(*ptr_str_ex).x = V_SEVEN;
-		(*ptr_str_ex).y = V_EIGHT;
+		viewNode(&my_Node);
 
-		printf("[%d]\t:\t[%d].\n", str_ex.x, str_ex.y);
+		(*ptr_my_Node).data = V_ZERO;
+		(*ptr_my_Node).leftNode = NULL;
+		(*ptr_my_Node).rightNode = NULL;
+		(*ptr_my_Node).fatherNode = NULL;
+
+		printf("\nptr_my_Node: [%p] = [%p].\n", &ptr_my_Node, ptr_my_Node);
+		viewNode(ptr_my_Node);
+
+		printf("\nptr_my_Node: [%p] = [%p].\n", &ptr_my_Node, ptr_my_Node);
+		viewDoubleNode(&ptr_my_Node);
+
+		ptr_my_Node = NULL;
+		printf("\nNullifying the pointer of my_Node.\n");
+		printf("ptr_my_Node: [%p] = [%p].\n", &ptr_my_Node, ptr_my_Node);
 
 		return V_ZERO;
 	}
