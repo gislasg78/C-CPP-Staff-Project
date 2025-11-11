91c1e94b598bc115099170324ca75e729ce01351
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-11-11 17:08:33 -0600

    Mathematical Functions General Project
    This program is intended to serve as an example of 'friendship' functions and classes that are allowed access to all attributes, methods, and resources of a class that allows them to do so.

--- MyRectangle.cpp
+++ MyRectangle.cpp
@@ -1,60 +1,138 @@
 //Friend functions.
+#include <algorithm>
 #include <iostream>
+#include <limits>
+#include <sstream>
 
-#define	V_THREE	3
-#define	V_TWO	2
-#define V_ZERO  0
+#define	CARRIAGE_RETURN	'\n'
 
+#define	V_TWO		2
+#define V_ZERO		0
+
+template <typename T>
 class Rectangle
 	{
 		private:
-			int width = V_ZERO, height = V_ZERO;
+			T width = V_ZERO, height = V_ZERO;
 
 		protected:
 			static int counter;
 
 		public:
-			Rectangle() : width(V_ZERO), height(V_ZERO)			{this->counter++;}
-			Rectangle (const int &x, const int &y) : width(x), height(y)	{this->counter++;}
+			Rectangle() : width(V_ZERO), height(V_ZERO)
+				{this->counter++; this->capture();}
+			Rectangle(const T &width, const T &height) : width(width), height(height)
+				{(*this).counter++;}
 
-			double area()		{return this->width * this->height;}
-			double perimeter()	{return V_TWO * this->width + V_TWO * this->height;}
+			Rectangle<T>& operator= (const Rectangle<T>& rectangle)
+				{(*this).copy(rectangle); return *this;}
 
-			friend Rectangle obtain_results	(const Rectangle& rect);
+			void capture()
+				{
+					std::cout << std::endl << "Capturing a 'Rectangle' object." << std::endl;
+					std::cout << "Enter the width: ";
+					this->width = Rectangle<T>::enter_a_value(&this->width);
+					std::cout << "Enter the height: ";
+					(*this).height = Rectangle<T>::enter_a_value(&(*this).height);
+				}
+
+			virtual Rectangle<T>& copy(const Rectangle<T>& rectangle)
+				{
+					(*this).width = rectangle.getWidth();
+					this->height = rectangle.getHeight();
+					return *this;
+				}
+
+			static void enter_a_pause(const std::string& str_Message)
+				{
+					std::cout << str_Message;
+					std::cin.clear();
+					std::cin.get();
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), CARRIAGE_RETURN);
+				}
+
+			static const T& enter_a_value(T *const ptr_value)
+				{
+					if (ptr_value)
+						{
+							std::string str_value {};
+							std::getline(std::cin >> std::ws, str_value);
+							str_value.erase(std::remove_if(str_value.begin(), str_value.end(), ::isspace), str_value.end());
+							std::stringstream(str_value) >> *ptr_value;
+
+							std::cout << "Value entered:\t[" << *ptr_value << "]. OK!" << std::endl;
+						}
+					else
+						std::cerr << std::endl << "A valid memory address was not provided." << std::endl;
+
+					return *ptr_value;
+				}
+
+			const T& getHeight() const		{return this->height;}
+			const T& getWidth() const		{return this->width;}
+
+			T& getHeight()				{return (*this).height;}
+			T& getWidth()				{return (*this).width;}
+
+			T getArea()				{return this->width * this->height;}
+			T getPerimeter()			{return V_TWO * (*this).width + V_TWO * (*this).height;}
+
+			void setHeight(const T& height)		{(*this).height = height;}
+			void setWidth(const T& width)		{(*this).width = width;}
+
+			virtual ~Rectangle() = default;
+
+			template <typename U>
+			friend const Rectangle<U> obtain_results (const Rectangle<U>& rect);
 	};
 
-int Rectangle::counter = V_ZERO;
+template <typename T>
+int Rectangle<T>::counter = V_ZERO;
 
-Rectangle obtain_results (const Rectangle& rect)
+template <typename U>
+const Rectangle<U> obtain_results (const Rectangle<U>& rect)
 	{
-		Rectangle res;
+		Rectangle<U> rec(rect.getWidth(), rect.getHeight());
 
-		res.width = rect.width;
-		res.height = rect.height;
+		std::cout << std::endl << "Rectangle's outcomes." << std::endl;
+		std::cout << "+ Counter:\t[" << rec.counter << "]." << std::endl;
+		std::cout << "+ Width:\t[" << rec.getWidth() << "]." << std::endl;
+		std::cout << "+ Height:\t[" << rec.getHeight() << "]." << std::endl;
+		std::cout << "> Area:\t\t[" << rec.getArea() << "]." << std::endl;
+		std::cout << "> Perimeter:\t[" << rec.getPerimeter() << "]." << std::endl << std::endl;
 
-		std::cout << "Rectangle's outcomes." << std::endl;
-		std::cout << "Counter:\t[" << res.counter << "]." << std::endl;
-		std::cout << "Height:\t\t[" << res.height << "]." << std::endl;
-		std::cout << "Width:\t\t[" << res.width << "]." << std::endl;
-		std::cout << "Area:\t\t[" << res.area() << "]." << std::endl;
-		std::cout << "Perimeter:\t[" << res.perimeter() << "]." << std::endl << std::endl;
-
-		return res;
+		return rec;
 	}
 
-int main ()
+int main()
 	{
-		Rectangle foo;
-		Rectangle bar (V_TWO, V_THREE);
-		Rectangle baz (V_THREE, V_TWO);
+		Rectangle<int> foo;
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
 
-		obtain_results(foo);
-		obtain_results(bar);
-		obtain_results(baz);
+		Rectangle<int> bar;
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
 
-		foo = obtain_results(bar);
-		bar = obtain_results(baz);
-		baz = obtain_results(foo);
+		Rectangle<int> baz;
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		obtain_results<int>(foo);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		obtain_results<int>(bar);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		obtain_results<int>(baz);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		foo = obtain_results<int>(bar);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		bar = obtain_results<int>(baz);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
+
+		baz = obtain_results<int>(foo);
+		Rectangle<int>::enter_a_pause("Press ENTER to continue...");
 
 		return V_ZERO;
 	}
