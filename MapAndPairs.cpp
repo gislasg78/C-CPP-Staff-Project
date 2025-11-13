d573c8c097df10ad49db02af1bf6d466e0024b7d
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-11-13 17:04:31 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to exemplify the use of maps (dictionaries) in the C++ language and to use them in various ways to improve my learning with the STL library containers of the C++ language.

---
+++ MapAndPairs.cpp
@@ -0,0 +1,37 @@
+#include <iomanip>
+#include <iostream>
+#include <map>
+
+#define SPACE           '\x20'
+
+#define V_TWENTY_SIX    26
+#define V_THREE         3
+#define V_TWO           2
+#define V_ZERO          0
+
+int main()
+	{
+		int counter = V_ZERO;
+		std::map<char, int> myMap {};
+		std::cout << "Map and pair insertion exercise." << std::endl;
+
+		for (int idx = V_ZERO; idx < V_TWENTY_SIX; idx++)
+			myMap.insert(std::pair<char, int>('\x41' + idx, 0x41 + idx));
+
+		for (int idx = V_ZERO; idx < V_TWENTY_SIX; idx++)
+			myMap.insert(std::pair<char, int>('\x61' + idx, 0x61 + idx));
+
+		std::pair<std::map<char, int>::iterator, bool> my_map_pair;
+		my_map_pair = myMap.insert(std::pair<char, int>('z', V_TWENTY_SIX));
+
+		if (!my_map_pair.second)
+			std::cout << std::endl << "The element: [" << my_map_pair.first->second << "] was already inserted." << std::endl;
+
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of map." << std::endl;
+		for (std::map<char, int>::iterator it_myMap = myMap.begin(); it_myMap != myMap.end(); it_myMap++)
+			std::cout << "#: " << std::setw(V_TWO) << std::setfill(SPACE) << std::fixed << counter ++ << " = [" << it_myMap->first << "] : {" << std::setw(V_THREE) << std::setfill(SPACE) << std::fixed << it_myMap->second << "}." << std::endl;
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		return V_ZERO;
+	}
