499204b468aafbf9bd8dd0f69f62009803157add
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-11-20 08:20:19 -0600

    Mathematical Functions General Project
    The purpose of this program is to exemplify the use of unordered sets and Hash tables with structures in the C++ language.

--- HashStruct2.cpp
+++ HashStruct2.cpp
@@ -1,8 +1,16 @@
+/* Standard work libraries. */
 #include <iostream>
 #include <unordered_set>
 
-#define V_ZERO  0
+/* Symbolic work constants. */
+#define	V_FIRST_NAME	"Gustavo"
+#define	V_LAST_NAME	"Islas"
 
+#define	V_100_5		100.5
+#define	V_ONE_HUNDRED	100
+#define V_ZERO		0
+
+/* Example of a 'Student' structure. */
 struct Student
 	{
 		int id = V_ZERO;
@@ -14,45 +22,73 @@ struct Student
 		const bool operator==(const Student& other) const
 			{return ((this->id == other.id) && (this->name == other.name) && (this->age == other.age));}
 
-		void print_student() const
+		void print() const
 			{
-				std::cout << "{id =[" << (*this).id << "], name = [" << (*this).name << "], age = [" << (*this).age << "]}." << std::endl;
+				std::cout << "{id:\t[" << (*this).id << "],\tname:\t[" << (*this).name << "],\tage:\t[" << (*this).age << "]}." << std::endl;
 			}
 	};
 
-struct StudentMyHashFunction
+/* Example of a 'Student' structure hash function. */
+struct MyStudentHashFunction
 	{
 		const std::size_t operator()(const Student& other) const
 			{return (std::hash<int>{}(other.id) + std::hash<std::string>{}(other.name) + std::hash<int>{}(other.age));}
 	};
 
+//Main function.
 int main()
 	{
-		std::size_t h1 = std::hash<std::string>{}("Hello");
-		std::size_t h2 = std::hash<std::string>{}("World");
+		/* Preliminary working variables. */
+		// Another testing values: {5, 10, 4, 20, 5, 5, 15, 15, 25, 60, 35, 30, 40, 45, 20, 10, 5, 4}
+		int my_array[] = {12, 7, 11, 10, 13, 3, 3, 20, 1, 16, 9, 4, 14};	//Raw arrangement of integer values.
+		std::size_t h1 = std::hash<std::string>{}(V_FIRST_NAME);
+		std::size_t h2 = std::hash<std::string>{}(V_LAST_NAME);
 
-		std::cout << "Hashing Values." << std::endl;
-		std::cout << "h1 = [" << h1 << "]." << std::endl;
-		std::cout << "h2 = [" << h2 << "]." << std::endl;
-		std::cout << "int = [" << std::hash<int>{}(100) << "]." << std::endl;
-		std::cout << "float = [" << std::hash<float>{}(100.5) << "]." << std::endl;
+		/* Test of values ​​converted to Hash. */
+		std::cout << "Test values ​​for conversion to Hash." << std::endl;
+		std::cout << "+ h1:\t\t[" << h1 << "]." << std::endl;
+		std::cout << "+ h2:\t\t[" << h2 << "]." << std::endl;
+		std::cout << "+ int:\t\t[" << std::hash<int>{}(V_ONE_HUNDRED) << "]." << std::endl;
+		std::cout << "+ float:\t[" << std::hash<float>{}(V_100_5) << "]." << std::endl << std::endl;
 
-		std::unordered_set<int> us = {5, 10, 4, 20, 5, 5, 15, 15, 25, 60, 35, 30, 40, 45, 20, 10, 5, 4};
+		/* Testing values ​​on unsorted numerical sets with a single value. */
+		std::unordered_set<int> us(std::begin(my_array), std::end(my_array));
 
-		std::cout << std::endl << "Set of Numbers." << std::endl;
-		for (int item : us)
+		std::cout << "A set of unordered but unique numerical values." << std::endl;
+		for (const int& item : us)
 			std::cout << "[" << item << "].\t";
 		std::cout << std::endl;
 
-		std::cout << "Size: [" << us.size() << "]." << std::endl;
-		std::cout << "Num buckets: [" << us.bucket_count() << "]." << std::endl;
-		std::cout << "Load factor: [" << us.load_factor() << "]." << std::endl;
+		std::cout << "+ Size: [" << us.size() << "]." << std::endl;
+		std::cout << "+ Num buckets: [" << us.bucket_count() << "]." << std::endl;
+		std::cout << "+ Load factor: [" << us.load_factor() << "]." << std::endl << std::endl;
 
-		std::unordered_set<Student, StudentMyHashFunction> uss = {{5, "Thomas", 50}, {3, "Phillip", 20}, {2, "Andrew", 30}, {7, "John", 40}, {11, "Robert", 60}};
+		/* Testing values ​​on unsorted numerical sets with a non-unique value. */
+		std::unordered_multiset<int> ums(std::begin(my_array), std::end(my_array));
 
-		std::cout << std::endl << "Set of Students." << std::endl;
+		std::cout << "A set of unordered but non-unique numerical values." << std::endl;
+		for (const int& item : ums)
+			std::cout << "[" << item << "].\t";
+		std::cout << std::endl;
+
+		std::cout << "+ Size: [" << ums.size() << "]." << std::endl;
+		std::cout << "+ Num buckets: [" << ums.bucket_count() << "]." << std::endl;
+		std::cout << "+ Load factor: [" << ums.load_factor() << "]." << std::endl << std::endl;
+
+		/* Testing values ​​with an unordered set of unique values ​​using object instances. */
+		std::unordered_set<Student, MyStudentHashFunction> uss = {{5, "Thomas", 50}, {3, "Phillip", 20}, {2, "Andrew", 30}, {7, "Joseph", 40}, {11, "Robert", 60}, {13, "Michael", 10}};
+
+		std::cout << "A set of unordered but unique object instances." << std::endl;
 		for (const Student& item : uss)
-			item.print_student();
+			item.print();
+		std::cout << std::endl;
+
+		/* Testing values ​​with an unordered set of non-unique values ​​using object instances. */
+		std::unordered_multiset<Student, MyStudentHashFunction> umss = {{5, "Thomas", 50}, {3, "Phillip", 20}, {2, "Andrew", 30}, {7, "Joseph", 40}, {11, "Robert", 60}, {13, "Michael", 10}};
+
+		std::cout << "A set of unordered but non-unique object instances." << std::endl;
+		for (const Student& item : umss)
+			item.print();
 
 		return V_ZERO;
 	}
