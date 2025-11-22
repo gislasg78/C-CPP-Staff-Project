d5a5eda05b591de4aff56c3728d3dce18f6faa7b
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-11-22 13:31:38 -0600

    Mathematical Functions General Project
    The primary objective of this program is to exemplify the use of containers, iterators, algorithms, and object functions from the C++ Standard Library (STL) for my own personal practice and to establish that I inevitably possess an intermediate level in this programming language.

--- ContainersSTL1.cpp
+++ ContainersSTL1.cpp
@@ -9,6 +9,8 @@
 #include <queue>
 #include <set>
 #include <stack>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 /* Symbolic work constants. */
@@ -41,15 +43,23 @@ int main()
 		std::priority_queue<int> myPQL = {};
 		std::priority_queue<int, std::vector<int>, std::greater<int>> myPQG {};
 		std::priority_queue<int, std::vector<int>, decltype(fn_cmp)> myPQ(fn_cmp);
-		std::set<int> mySet;
-		std::map<int, int> myMap;
+		std::set<int> myASet; std::set<int, std::greater<int>> myDSet;
+		std::unordered_set<int> myUSet;
+		std::map<int, int> myAMap; std::map<int, int, std::greater<int>> myDMap;
+		std::unordered_map<int, int> myUMap;
 
 		/* Main header message. */
 		std::cout << "C++ STL library containers." << std::endl;
 
-
 		/* Display de array. */
-		std::cout << std::endl << "Dumping of array." << std::endl;
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Array." << std::endl;
+		for (const int& item : myArray)
+			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		counter = V_ZERO;
+		std::cout << std::endl << "Array." << std::endl;
 		for (std::array<int, V_TEN>::const_iterator itc_myArray = myArray.cbegin(); itc_myArray != myArray.cend(); itc_myArray++)
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myArray << "}." << std::endl;
@@ -62,69 +72,63 @@ int main()
 				myPQ.push(*itc_myArray); myPQL.push(*itc_myArray); myPQG.push(*itc_myArray);
 
 				/* Associative containers. */
-				mySet.insert(*itc_myArray);
-				myMap.insert(std::pair<int, int>(counter, *itc_myArray));
+				myDSet.insert(*itc_myArray);
+				myASet.insert(*itc_myArray);
+				myUSet.insert(*itc_myArray);
+				myAMap.insert(std::pair<int, int>(counter, *itc_myArray));
+				myDMap.insert(std::pair<int, int>(counter, *itc_myArray));
+				myUMap.insert(std::pair<int, int>(counter, *itc_myArray));
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
+		/* Display the vector. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Array." << std::endl;
-		for (const int& item : myArray)
+		std::cout << std::endl << "Dumping of Vector." << std::endl;
+		for (const int& item : myVector)
 			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the vector. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of vector." << std::endl;
+		std::cout << std::endl << "Vector." << std::endl;
 		for (std::vector<int>::const_iterator itc_myVec = myVector.cbegin(); itc_myVec != myVector.cend(); itc_myVec++)
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myVec << "}." << std::endl;
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
+		/* Display the list. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Vector." << std::endl;
-		for (const int& item : myVector)
+		std::cout << std::endl << "Dumping of List." << std::endl;
+		for (const int& item : myList)
 			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the list. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of list." << std::endl;
+		std::cout << std::endl << "List." << std::endl;
 		for (std::list<int>::const_iterator itc_myList = myList.cbegin(); itc_myList != myList.cend(); itc_myList++)
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myList << "}." << std::endl;
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
+		/* Display the deque. */
 		counter = V_ZERO;
-		std::cout << std::endl << "List." << std::endl;
-		for (const int& item : myList)
+		std::cout << std::endl << "Dumping of Deque." << std::endl;
+		for (const int& item : myDeq)
 			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the deque. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of deque." << std::endl;
+		std::cout << std::endl << "Deque." << std::endl;
 		for (std::deque<int>::const_iterator itc_myDeq = myDeq.cbegin(); itc_myDeq != myDeq.end(); itc_myDeq++)
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myDeq << "}." << std::endl;
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-		counter = V_ZERO;
-		std::cout << std::endl << "Deque." << std::endl;
-		for (const int& item : myDeq)
-			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
-		std::cout << "[" << counter << "] Output generated results." << std::endl;
-
-
 		/* Display the stack. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of stack." << std::endl;
+		std::cout << std::endl << "Dumping of Stack." << std::endl;
 		while (!myS.empty())
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << myS.top() << "}." << std::endl;
@@ -132,10 +136,9 @@ int main()
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
 		/* Display the queue. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of queue." << std::endl;
+		std::cout << std::endl << "Dumping of Queue." << std::endl;
 		while (!myQ.empty())
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << myQ.front() << "}." << std::endl;
@@ -143,10 +146,9 @@ int main()
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the first priority queue (from highest to lowest order). */
+		/* Display the first Priority Queue (from highest to lowest order). */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of first priority queue (least)." << std::endl;
+		std::cout << std::endl << "Dumping of first Priority Queue (least)." << std::endl;
 		while (!myPQL.empty())
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << myPQL.top() << "}." << std::endl;
@@ -154,10 +156,9 @@ int main()
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the second priority queue (from lowest to highest order). */
+		/* Display the second Priority Queue (from lowest to highest order). */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of second priority queue (greatest)." << std::endl;
+		std::cout << std::endl << "Dumping of second Priority Queue (greatest)." << std::endl;
 		while (!myPQG.empty())
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << myPQG.top() << "}." << std::endl;
@@ -165,10 +166,9 @@ int main()
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the third priority queue (from owner lambda function). */
+		/* Display the third Priority Queue (from owner lambda function). */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of third priority queue (owner)." << std::endl;
+		std::cout << std::endl << "Dumping of third Priority Queue (owner)." << std::endl;
 		while (!myPQ.empty())
 			{
 				std::cout << "#: [" << counter++ << "]\t=\t{" << myPQ.top() << "}." << std::endl;
@@ -176,29 +176,75 @@ int main()
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the set. */
+		/* Display the Set (Ascending). */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of set." << std::endl;
-		for (std::set<int>::const_iterator itc_mySet = mySet.cbegin(); itc_mySet != mySet.cend(); itc_mySet++)
-			{
-				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_mySet << "}." << std::endl;
-			}
+		std::cout << std::endl << "Dumping of Set (Ascending)." << std::endl;
+		for (const int& item : myASet)
+			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
 		counter = V_ZERO;
 		std::cout << std::endl << "Set." << std::endl;
-		for (const int& item : mySet)
+		for (std::set<int>::const_iterator itc_myASet = myASet.cbegin(); itc_myASet != myASet.cend(); itc_myASet++)
+			{
+				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myASet << "}." << std::endl;
+			}
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		/* Display the Set (Descending). */
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Set (Descending)." << std::endl;
+		for (const int& item : myDSet)
 			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
-
-		/* Display the map. */
 		counter = V_ZERO;
-		std::cout << std::endl << "Dumping of map." << std::endl;
-		for (std::map<int, int>::const_iterator itc_myMap = myMap.cbegin(); itc_myMap != myMap.cend(); itc_myMap++)
+		std::cout << std::endl << "Set." << std::endl;
+		for (std::set<int>::const_iterator itc_myDSet = myDSet.cbegin(); itc_myDSet != myDSet.cend(); itc_myDSet++)
 			{
-				std::cout << "#: [" << counter++ << "]\t=\t{" << itc_myMap->first << "} = [" << itc_myMap->second << "]." << std::endl;
+				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myDSet << "}." << std::endl;
+			}
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		/* Display the Unordered Set. */
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Unordered Set." << std::endl;
+		for (const int& item : myUSet)
+			std::cout << "#: [" << counter++ << "]\t=\t{" << item << "}." << std::endl;
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		counter = V_ZERO;
+		std::cout << std::endl << "Unordered Set." << std::endl;
+		for (std::unordered_set<int>::const_iterator itc_myUSet = myUSet.cbegin(); itc_myUSet != myUSet.cend(); itc_myUSet++)
+			{
+				std::cout << "#: [" << counter++ << "]\t=\t{" << *itc_myUSet << "}." << std::endl;
+			}
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		/* Display the Map (Ascending). */
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Map (Ascending)." << std::endl;
+		for (std::map<int, int>::const_iterator itc_myAMap = myAMap.cbegin(); itc_myAMap != myAMap.cend(); itc_myAMap++)
+			{
+				std::cout << "#: [" << counter++ << "]\t=\t{" << itc_myAMap->first << "} = [" << itc_myAMap->second << "]." << std::endl;
+			}
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		/* Display the Map (Descending). */
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Map (Descending)." << std::endl;
+		for (std::map<int, int>::const_iterator itc_myDMap = myDMap.cbegin(); itc_myDMap != myDMap.cend(); itc_myDMap++)
+			{
+				std::cout << "#: [" << counter++ << "]\t=\t{" << itc_myDMap->first << "} = [" << itc_myDMap->second << "]." << std::endl;
+			}
+		std::cout << "[" << counter << "] Output generated results." << std::endl;
+
+		/* Display the Unordered Map. */
+		counter = V_ZERO;
+		std::cout << std::endl << "Dumping of Unordered Map." << std::endl;
+		for (std::unordered_map<int, int>::const_iterator itc_myUMap = myUMap.cbegin(); itc_myUMap != myUMap.cend(); itc_myUMap++)
+			{
+				std::cout << "#: [" << counter++ << "]\t=\t{" << itc_myUMap->first << "} = [" << itc_myUMap->second << "]." << std::endl;
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
