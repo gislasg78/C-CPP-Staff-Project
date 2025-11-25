18b64ebe0f840bcec262474e7b54f7284fa16f79
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-11-24 18:46:33 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to perform two autonomous functions and assign them to threads, which are like timers that will cause both to run simultaneously until their autonomous execution is complete.

--- Threads.cpp
+++ Threads.cpp
@@ -1,37 +1,56 @@
+/* The primary purpose of this program is to perform two autonomous functions
+   and assign them to threads, which are like timers that will cause both
+   to run simultaneously until their autonomous execution is complete. */
+
+/* Libraries Standards of Work. */
 #include <iostream>
 #include <thread>
 
-#define V_ZERO  0
+/* Symbolic work constants. */
+#define	V_ONE_HUNDRED	100
+#define	V_TWO_HUNDRED	200
+#define V_ZERO 		0
 
-//Function to be executed in the first thread.
-void func1()
+/* Function to be executed in the first thread. */
+const int int_function_one(const int& int_loops, const int& int_delay)
 	{
-		for (int int_idx = V_ZERO; int_idx < 200; int_idx++)
+		int counter = V_ZERO;
+
+		for (int int_idx = V_ZERO; int_idx < int_loops; int_idx++)
 			{
-				std::cout << "Thread #1: [" << int_idx << "]." << std::endl;
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));    //One hundred millisecond wait.
+				std::cout << "#1:\t(" << counter++ << ")." << std::endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(int_delay));	//Two hundred millisecond wait.
 			}
+
+		return counter;
 	}
 
-//Function to be executed in the second thread.
-void func2()
+/* Function to be executed in the second thread. */
+const int int_function_two(const int& int_loops, const int& int_delay)
 	{
-		for (int int_idx = V_ZERO; int_idx < 100; int_idx++)
+		int counter = V_ZERO;
+
+		for (int int_idx = V_ZERO; int_idx < int_loops; int_idx++)
 			{
-				std::cout << "Thread #2: [" << int_idx << "]." << std::endl;
-				std::this_thread::sleep_for(std::chrono::milliseconds(200));    //Two hundred millisecond wait.
+				std::cout << "#2:\t[" << counter++ << "]." << std::endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(int_delay));	//One hundred millisecond wait.
 			}
+
+		return counter;
 	}
 
+//Main function.
 int main()
 	{
-		//Create two threads and assign them functions to execute.
-		std::thread thread1(func1);
-		std::thread thread2(func2);
+		std::cout << "Multithreaded testing system in C++." << std::endl;
 
-		//Wait for both created threads to finish their execution.
-		thread1.join();
-		thread2.join();
+		/* Create three threads and assign them functions to execute. */
+		std::thread thread_one(int_function_one, V_ONE_HUNDRED, V_TWO_HUNDRED);
+		std::thread thread_two(int_function_two, V_TWO_HUNDRED, V_ONE_HUNDRED);
+
+		/* Wait for both created threads to finish their execution. */
+		thread_one.join();
+		thread_two.join();
 
 		return V_ZERO;
 	}
