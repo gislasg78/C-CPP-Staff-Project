e5ecf6052a9b2205482861254f98ec350fe52802
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-12-01 15:24:57 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to detect, using maps and stacks, whether a string of characters with series of parentheses, square brackets, angle brackets, or braces is perfectly balanced.

--- BalancedBrackets.cpp
+++ BalancedBrackets.cpp
@@ -1,4 +1,5 @@
 /* Standard work libraries. */
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <stack>
@@ -14,23 +15,34 @@
 #define	V_PARENTHESES_CLOSE	'\x29'
 
 /* Working numerical constants. */
-#define	V_MINUS_FOUR	-4
-#define	V_MINUS_ONE	-1
-#define	V_MINUS_THREE	-3
-#define	V_MINUS_TWO	-2
-#define	V_FOUR		4
-#define V_ONE		1
-#define	V_THREE		3
-#define	V_TWO		2
-#define V_ZERO		0
+#define	V_MINUS_FOUR		-4
+#define	V_MINUS_ONE		-1
+#define	V_MINUS_THREE		-3
+#define	V_MINUS_TWO		-2
+#define	V_FOUR			4
+#define V_ONE			1
+#define	V_THREE			3
+#define	V_TWO			2
+#define V_ZERO			0
 
 /* Function that balances parentheses, brackets, braces, and angle brackets. */
-const bool IsBalanced(const std::string& s)
+const bool IsBalanced(const std::string& str_data)
 	{
-		std::map<char, int> symbols = {{V_PARENTHESES_OPEN, V_MINUS_FOUR}, {V_ANGLE_BRACKETS_OPEN, V_MINUS_THREE}, {V_BRACKETS_OPEN, V_MINUS_TWO}, {V_BRACES_OPEN, V_MINUS_ONE}, {V_PARENTHESES_CLOSE, V_FOUR}, {V_ANGLE_BRACKETS_CLOSE, V_THREE}, {V_BRACKETS_CLOSE, V_TWO}, {V_BRACES_CLOSE, V_ONE}};
-		std::stack<int> st;
+		/* Map that stores the types of parentheses to be analyzed. */
+		std::map<char, int> symbols =	{
+							{V_PARENTHESES_OPEN, V_MINUS_FOUR},
+							{V_ANGLE_BRACKETS_OPEN, V_MINUS_THREE},
+							{V_BRACKETS_OPEN, V_MINUS_TWO},
+							{V_BRACES_OPEN, V_MINUS_ONE},
+							{V_PARENTHESES_CLOSE, V_FOUR},
+							{V_ANGLE_BRACKETS_CLOSE, V_THREE},
+							{V_BRACKETS_CLOSE, V_TWO},
+							{V_BRACES_CLOSE, V_ONE}
+						};
+		std::stack<int> st;	//A stack to store the last type of opening parenthesis.
 
-		for (const char& bracket : s)
+		/* Loop that is responsible for checking the string of characters to be analyzed character by character. */
+		for (const char& bracket : str_data)
 			{
 				if (symbols[bracket] < V_ZERO)
 					{
@@ -38,17 +50,12 @@ const bool IsBalanced(const std::string& s)
 					}
 				else
 					{
-						if (st.empty()) return false;
-
-						char top = st.top();
+						if ((st.empty()) || (symbols[st.top()] + symbols[bracket])) return false;
 						st.pop();
-
-						if (symbols[top] + symbols[bracket]) return false;
 					}
 			}
 
-		if (st.empty()) return true;
-		return false;
+		return st.empty();
 	}
 
 //Main function.
@@ -57,19 +64,21 @@ int main()
 		/* Preliminary working variables. */
 		int counter = V_ZERO, numbers = V_ZERO;
 
+		/* Initial header messages. */
 		std::cout << "Balanced Brackets Matching." << std::endl;
 		std::cout << "Number of values: ";
 		std::cin >> numbers;
 
 		/* Introduction of each string of characters with parentheses. */
-		std::cout << std::endl << "Capturing character strings with series of parentheses." << std::endl;
+		std::cout << std::endl << "Capturing character strings with parentheses." << std::endl;
 		for (int idx = V_ZERO; idx < numbers; idx++)
 			{
-				std::string s;
+				std::string str_value {};
 				std::cout << "Enter the string #: [" << counter++ + V_ONE << "] of: [" << numbers << "]: ";
-				std::cin >> s;
+				std::getline(std::cin >> std::ws, str_value);
+				str_value.erase(std::remove_if(str_value.begin(), str_value.end(), ::isspace), str_value.end());
 
-				std::cout << "[" << std::boolalpha << IsBalanced(s) << "]." << std::endl;
+				std::cout << "[" << std::boolalpha << IsBalanced(str_value) << "]." << std::endl;
 			}
 		std::cout << "[" << counter << "] Output generated results." << std::endl;
 
