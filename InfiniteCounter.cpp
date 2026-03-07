a41aef209d78ccc495e52b45f32259efdbcda621
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-03-07 10:46:22 -0600

    Mathematical Functions General Project
    The purpose of this program is to create a class that returns the next element in a given range created by the user.

--- InfiniteCounter.cpp
+++ InfiniteCounter.cpp
@@ -6,22 +6,26 @@
 #define V_ONE   1
 #define V_ZERO  0
 
+/* Example class of an infinite counter. */
 class InfiniteCounter
 	{
 		private:
 			int start = V_ZERO;
 			int step = V_ZERO;
+			int stop = V_ZERO;
+			int value = start;
 
 		protected:
 			static int counter;
 
 		public:
 			InfiniteCounter()		{this->counter++; this->capture();}
-			InfiniteCounter(int &start, int &step) : start(start), step(step) {}
+			InfiniteCounter(int &start, int &step, int &stop) : start(start), step(step), stop(stop) {}
 			InfiniteCounter(InfiniteCounter &infinitecounterobject)
 				{
-					this->start = infinitecounterobject.start;
-					this->step = infinitecounterobject.step;
+					this->start = infinitecounterobject.getStart();
+					this->step = infinitecounterobject.getStep();
+					this->stop = infinitecounterobject.getStop();
 				}
 
 			virtual void capture()
@@ -32,26 +36,41 @@ class InfiniteCounter
 					std::cin >> this->start;
 					std::cout << "Step  value: ";
 					std::cin >> this->step;
+					std::cout << "Stop  value: ";
+					std::cin >> this->stop;
 				}
 
-			int getStart()	const		{return this->start;}
-			int getStep()	const		{return this->step;}
+			const int& getStart()	const	{return this->start;}
+			int& getStart()			{return this->start;}
 
-			int operator()()		{return this->start += this->step;}
+			const int& getStep()	const	{return this->step;}
+			int& getStep()			{return this->step;}
+
+			const int& getStop()	const	{return this->stop;}
+			int& getStop()			{return this->stop;}
+
+			const int& getValue()	const	{return (*this).value;}
+			int& getValue()			{return (*this).value;}
+
+			int operator()()
+				{return (this->value >= this->start && this->value < this->stop) ? this->value += this->step : (*this).value = (*this).start + V_ONE;}
 
 			virtual void print()
 				{
 					std::cout << std::endl;
 					std::cout << "Current values ​​of the 'InfiniteCounter' class." << std::endl;
-					std::cout << "Start:\t[" << this->start << "]." << std::endl;
-					std::cout << "Step:\t[" << this->step << "]." << std::endl;
+					std::cout << "+ Start:\t[" << this->start << "]." << std::endl;
+					std::cout << "+ Step:\t\t[" << this->step << "]." << std::endl;
+					std::cout << "+ Stop:\t\t[" << this->stop << "]." << std::endl;
+					std::cout << "+ Value:\t[" << (*this).value << "]." << std::endl;
 					std::cout << std::endl;
 				}
 
-			virtual void reset()		{this->start = V_ZERO; this->step = V_ONE;}
+			virtual void reset()		{this->start = this->stop = this->value = V_ZERO; this->step = V_ONE;}
 
 			void setStart(const int &start)	{this->start = start;}
 			void setStep(const int &step)	{this->step = step;}
+			void setStop(const int &stop)	{this->stop = stop;}
 
 			virtual ~InfiniteCounter() = default;
 	};
@@ -62,7 +81,7 @@ int InfiniteCounter::counter = V_ZERO;
 int main()
 	{
 		/* Preliminary working variables. */
-		int nums = V_ZERO, start = V_ZERO, step = V_ZERO;
+		int nums = V_ZERO, start = V_ZERO, step = V_ZERO, stop = V_ZERO;
 
 		/* Generate a range of infinite series of numbers. */
 		std::cout << "Generator a range of infinite series of numbers." << std::endl;
@@ -72,12 +91,14 @@ int main()
 		std::cin >> start;
 		std::cout << "Interval between each element: ";
 		std::cin >> step;
+		std::cout << "Stopping point value: ";
+		std::cin >> stop;
 
 		/* Create a class with the overloaded 'int' operator to generate series of numbers. */
-		InfiniteCounter my_counter(start, step);
+		InfiniteCounter my_counter(start, step, stop);
 
 		/* We use the generator to take 'n' given values. */
-		std::cout << std::endl << "Generator of series of consecutive numbers according to their interval." << std::endl;
+		std::cout << "Generator of series of consecutive numbers according to their interval." << std::endl;
 
 		my_counter.print();
 		for (int idx = V_ZERO; idx < nums; idx++)
@@ -89,6 +110,9 @@ int main()
 		/* We use the generator to take 'n' given values. */
 		std::cout << std::endl << "Generator of series of consecutive numbers according to a predetermined interval." << std::endl;
 		my_counter.reset();
+		my_counter.setStart(start);
+		my_counter.setStep(step);
+		my_counter.getStop() = stop;
 		my_counter.print();
 
 		for (int idx = V_ZERO; idx < nums; idx++)
