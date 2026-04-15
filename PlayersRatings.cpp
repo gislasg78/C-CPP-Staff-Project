d8fd4159144db8fa95005010d7131624f3424ca3
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-04-15 16:24:14 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to use some of the most important functions of the C++ STL library for my personal practice. This program creates three doubly linked lists, generating the first with random numbers from one to ten. These lists are then placed in two lists: one with numbers from one to five, and the second with numbers from six to ten. This example is just a glimpse into my advanced C++ learning process.

--- PlayersRatings.cpp
+++ PlayersRatings.cpp
@@ -1,4 +1,10 @@
+/* This program is responsible for separating a linked list
+   into two lists that have values ​​within ranges specified
+   by the programmer. */
+
+/* Standard work libraries. */
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <iterator>
 #include <limits>
@@ -6,6 +12,7 @@
 #include <random>
 #include <sstream>
 
+/* Symbolic work constants. */
 #define	CARRIAGE_RETURN	'\n'
 
 #define	V_FIVE		5
@@ -14,7 +21,7 @@
 #define	V_TEN		10
 #define	V_ZERO		0
 
-
+/* Iterator to sum the elements obtained from the list. */
 template <typename Iter>
 typename std::iterator_traits<Iter>::value_type addRatings(Iter start, Iter finish)
 	{
@@ -28,29 +35,30 @@ typename std::iterator_traits<Iter>::value_type addRatings(Iter start, Iter fini
 		return addition;
 	}
 
+/* Display the values ​​corresponding to a given list. */
 template <typename T>
 T displayRatings(const std::string& strHeaderMsgList, const std::list<T>& playersRatings)
 	{
-		T count = V_ZERO;
+		T count = V_ZERO, sum = V_ZERO;
 
 		std::cout << std::endl << "Displaying the list elements." << std::endl;
 
-		auto fnDisplayItem = [&count] (const T& item) -> T
+		auto fnDisplayItem = [&count](const T& item) -> T
 			{count++; std::cout << item << '\t'; return item;};
 
 		std::cout << "[" << strHeaderMsgList << "]." << std::endl;
 		std::for_each(playersRatings.cbegin(), playersRatings.cend(), fnDisplayItem);
 
-		std::cout << std::endl << "Sum:\t[" << addRatings(playersRatings.begin(), playersRatings.end()) << "]." << std::endl;
-		std::cout << "[" << count << "] Output results generated." << std::endl;
+		sum = std::accumulate(playersRatings.cbegin(), playersRatings.cend(), V_ZERO);
 
-		std::cin.get();
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), CARRIAGE_RETURN);
+		std::cout << std::endl << "+ Accumulate:\t[" << sum << "]." << std::endl;
+		std::cout << "+ Sum:\t\t[" << addRatings(playersRatings.cbegin(), playersRatings.cend()) << "]." << std::endl;
+		std::cout << "[" << count << "] Output results generated." << std::endl;
 
 		return count;
 	}
 
+/* Fill a given list with specific values. */
 template <typename T>
 T fillRatings(std::list<T>& playersRatings, const T& lower_bound, const T& upper_bound)
 	{
@@ -70,22 +78,48 @@ T fillRatings(std::list<T>& playersRatings, const T& lower_bound, const T& upper
 		return count;
 	}
 
+/* Get a simple pause from the program. */
+void getPause(const std::string& str_Message)
+	{
+		std::cout << str_Message;
+		std::cin.clear();
+		std::cin.get();
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), CARRIAGE_RETURN);
+	}
+
+/* Capture the number of values ​​to generate. */
 template <typename T>
 T obtainValue(const std::string& str_Message, T* data_value)
 	{
 		T t_data_value = V_ZERO;
-		std::string str_data_value = {};
+		std::string str_data_value = std::string();
 
-		std::cout << "Enter the data number: ";
+		std::function<bool(const std::string&)> IsNumeric = [&](const std::string& str_value) -> bool
+			{
+				if (str_value.empty()) return false;
+
+				return std::all_of(std::begin(str_value), std::end(str_value), [&](const unsigned char& ch)
+					{return std::isdigit(ch);});
+			};
+
+		std::cout << str_Message;
 		std::getline(std::cin >> std::ws, str_data_value);
+
+		if (IsNumeric(str_data_value))
+			std::cout << std::endl << "The value entered is numeric." << std::endl;
+		else
+			std::cout << std::endl << "The value entered is not numeric." << std::endl;
+
 		std::stringstream(str_data_value) >> t_data_value;
 
 		*data_value = t_data_value;
 		return t_data_value;
 	}
 
+/* Separate the elements of a given list into two separate lists. */
 template <typename T>
-T separateRatings(const std::list<T>& allPlayers, std::list<T> &beginners, std::list<T> &pros)
+T separateRatings(const std::list<T>& allPlayers, std::list<T> &beginners, std::list<T> &pros, const T& lower_limit_beginners, const T& upper_limit_beginners, const T& lower_limit_pros, const T& upper_limit_pros)
 	{
 		T count = V_ZERO;
 
@@ -93,17 +127,18 @@ T separateRatings(const std::list<T>& allPlayers, std::list<T> &beginners, std::
 		std::copy(std::begin(allPlayers), std::end(allPlayers), std::ostream_iterator<T>(std::cout, "\t"));
 		std::cout << std::endl;
 
-		for (std::list<int>::const_iterator c_it_allPlayers = allPlayers.cbegin(); c_it_allPlayers != allPlayers.cend(); c_it_allPlayers++, count++)
-			{
-				if (*c_it_allPlayers >= V_ONE && *c_it_allPlayers <= V_FIVE)
-					beginners.push_back(*c_it_allPlayers);
-				else if (*c_it_allPlayers >= V_SIX && *c_it_allPlayers <= V_TEN)
-					pros.push_back(*c_it_allPlayers);
-			}
+		std::copy_if(allPlayers.cbegin(), allPlayers.cend(), std::back_inserter(beginners),
+			[&count, &lower_limit_beginners, &upper_limit_beginners](const T& value)
+			{count++; return (value >= lower_limit_beginners && value <= upper_limit_beginners);});
+
+		std::copy_if(allPlayers.cbegin(), allPlayers.cend(), std::back_inserter(pros),
+			[&count, &lower_limit_pros, &upper_limit_pros](const T& value)
+			{count++; return (value >= lower_limit_pros && value <= upper_limit_pros);});
 
 		return count;
 	}
 
+//Main function.
 int main()
 	{
 		int int_count {V_ZERO};
@@ -117,11 +152,22 @@ int main()
 		std::list<int> allPlayers(int_count);
 
 		int_count = fillRatings<int>(allPlayers, V_ONE, V_TEN);
-		int_count = separateRatings<int>(allPlayers, beginners, pros);
+		getPause("Press the ENTER key to continue...");
+
+		int_count = separateRatings<int>(allPlayers, beginners, pros, V_ONE, V_FIVE, V_SIX, V_TEN);
+		getPause("Press the ENTER key to continue...");
 
 		int_count = displayRatings<int>("All Players", allPlayers);
+		getPause("Press the ENTER key to continue...");
+
 		int_count = displayRatings<int>("Beginners", beginners);
+		getPause("Press the ENTER key to continue...");
+
 		int_count = displayRatings<int>("Pros", pros);
+		getPause("Press the ENTER key to continue...");
+
+		std::cout << std::endl << "Done!" << std::endl;
+		std::cout << "This program has ended." << std::endl;
 
 		return V_ZERO;
 	}
