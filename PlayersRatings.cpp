1f53a98faf66d937db291b0d37a5abe65e370390
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-04-16 16:25:29 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to use some of the most important functions of the C++ STL library for my personal practice. This program creates three doubly linked lists, generating the first with random numbers from one to ten. These lists are then placed in two lists: one with numbers from one to five, and the second with numbers from six to ten. This example is just a glimpse into my advanced C++ learning process.

--- PlayersRatings.cpp
+++ PlayersRatings.cpp
@@ -37,6 +37,7 @@ typename std::iterator_traits<Iter>::value_type addRatings(Iter start, Iter fini
 template <typename T>
 T displayRatings(const std::string& strHeaderMsgList, const std::list<T>& playersRatings)
 	{
+		/* Preliminary working variables. */
 		T count = V_ZERO, sum = V_ZERO;
 
 		std::cout << std::endl << "Displaying the list elements." << std::endl;
@@ -47,6 +48,7 @@ T displayRatings(const std::string& strHeaderMsgList, const std::list<T>& player
 		std::cout << "[" << strHeaderMsgList << "]." << std::endl;
 		std::for_each(playersRatings.cbegin(), playersRatings.cend(), fnDisplayItem);
 
+		/* Algorithms prepared to accumulate the container values. */
 		sum = std::accumulate(playersRatings.cbegin(), playersRatings.cend(), V_ZERO);
 
 		std::cout << std::endl << "+ Accumulate:\t[" << sum << "]." << std::endl;
@@ -58,7 +60,7 @@ T displayRatings(const std::string& strHeaderMsgList, const std::list<T>& player
 
 /* Fill a given list with specific values. */
 template <typename T>
-T fillRatings(std::list<T>& playersRatings, T& minimum, T& maximum)
+std::list<T> fillRatings(std::list<T>& playersRatings, T& minimum, T& maximum)
 	{
 		T count = V_ZERO;
 
@@ -66,19 +68,21 @@ T fillRatings(std::list<T>& playersRatings, T& minimum, T& maximum)
 		if ((minimum > maximum) || (maximum < minimum))
 			{swap(&minimum, &maximum);}
 
+		/* Automatic random number generation process. */
 		std::random_device myRandomDevice;
 		std::mt19937 myGenerator(myRandomDevice());
 		std::uniform_int_distribution<> myDistribution(minimum, maximum);
-
 		std::generate(playersRatings.begin(), playersRatings.end(), [&](){return myDistribution(myGenerator);});
 
+		playersRatings.sort();
 		count = static_cast<T>(std::count_if(std::begin(playersRatings), std::end(playersRatings), [&](const T& value){return value;}));
 
+		/* Display the elements generated with random values. */
 		std::cout << std::endl << "Filling the list elements." << std::endl;
 		std::copy(playersRatings.cbegin(), playersRatings.cend(), std::ostream_iterator<T>(std::cout, "\t"));
 		std::cout << std::endl << "[" << count << "] Output results generated." << std::endl;
 
-		return count;
+		return playersRatings;
 	}
 
 /* Get a simple pause from the program. */
@@ -95,9 +99,11 @@ void getPause(const std::string& str_Message)
 template <typename T>
 T obtainValue(const std::string& str_Message, T* data_value)
 	{
+		/* Preliminary working variables. */
 		T t_data_value = V_ZERO;
 		std::string str_data_value = std::string();
 
+		/* Function to validate a string of characters as numeric. */
 		std::function<bool(const std::string&)> IsNumeric = [&](const std::string& str_value) -> bool
 			{
 				if (str_value.empty()) return false;
@@ -109,10 +115,11 @@ T obtainValue(const std::string& str_Message, T* data_value)
 		std::cout << str_Message;
 		std::getline(std::cin >> std::ws, str_data_value);
 
+		/* A string of characters is validated as numeric. */
 		if (IsNumeric(str_data_value))
-			std::cout << std::endl << "The value entered is numeric." << std::endl;
+			std::cout << "The value entered is numeric." << std::endl;
 		else
-			std::cout << std::endl << "The value entered is not numeric." << std::endl;
+			std::cout << "The value entered is not numeric." << std::endl;
 
 		std::stringstream(str_data_value) >> t_data_value;
 
@@ -122,7 +129,7 @@ T obtainValue(const std::string& str_Message, T* data_value)
 
 /* Separate the elements of a given list into two separate lists. */
 template <typename T>
-std::list<T> separateRatings(const std::list<T>& allPlayers, std::list<T> &playersRating, T& minimum, T& maximum)
+std::list<T> separateRatings(const std::list<T>& allPlayers, std::list<T> &playersRatings, T& minimum, T& maximum)
 	{
 		T count = V_ZERO;
 
@@ -133,14 +140,16 @@ std::list<T> separateRatings(const std::list<T>& allPlayers, std::list<T> &playe
 		std::cout << std::endl << "Separating the list elements." << std::endl;
 		std::cout << "Range between: [" << minimum << "] and: [" << maximum << "]." << std::endl;
 
-		std::copy_if(allPlayers.cbegin(), allPlayers.cend(), std::back_inserter(playersRating),
+		std::copy_if(allPlayers.cbegin(), allPlayers.cend(), std::back_inserter(playersRatings),
 			[&count, &minimum, &maximum](const T& value)
 			{return (value >= minimum && value <= maximum) ? ++count : V_ZERO;});
 
-		std::copy(std::begin(playersRating), std::end(playersRating), std::ostream_iterator<T>(std::cout, "\t"));
+		playersRatings.sort();
+
+		std::copy(std::begin(playersRatings), std::end(playersRatings), std::ostream_iterator<T>(std::cout, "\t"));
 		std::cout << std::endl << "[" << count << "] Output results generated." << std::endl;
 
-		return playersRating;
+		return playersRatings;
 	}
 
 /* Function that is responsible for swapping two variables with each other. */
@@ -155,6 +164,7 @@ void swap(T* left_hand_side, T* right_hand_side)
 //Main function.
 int main()
 	{
+		/* Preliminary working variables. */
 		int int_count {V_ZERO};
 		int int_minimum {}, int_maximum {};
 		int int_beginners_minimum{}, int_beginners_maximum{};
@@ -163,6 +173,7 @@ int main()
 		std::list<int> beginners{};
 		std::list<int> pros{};
 
+		/* The container is generated with the specified range of random values. */
 		std::cout << "Players ratings." << std::endl;
 		int_count = obtainValue<int>("Enter the data number: ", &int_count);
 
@@ -171,10 +182,10 @@ int main()
 		int_maximum = obtainValue<int>("+ Generative maximum value: ", &int_maximum);
 
 		std::list<int> allPlayers(int_count);
-
-		int_count = fillRatings<int>(allPlayers, int_minimum, int_maximum);
+		allPlayers = fillRatings<int>(allPlayers, int_minimum, int_maximum);
 		getPause("Press the ENTER key to continue...");
 
+		/* Capturing and separating the range of values ​​for Beginners. */
 		std::cout << std::endl << "Value Range for Beginners." << std::endl;
 		int_beginners_minimum = obtainValue<int>("+ Beginners minimum value: ", &int_beginners_minimum);
 		int_beginners_maximum = obtainValue<int>("+ Beginners maximum value: ", &int_beginners_maximum);
@@ -182,6 +193,7 @@ int main()
 		beginners = separateRatings<int>(allPlayers, beginners, int_beginners_minimum, int_beginners_maximum);
 		getPause("Press the ENTER key to continue...");
 
+		/* Capturing and separating the range of values ​​for Pros. */
 		std::cout << std::endl << "Value Range for Pros." << std::endl;
 		int_pros_minimum = obtainValue<int>("+ Pros minimum value: ", &int_pros_minimum);
 		int_pros_maximum = obtainValue<int>("+ Pros maximum value: ", &int_pros_maximum);
@@ -189,6 +201,7 @@ int main()
 		pros = separateRatings<int>(allPlayers, pros, int_pros_minimum, int_pros_maximum);
 		getPause("Press the ENTER key to continue...");
 
+		/* Display of values ​​for each segmented list. */
 		int_count = displayRatings<int>("All Players", allPlayers);
 		getPause("Press the ENTER key to continue...");
 
@@ -198,6 +211,7 @@ int main()
 		int_count = displayRatings<int>("Pros", pros);
 		getPause("Press the ENTER key to continue...");
 
+		/* Program completion. */
 		std::cout << std::endl << "Done!" << std::endl;
 		std::cout << "This program has ended." << std::endl;
 
