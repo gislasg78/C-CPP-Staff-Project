fcce430a4830cf2f5e922d4b7e28077417a4b2cc
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-04-29 07:13:22 -0600

    Mathematical Functions General Project
    This program exemplifies the use of shallow and deep copying of classes in C++ and controls different circumstances of assignment and construction of objects of the instantiated class.

--- VStrings3.cpp
+++ VStrings3.cpp
@@ -1,8 +1,6 @@
 #include <cstring>
 #include <iostream>
-#include <new>
 
-constexpr size_t	V_NINE	{9};
 constexpr size_t	V_ONE	{1};
 constexpr size_t	V_ZERO	{};
 
@@ -13,13 +11,11 @@ class Hire
 		size_t length{};
 
 	public:
-		Hire(const char* _data = nullptr, const size_t _length = V_ZERO)
+		Hire(const char* _data = nullptr, const size_t _length = V_ZERO) : data(const_cast<char*>(_data)), length(_length)
 		{
 			if (_data && _length)
 			{
-				length = _length;
-
-				if ((data = new (std::nothrow) char[length + V_ONE]()))
+				if ((data = new char[length + V_ONE]()))
 				{
 					for (size_t i{}, j{}; data && i < length && j < std::strlen(_data); i++, j++)
 						data[i] = _data[j];
@@ -29,45 +25,45 @@ class Hire
 				else
 				{
 					std::cerr << std::endl << "It is not possible to allocate new memory blocks." << std::endl;
-					throw std::invalid_argument("The memory allocation has failed.");
+					throw std::invalid_argument("The memory allocation has failed!");
 				}
-
 			}
 			else
 			{
-				length = V_ONE;
-
-				if ((data = new char[length]()));
-				else
-					{
-						std::cerr << std::endl << "It is not possible to allocate new memory blocks." << std::endl;
-						throw std::bad_alloc();
-					}
+				if ((data = new char[length = V_ONE]()) == nullptr)
+				{
+					std::cerr << std::endl << "It is not possible to allocate new memory blocks." << std::endl;
+					throw std::bad_alloc();
+				}
 			}
 		}
 
+		Hire& operator= (const Hire& hire);
+		friend std::ostream& operator<< (std::ostream& os, const Hire& hire);
+
 		~Hire()
 		{
-			if (data && length)
+			if (data)
 			{
 				length = V_ZERO;
 				delete[] data;
 				data = nullptr;
 			}
 		}
-
-		Hire& operator= (const Hire& hire);
-		friend std::ostream& operator<< (std::ostream& os, const Hire& hire);
 };
 
 std::ostream& operator<< (std::ostream& os, const Hire& hire)
 {
 	os << "Visualization of the 'Hire' object." << std::endl;
 	os << "+ Pointer direction:\t(" << &hire.data << ")." << std::endl;
-	os << "+ First position:\t[" << *hire.data << "]." << std::endl;
-	os << "+ Content pointer:\t{" << hire.data << "}." << std::endl;
-	os << "+ Length of string:\t<" << hire.length << ">." << std::endl;
 
+	if (hire.data)
+	{
+		os << "* First position:\t[" << *hire.data << "]." << std::endl;
+		os << "* Content pointer:\t{" << hire.data << "}." << std::endl;
+	}
+
+	os << "+ Length of string:\t<" << hire.length << ">." << std::endl;
 	return os;
 };
 
@@ -79,7 +75,7 @@ Hire& Hire::operator= (const Hire& hire)
 
 		length = hire.length;
 
-		if ((data = new (std::nothrow) char[length + V_ONE]()))
+		if ((data = new char[length + V_ONE]()))
 		{
 			for (size_t i{}, j{}; data && i < length && j < std::strlen(hire.data); i++, j++)
 				data[i] = hire.data[j];
@@ -89,7 +85,7 @@ Hire& Hire::operator= (const Hire& hire)
 		else
 		{
 			std::cerr << std::endl << "It is not possible to allocate new memory blocks." << std::endl;
-			throw std::runtime_error("The memory allocation has failed.");
+			throw std::runtime_error("The memory allocation has failed!");
 		}
 	}
 
@@ -98,32 +94,40 @@ Hire& Hire::operator= (const Hire& hire)
 
 int main()
 {
-	std::cout << "Creation of objects of the type 'Hire'." << std::endl;
+	std::string str_noun = std::string();
 
-	Hire alex("Alexander", V_NINE);
+	std::cout << "Creation of objects of the type 'Hire'." << std::endl;
+	std::cout << "Enter your full name: ";
+	std::getline(std::cin >> std::ws, str_noun);
+	const char* str_Name = str_noun.c_str();
+
+	Hire someone(str_Name, std::strlen(str_Name));
 	Hire employee;
 
-	std::cout << std::endl << "Alexander." << std::endl;
-	std::cout << alex;
+	std::cout << std::endl << str_Name << "." << std::endl;
+	std::cout << someone;
 
 	std::cout << std::endl << "Employee." << std::endl;
 	std::cout << employee;
 
-	std::cout << std::endl << "Employee = Alexander." << std::endl;
-	employee = alex;
+	std::cout << std::endl << "Employee = " << str_Name << "." << std::endl;
+	employee = someone;
 	std::cout << employee;
 
-	std::cout << std::endl << "Alexander = Employee." << std::endl;
-	alex = employee;
-	std::cout << alex;
+	std::cout << std::endl << str_Name << " = Employee." << std::endl;
+	someone = employee;
+	std::cout << someone;
 
-	std::cout << std::endl << "Alexander = Alexander." << std::endl;
-	alex = alex;
-	std::cout << alex;
+	std::cout << std::endl << str_Name << " = " << str_Name << "." << std::endl;
+	someone = someone;
+	std::cout << someone;
 
 	std::cout << std::endl << "Employee = Employee." << std::endl;
 	employee = employee;
 	std::cout << employee;
+
+	std::cout << std::endl << "Done!" << std::endl;
+	std::cout << "This program has ended." << std::endl;
 
 	return V_ZERO;
 }
