a999a99d51c734a54e840de3882d7eb8b2960555
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-05-02 09:57:32 -0600

    Mathematical Functions General Project
    This program simply exemplifies the use of Base and Derived classes using dynamic memory allocations and accessing their respective member functions.

--- CastCPP.cpp
+++ CastCPP.cpp
@@ -26,25 +26,52 @@ int main()
 	std::cout << "Inheritance.\n";
 
 	/* Normal assignement. */
+	std::cout << std::endl << "Normal assignment." << std::endl;
 	Base *b = new Base;
 	b->f();
 	Derived *d = new Derived;
 	d->f();
 
 	/* Polymorfism. */
+	std::cout << std::endl << "Polymorfism." << std::endl;
 	Base *bd = new Derived;
 	bd->f();
 	bd->Base::f();
 
+	/* To force the behavior of a 'Derived' class
+	   as if it were exclusively a 'Base' class. */
+	std::cout << std::endl << "Conversion of a 'Base' type containing a 'Derivative' object to a 'Base' type." << std::endl;
 	Base *bd1 = dynamic_cast<Base*>(bd);
 	bd1->f();
 
+	Base *db = static_cast<Base*>(bd);
+	db->f();
+
+	Base *dbb = dynamic_cast<Base*>(d);
+	dbb->f();
+
+	Base *bdb = static_cast<Base*>(d);
+	bdb->f();
+
 	/* A 'Base' object type can contain instances of objects
-	   of the same type or of Derived types. */
+	   of the same type or of 'Derived' types. */
+	std::cout << std::endl << "Conversion of a 'Base' type containing a 'Derivative' object to a 'Derivative' type." << std::endl;
+
 	if (Derived *db1 = dynamic_cast<Derived*>(bd))
 		db1->f();
 	else
-		std::cout << "It is not Derived!\n";
+		std::cout << "It is not a Derived!\n";
+
+	Derived *bdbd = static_cast<Derived*>(bd);
+	bdbd->f();
+
+	if (Derived *bddb = dynamic_cast<Derived*>(d))
+		bddb->f();
+	else
+		std::cout << "It is not a Derived!\n";
+
+	Derived *bbdd = static_cast<Derived*>(d);
+	bbdd->f();
 
 	delete b;
 	delete d;
