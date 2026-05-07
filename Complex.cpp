da631a3c61814804127c04eb029e3be66673dec3
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-05-07 15:20:30 -0600

    Mathematical Functions General Project
    This program receives a string of characters with the typical format of a complex number and extracts the corresponding values ​​converted to decimal numerics for further processing. Includes overloading of the std::cin & std::cout classes and inheritance with exceptions.

--- Complex.cpp
+++ Complex.cpp
@@ -1,15 +1,18 @@
-#include <exception>
+/* This program exemplifies the use of a class that functions
+   to store complex numbers and perform mathematical operations
+   with those numbers. */
+
+/* Libraries Standards of Work. */
 #include <iostream>
 #include <sstream>
-#include <typeinfo>
 
-#define V_MINUS_TWO -2
-#define V_ONE       1
-#define V_PLUS_SIGN 0x2b
-#define V_ZERO      0
+/* Symbolic work constants. */
+constexpr int  V_MINUS_TWO	{-2};
+constexpr int  V_ONE		{1};
+constexpr char V_PLUS_SIGN	{0x2b};
+constexpr int  V_ZERO		{};
 
-
-//Class to handle custom exceptions.
+/* Class to handle custom exceptions. */
 class ComplexNumberFormatErrorException : public std::exception
 	{
 		public:
@@ -20,87 +23,141 @@ class ComplexNumberFormatErrorException : public std::exception
 	};
 
 
-//Definition of the 'Complex' class.
+/* Definition of the 'Complex' class. */
+template <typename TX, typename TY>
 class Complex
 	{
+		private:
+			/* Member variables of the 'Complex' class. */
+			TX real {V_ZERO};
+			TY imaginary {V_ZERO};
+
+			/* Overloading of the '>>' extraction operator. */
+			template <typename U, typename V>
+			friend std::istream& operator>> (std::istream& in, Complex<U, V>& complex);
+
+			/* Overloading of the '<<' insertion operator. */
+			template <typename U, typename V>
+			friend std::ostream& operator<< (std::ostream& out, const Complex<U, V>& complex);
+
+		protected:
+			/* Static variable for counting class instances. */
+			static size_t counter;
+
 		public:
-			double dbl_real = V_ZERO;
-			double dbl_imaginary = V_ZERO;
+			/* Default constructor. */
+			Complex() : real(V_ZERO), imaginary(V_ZERO)				{(*this).counter++;}
 
-			//Default constructor.
-			Complex() : dbl_real(V_ZERO), dbl_imaginary(V_ZERO) {}
+			/* Constructor with mandatory parameters. */
+			Complex(TX _real, TY _imaginary) : real(_real), imaginary(_imaginary)	{this->counter++;}
 
-			//Constructor with parameters.
-			Complex(double dbl_real, double dbl_imaginary) : dbl_real(dbl_real), dbl_imaginary(dbl_imaginary) {}
+			/* Functions that access the member variables of the 'Complex' class. */
+			const size_t& getCounter()	const		{return (*this).counter;}
 
-			//Overloading of the '>>' operator.
-			friend std::istream& operator>> (std::istream& console_input, Complex& complex);
+			const TX& getReal()		const		{return this->real;}
+			TX& getReal()					{return (*this).real;}
+
+			const TY& getImaginary()	const		{return this->imaginary;}
+			TY& getImaginary()				{return (*this).imaginary;}
+
+			/* Modifying functions to the member variables of the 'Complex' class. */
+			void setReal (const TX& _real)			{this->real = _real;}
+			void setImaginary (const TY& _imaginary)	{this->imaginary = _imaginary;}
+
+			/* Virtual destructor for polymorphism and inheritance. */
+			virtual ~Complex() = default;
 	};
 
-
-//Defining the '>>' operator overload for std::cin.
-std::istream& operator>> (std::istream& input, Complex& complex)
+/* Defining the '>>' operator overload for std::cin. */
+template <typename T, typename U>
+std::istream& operator>> (std::istream& in, Complex<T, U>& complex)
 	{
 		/* Preliminary working variables. */
-		std::string inputStr;
-		input >> inputStr;
+		std::string str_value {std::string()};
+		std::cin >> str_value;
 
-		//Find the position of the '+' symbol.
-		size_t plusPos = inputStr.find(V_PLUS_SIGN);
+		/* Find the position of the '+' symbol. */
+		std::size_t plusPos = str_value.find(V_PLUS_SIGN);
 
+		/* It is validated whether the position returned when searching for the '+' symbol is valid. */
 		if (plusPos == std::string::npos)
 			{
-				//Handle error if format is not correct.
-				input.setstate(std::ios::failbit);
+				/* Handle error if format is not correct. */
+				std::cin.setstate(std::ios::failbit);
 				throw ComplexNumberFormatErrorException();
 			}
 		else
 			{
-				//Extract the real part and the imaginary part of the complex number from the character string as decimals.
+				/* Extract the real part and the imaginary part of the complex number from the string. */
 				try
 					{
-						complex.dbl_real = std::stod(inputStr.substr(V_ZERO, plusPos));
-						complex.dbl_imaginary = std::stod(inputStr.substr(plusPos + V_ONE, inputStr.length() - plusPos + V_MINUS_TWO)); //-2 to remove 'i'.
+						complex.real = std::stod(str_value.substr(V_ZERO, plusPos));
+						complex.imaginary = std::stod(str_value.substr(plusPos + V_ONE, str_value.length() - plusPos + V_MINUS_TWO));	//-2 to remove 'i'.
 					}
-				catch (std::exception& ex)
+				catch (const std::exception& e)
 					{
-						std::cerr << "An exception has occurred: " << '\x5b' << ex.what() << '\x5d' << std::endl;
+						std::cerr << "Exception thrown: " << '\x5b' << e.what() << '\x5d' << '\x2e' << std::endl;
 					}
 			}
 
-		return input;
+		return in;
 	}
 
+/* Defining the '<<' operator overload for std::cout. */
+template <typename T, typename U>
+std::ostream& operator<< (std::ostream& out, const Complex<T, U>& complex)
+	{
+		std::cout << std::endl << "Information about the class 'Complex' Number." << std::endl;
+		std::cout << "> Counter:\t" << '\x5b' << complex.counter << '\x5d' << '\x2e' << std::endl;
+		std::cout << "+ Real:\t\t" << '\x5b' << complex.real << '\x5d' << '\x2e' << std::endl;
+		std::cout << "+ Imaginary:\t" << '\x5b' << complex.imaginary << '\x5d' << '\x2e' << std::endl;
+		std::cout << "* Type:\t\t" << '\x5b' << typeid(complex).name() << '\x5d' << '\x2e' << std::endl;
 
+		return out;
+	}
+
+/* Initialization of the static variable for counting class instances. */
+template <typename T, typename U>
+size_t Complex<T, U>::counter {V_ZERO};
+
+//Main function.
 int main()
 	{
-		//Initial header messages.
-		Complex c;
+		/* Initial header messages. */
+		Complex<double, double> *p_complex {new Complex<double, double>{}};
 		std::cout << "Program that tests with exceptions." << std::endl;
-		std::cout << "Enter a complex number of the form: 'a+bi': ";
+		std::cout << "Enter a complex number of the exact form: 'a+bi': ";
 
-		//Testing and obtaining the complex number of the form: 'a+bi'.
+		/* Testing and obtaining the complex number of the form: 'a+bi'. */
 		try
 			{
-				std::cin >> c;
+				try
+					{
+						std::cin >> *p_complex;
+					}
+				catch (const std::exception& e)
+					{
+						std::cerr << "Exception thrown: " << '\x5b' << e.what() << '\x5d' << '\x2e' << std::endl;
+						throw e;
+					}
 			}
-		catch (std::exception& ex)
+		catch (const std::exception& e)
 			{
-				std::cerr << "An exception has occurred: " << '\x5b' << ex.what() << '\x5d' << std::endl;
+				std::cerr << "Exception thrown: " << '\x5b' << e.what() << '\x5d' << '\x2e' << std::endl;
 			}
-
-		//Tipo de objeto instanciado de la clase 'Complejo'.
-		std::cout << "Instance of class 'Complex' is of type: [" << typeid(c).name() << "]." << std::endl;
 
 		//Sets flag on input as invalid complex number format.
 		if (std::cin.fail())
 			{
-				std::cerr << "Mistake! Incorrect input complex number format." << std::endl;
+				std::cerr << std::endl << "Mistake! Incorrect input complex number format." << std::endl;
 			}
 		else
 			{
-				std::cout << "Complex number entered: [" << c.dbl_real << "] + [" << c.dbl_imaginary << "]i." << std::endl;
+				std::cout << *p_complex;
+				std::cout << std::endl << "Complex number entered: " << '\x5b' << (*p_complex).getReal() << '\x5d' << " + " << '\x5b' << (*p_complex).getImaginary() << '\x5d' << "i" << '\x2e' << std::endl;
 			}
+
+		delete p_complex;
 
 		return V_ZERO;
 	}
