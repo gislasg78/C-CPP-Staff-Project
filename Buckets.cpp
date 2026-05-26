cfaef7d854e95f6cce9c8a149236e9ca01a39e36
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-05-26 13:22:14 -0600

    Mathematical Functions General Project
    The primary purpose of these programs is to implement different applications of the properties of object-oriented programming in C++, with the aim of showing examples of inheritance between base classes and derived classes, as well as overloading of the 'new' and 'delete' operators in the same programming language cited.

--- Buckets.cpp
+++ Buckets.cpp
@@ -11,76 +11,90 @@
 #include <sstream>
 
 /* Symbolic work constants. */
-#define	NULL_CHARACTER	'\0'
-
-#define	V_CHAR_LOWER_Y	'\x79'
-#define	V_CHAR_UPPER_Y	'\x59'
+template <typename T>
+constexpr T CARRIAGE_RETURN		{T('\n')};
+template <typename T>
+constexpr T NULL_CHARACTER		{T('\0')};
+template <typename T>
+constexpr T V_CHAR_LOWER_Y		{T('\x79')};
+template <typename T>
+constexpr T V_CHAR_UPPER_Y		{T('\x59')};
 
 /* Working numerical constants. */
-#define	V_ONE		1
-#define	V_TEN		10
-#define	V_ZERO		0
-#define	V_4096		4096
+template <typename T>
+constexpr T MAX_NUMBER_OF_BUCKETS	{T{20}};
+template <typename T>
+constexpr T MAX_SIZE_OF_BUCKET		{T(4096)};
+
+template <typename T>
+constexpr T V_ONE			{T(1)};
+template <typename T>
+constexpr T V_ZERO			{T(0)};
 
 /* Class type Exception for memory segment allocation errors. */
 class MemorySegmentAllocationException: public std::exception
-	{
-		public:
-			const char* what() const noexcept override
-				{
-					return "Damn! Memory Segment Allocation Error. Bucket Array already occupied.";
-				}
-	};
+{
+	public:
+		const char* what() const noexcept override
+		{
+			return "Damn! Memory Segment Allocation Error. Inner Bucket Array already occupied.";
+		}
+};
 
 /* 'Bucket' type class to house memory blocks of a given size in an array of a given size. */
 struct Bucket
+{
+	/* Symbolic static constants of the number of buckets and the size per bucket. */
+	static constexpr std::size_t number_of_buckets { MAX_NUMBER_OF_BUCKETS<size_t> };
+	static constexpr std::size_t size_of_bucket { MAX_SIZE_OF_BUCKET<size_t> };
+
+	/* Two-dimensional matrix with the number of rows related to the number of buckets
+           and columns according to the size per bucket. */
+	std::byte matrix_buckets[number_of_buckets][size_of_bucket]{};
+	bool bucket_used[number_of_buckets]{};
+
+	/* Static member function to house the required memory. */
+	static void *allocate_memory(Bucket& bucket, std::size_t num_bytes)
 	{
-		static constexpr std::size_t number_of_buckets{ V_TEN };
-		static constexpr std::size_t size_of_bucket{ V_4096 };
-
-		std::byte matrix_buckets[number_of_buckets][size_of_bucket]{};
-		bool bucket_used[number_of_buckets]{};
-
-		static void *allocate_memory(Bucket& bucket, std::size_t bytes)
+		if (num_bytes <= size_of_bucket)
+		{
+			for (std::size_t idx{}; idx < number_of_buckets; idx++)
 			{
-				if (bytes <= size_of_bucket)
-					{
-						for (std::size_t idx{}; idx < number_of_buckets; idx++)
-							{
-								if (!bucket.bucket_used[idx])
-									{
-										bucket.bucket_used[idx] = true;
-										return bucket.matrix_buckets[idx];
-									}
-							}
-					}
-
-				throw MemorySegmentAllocationException();
+				if (!bucket.bucket_used[idx])
+				{
+					bucket.bucket_used[idx] = true;
+					return bucket.matrix_buckets[idx];
+				}
 			}
+		}
 
-		void static deallocate_memory(Bucket& bucket, void *ptr, size_t bytes)
+		throw MemorySegmentAllocationException();
+	}
+
+	/* Static member function to deallocate (free up) the required memory. */
+	void static deallocate_memory(Bucket& bucket, void *ptr, size_t num_bytes)
+	{
+		for (std::size_t idx{}; idx < number_of_buckets; idx++)
+		{
+			if (bucket.matrix_buckets[idx] == ptr)
 			{
-				for (std::size_t idx{}; idx < number_of_buckets; idx++)
-					{
-						if (bucket.matrix_buckets[idx] == ptr)
-							{
-								if (bytes <= size_of_bucket)
-									{
-										for (std::size_t ind{}; ind < size_of_bucket && ind <= bytes; ind++)
-											bucket.matrix_buckets[idx][ind] = static_cast<std::byte>(NULL_CHARACTER);
-									}
-								else
-									throw std::bad_alloc();
+				if (num_bytes <= size_of_bucket)
+				{
+					for (std::size_t ind{}; ind < size_of_bucket && ind <= num_bytes; ind++)
+						bucket.matrix_buckets[idx][ind] = static_cast<std::byte>(NULL_CHARACTER<char>);
+				}
+				else
+					throw std::bad_alloc();
 
-								bucket.bucket_used[idx] = false;
-								return;
-							}
-					}
+				bucket.bucket_used[idx] = false;
+				return;
 			}
-	};
+		}
+	}
+};
 
 /* Global declaration of an instance of a class of type 'Bucket'. */
-Bucket bucket;
+Bucket bucket;	//Default Class constructors, assigment operators and destructor.
 
 /* Improvised 'Template' type function to capture a given string of characters. */
 template <typename T>
@@ -88,23 +102,23 @@ const T& capture_a_value(T* const ptr_value)
 	{
 		if (ptr_value)
 			{
-				std::string str_value{};
+				std::string str_value {std::string()};
 				std::getline(std::cin >> std::ws, str_value);
 				str_value.erase(std::remove_if(str_value.begin(), str_value.end(), ::isspace), str_value.end());
 				std::stringstream(str_value) >> *ptr_value;
 
-				std::cout << "Value entered: [" << *ptr_value << "]. OK!" << std::endl;
+				std::cout << "+ Value entered:\t[" << *ptr_value << "]. OK!" << std::endl;
 			}
 		else
 			std::cerr << std::endl << "A valid memory address was not provided." << std::endl;
 
-		return *ptr_value;
+		return (ptr_value) ? *ptr_value : V_ZERO<T>;
 	}
 
 /* Overloading the 'new' operator whose argument is a number of characters to assign. */
-void* operator new(size_t bytes)
+void* operator new(size_t num_bytes)
 	{
-		return Bucket::allocate_memory(bucket, bytes);
+		return Bucket::allocate_memory(bucket, num_bytes);
 	}
 
 /* Overloading the 'delete' operator whose argument is a 'void' pointer to be evicted. */
@@ -114,35 +128,35 @@ void operator delete(void* ptr)
 	}
 
 /* Overloading the 'delete' operator whose argument is a 'void' pointer to be evicted and quantity of bytes to free. */
-void operator delete(void* ptr, size_t bytes)
+void operator delete(void* ptr, size_t num_bytes)
 	{
-		return Bucket::deallocate_memory(bucket, ptr, bytes);
+		return Bucket::deallocate_memory(bucket, ptr, num_bytes);
 	}
 
 //Main function.
 int main()
 	{
 		/* Preliminary working variables. */
-		char chr_response = NULL_CHARACTER;
-		int first_value = V_ZERO, second_value = V_ZERO, third_value = V_ZERO;
-		size_t counter = V_ZERO;
+		char chr_response {NULL_CHARACTER<char>};
+		int first_value = V_ZERO<int>, second_value = {V_ZERO<int>}, third_value = V_ZERO<int>;
+		size_t counter = {V_ZERO<size_t>};
 
 		/* Capture of values ​​to be assigned to integer type pointers. */
 		std::cout << "Overloading of the 'new' and 'delete' operators." << std::endl;
-		std::cout << "First  value : ";
+		std::cout << "> First  value: ";
 		first_value = capture_a_value<int>(&first_value);
-		std::cout << "Second value : ";
+		std::cout << "> Second value: ";
 		second_value = capture_a_value<int>(&second_value);
-		std::cout << "Third value  : ";
+		std::cout << "> Third  value: ";
 		third_value = capture_a_value<int>(&third_value);
 
 		/* Information about the assigned 'Bucket'. */
 		std::cout << std::endl << "Bucket Information." << std::endl;
-		std::cout << "+ Quantity of buckets:\t[" << Bucket::number_of_buckets << "]." << std::endl;
-		std::cout << "+ Size of each bucket:\t[" << Bucket::size_of_bucket << "]." << std::endl << std::endl;
+		std::cout << "+ Quantity of buckets:\t[" << Bucket::number_of_buckets << "]\t=\t[" << MAX_NUMBER_OF_BUCKETS<size_t> << "]." << std::endl;
+		std::cout << "+ Size of each bucket:\t[" << Bucket::size_of_bucket << "]\t=\t[" << MAX_SIZE_OF_BUCKET<size_t> << "]." << std::endl << std::endl;
 
 		/* Assignment of independent dynamic objects of integer type. */
-		std::cout << "Generating new dynamic objects..." << std::endl;
+		std::cout << "Generating three new dynamic objects..." << std::endl;
 
 		int *first_tray = new int {first_value};
 		int *second_tray = new int {second_value};
@@ -150,9 +164,9 @@ int main()
 
 		/* Dumping information from assigned integer pointers. */
 		std::cout << std::endl << "Traying Information." << std::endl;
-		std::cout << "First  Tray :\t{" << std::hex << first_tray << "}\t=\t[" << std::dec << *first_tray << "]." << std::endl;
-		std::cout << "Second Tray :\t{" << std::hex << second_tray << "}\t=\t[" << std::dec << *second_tray << "]." << std::endl;
-		std::cout << "Third  Tray :\t{" << std::hex << third_tray << "}\t=\t[" << std::dec << *third_tray << "]." << std::endl;
+		std::cout << "- First  Tray:\t(" << std::hex << first_tray << ")\t=\t[" << std::dec << *first_tray << "]." << std::endl;
+		std::cout << "- Second Tray:\t(" << std::hex << second_tray << ")\t=\t[" << std::dec << *second_tray << "]." << std::endl;
+		std::cout << "- Third  Tray:\t(" << std::hex << third_tray << ")\t=\t[" << std::dec << *third_tray << "]." << std::endl;
 
 		/* Destruction of assigned integer pointers. */
 		std::cout << std::endl << "Deleting created dynamic objects..." << std::endl;
@@ -173,11 +187,11 @@ int main()
 				chr_response = capture_a_value<char>(&chr_response);
 
 				/* A loop that assigns character by character to the 'Bucket' class until it is exhausted. */
-				while ((chr_response == V_CHAR_LOWER_Y || chr_response == V_CHAR_UPPER_Y) && counter <= Bucket::number_of_buckets)
+				while ((chr_response == V_CHAR_LOWER_Y<char> || chr_response == V_CHAR_UPPER_Y<char>) && counter <= Bucket::number_of_buckets)
 					{
 						new char;	//Use the available memory of the 'Bucket' but do not save it.
 
-						std::cout << std::endl << "Char Assignment #: [" << std::dec << counter++ + V_ONE << "] of: [" << Bucket::number_of_buckets << "]." << std::endl;
+						std::cout << std::endl << "Char Assignment #: [" << std::dec << counter++ + V_ONE<size_t> << "] of: [" << Bucket::number_of_buckets << "]." << std::endl;
 						std::cout << "Do you wish to continue the allocation operation? (y/n) : ";
 
 						chr_response = capture_a_value<char>(&chr_response);
@@ -188,5 +202,8 @@ int main()
 				std::cout << std::endl << "Exception ocurred: [" << e.what() << "]." << std::endl;
 			}
 
-		return V_ZERO;
+		std::cout << CARRIAGE_RETURN<char> << "Done!" << CARRIAGE_RETURN<char>;
+		std::cout << "This program has ended." << CARRIAGE_RETURN<char>;
+
+		return V_ZERO<int>;
 	}
