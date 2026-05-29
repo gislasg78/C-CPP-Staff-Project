9e3b479add2d263f274f95f74edcf0d869c82741
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-05-29 08:12:23 -0600

    Mathematical Functions General Project
    This simple program exemplifies the use of RValues ​​in C++.

--- RValues.cpp
+++ RValues.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <sstream>
 
-constexpr int V_ZERO {};
+template <typename T>
+constexpr T V_ZERO	{T(0)};
 
 template<typename TX, typename TY>
-auto sum(TX x, TY y)
+auto sum(TX& x, TY& y)
 {return x + y;}
 
 template<typename TX, typename TY, typename TZ>
@@ -28,11 +29,26 @@ int main()
 	str_value.erase(std::remove_if(std::begin(str_value), std::end(str_value), ::isspace), std::end(str_value));
 	std::stringstream(str_value) >> y;
 
-	const int &&rref1 {sum<int, int>(x, y)};
-	int &&rref2 {sum<int, int>(y, x)};
+	std::cout << std::endl << "Outcomes." << std::endl;
 
+	/* left values ​​whose step is by value. */
+	print(x, y, sum<int, int>(x, y));
+
+	/* modifiable left values. */
+	int& refX {x}, &refY {y};
+	int refSum {sum<int, int>(x, y)};
+	print(refX, refY, refSum);
+
+	const int& refCX {x}, &refCY {y};
+	const int& refCSum {sum<int, int>(x, y)};
+	print(refCX, refCY, refCSum);
+
+	/* modifiable right values. */
+	int &&rref1 {sum<int, int>(y, x)};
 	print(x, y, rref1);
-	print(y, x, rref2);
 
-	return V_ZERO;
+	const int &&rref2 {sum<int, int>(x, y)};
+	print(x, y, rref2);
+
+	return V_ZERO<int>;
 }
