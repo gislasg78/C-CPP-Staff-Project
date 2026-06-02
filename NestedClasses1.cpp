2d061200f45dfa20cf05dd6e55578fb76e9a875b
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-06-02 11:30:00 -0600

    Mathematical Functions General Project
    The purpose of this program is to exemplify static member variables, as well as nested classes that initialize and affect such static member variables.

--- NestedClasses1.cpp
+++ NestedClasses1.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
 #include <vector>
 
-#define V_UPPER_CASE_A  '\x41'
-#define V_UPPER_CASE_Z  '\x5a'
-
-#define V_LOWER_CASE_A  '\x61'
-#define V_LOWER_CASE_Z  '\x7a'
+template <typename T>
+constexpr T V_LOWER_CASE_A	{T('\x61')};
+template <typename T>
+constexpr T V_LOWER_CASE_Z	{T('\x7a')};
+template <typename T>
+constexpr T V_UPPER_CASE_A	{T('\x41')};
+template <typename T>
+constexpr T V_UPPER_CASE_Z	{T('\x5a')};
 
 template <typename T>
 class WhatEver
 {
 	public:
-		static std::vector<T> st_vector;
+		static std::vector<T> s_vector;
 
-	class InitStatic
-	{
-		public:
-			InitStatic()
-			{
-				for (T v {V_LOWER_CASE_A}; v <= V_LOWER_CASE_Z; v++)
-					st_vector.push_back(v);
-			}
-	};
+		void print() const
+		{
+			std::cout << std::endl << "Vector Contents." << std::endl;
+
+			for (const auto& item : s_vector)
+				std::cout << "{" << item << "}." << '\t';
+			std::cout << std::endl;
+		}
+
+		class InitStatic
+		{
+			public:
+				InitStatic()
+				{
+					for (T v {V_LOWER_CASE_A<T>}; v <= V_LOWER_CASE_Z<T>; v++)
+						s_vector.push_back(v);
+				}
+		};
+
+		static InitStatic s_own_initializer;
 };
 
 template <typename T>
-std::vector<T> WhatEver<T>::st_vector
+typename WhatEver<T>::InitStatic WhatEver<T>::s_own_initializer;
+
+template <typename T>
+std::vector<T> WhatEver<T>::s_vector
 {
 	[]
 	{
 		std::vector<T> my_vector {std::vector<T>()};
 
-		for (T v {V_UPPER_CASE_A}; v <= V_UPPER_CASE_Z; v++)
+		for (T v {V_UPPER_CASE_A<T>}; v <= V_UPPER_CASE_Z<T>; v++)
 			my_vector.push_back(v);
 
 		return my_vector;
@@ -43,23 +60,36 @@ int main()
 	std::cout << "Static Constructors & Static Members." << std::endl;
 
 	std::cout << std::endl << "Static vector without class instantiation." << std::endl;
-	for (auto c : WhatEver<char>::st_vector)
+	for (auto c : WhatEver<char>::s_vector)
 		std::cout << "(" << c << ")." << '\t';
 	std::cout << std::endl;
 
 	std::cout << std::endl << "Static vector with class instantiation." << std::endl;
 	WhatEver<char> whatever;
-	for (auto c : whatever.st_vector)
+	for (auto c : whatever.s_vector)
 		std::cout << "[" << c << "]." << '\t';
 	std::cout << std::endl;
 
+	whatever.print();
+
 	std::cout << std::endl << "Static vector with inner class instantiation." << std::endl;
 	WhatEver<char>::InitStatic s_initializer;
-	for (auto c : WhatEver<char>::st_vector)
+	for (auto c : WhatEver<char>::s_vector)
 		std::cout << "{" << c << "}." << '\t';
 	std::cout << std::endl;
 
-	std::cout << "\nDone!\n";
+	std::cout << std::endl << "Static vector with inner member class instantiation." << std::endl;
+	/* WhatEver<char>::s_own_initializer; */
+	for (auto c : WhatEver<char>::s_vector)
+		std::cout << "[" << c << "]." << '\t';
+	std::cout << std::endl << std::endl;
+
+	/* whatever.s_own_initializer; */
+	for (auto c : WhatEver<char>::s_vector)
+		std::cout << "{" << c << "}." << '\t';
+	std::cout << std::endl;
+
+	std::cout << '\n' << "Done!" << '\n';
 	std::cout << "This program has ended." << '\n';
 
 	return EXIT_SUCCESS;
