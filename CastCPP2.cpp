513d72b165e1e174c73c9ed9a4614cd6e4f40aa7
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-06-10 09:12:16 -0600

    Mathematical Functions General Project
    This program simply exemplifies the use of Base and Derived classes using dynamic memory allocations and accessing their respective member functions.

---
+++ CastCPP2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+class Base
+{
+	public:
+		virtual Base* getThis()
+		{
+			std::cout << "Calling (Base)::(getThis())." << std::endl;
+			return this;
+		}
+
+		virtual void printType()
+		{
+			std::cout << "Returning a 'Base' Class." << std::endl;
+		}
+};
+
+class Derivative : public Base
+{
+	public:
+		Derivative* getThis() override
+		{
+			std::cout << "Calling [Derivative]::[getThis()]." << std::endl;
+			return this;
+		}
+
+		void printType()
+		{
+			std::cout << "Returning a 'Derivative' Class." << std::endl;
+		}
+};
+
+int main()
+{
+	std::cout << "CoVariant Classes." << std::endl;
+
+	std::cout << std::endl << "Derivative Class." << std::endl;
+	Derivative derivative {};
+	derivative.getThis()->printType();
+
+	std::cout << std::endl << "Pointer to Derivative Class." << std::endl;
+	Derivative* ptr_derivative {&derivative};
+	ptr_derivative->getThis()->printType();
+
+	std::cout << std::endl << "Pointer to Base Class from Derivative Object." << std::endl;
+	Base* ptr_base {&derivative};
+	ptr_base->getThis()->printType();
+
+	std::cout << std::endl << "Pointer to Base Class from Derivative Object." << std::endl;
+	Base* ptr_base_derivative_static {static_cast<Base*>(&derivative)};
+	ptr_base_derivative_static->getThis()->printType();
+
+	std::cout << std::endl << "Pointer to Base Class from Derivative Object." << std::endl;
+	Base* ptr_base_derivative_dynamic {dynamic_cast<Base*>(&derivative)};
+	ptr_base_derivative_dynamic->getThis()->printType();
+
+	std::cout << std::endl << "Done!" << std::endl;
+	std::cout << "This program has ended." << std::endl;
+
+	return EXIT_SUCCESS;
+}
