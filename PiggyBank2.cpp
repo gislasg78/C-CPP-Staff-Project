84fbe09ef7b9554dd6b24ff80d2096a804179c0f
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-03 16:23:31 -0600

    Mathematical Functions General Project
    This program aims to exemplify the use of a class with a smart array with a specified dimension and length stored as class attributes, and with different functions to manipulate the content and generation of said internal pointers in C++.

--- PiggyBank2.cpp
+++ PiggyBank2.cpp
@@ -10,14 +10,16 @@ constexpr T V_ELEVEN		{static_cast<T>(11)};
 template <typename T>
 constexpr T V_ONE		{static_cast<T>(1)};
 template <typename T>
+constexpr T V_TWENTY_THREE	{static_cast<T>(23)};
+template <typename T>
 constexpr T V_ZERO		{static_cast<T>(0)};
 
-template <typename T>
+template <typename TX, typename TY>
 class MyArray
 {
 	private:
-		std::size_t m_array_size {};
-		T* m_array {nullptr};
+		TX m_array_size {};
+		TY* m_array {nullptr};
 
 	protected:
 		static int s_counter;
@@ -26,18 +28,18 @@ class MyArray
 		MyArray()
 		{s_counter++;}
 
-		MyArray(const std::size_t& array_size) : m_array_size(array_size), m_array(new T[array_size]())
+		MyArray(const TX& array_size) : m_array_size(array_size), m_array(new TY[array_size]())
 		{s_counter++;}
 
-		MyArray(const MyArray<T>& my_array) : m_array_size {my_array.m_array_size}, m_array {nullptr}
+		MyArray(const MyArray<TX, TY>& my_array) : m_array_size {my_array.m_array_size}, m_array {nullptr}
 		{
 			s_counter++;
 
 			if (my_array.m_array && my_array.m_array_size)
 			{
-				if ((m_array = new T[m_array_size]{}))
+				if ((m_array = new TY[m_array_size]{}))
 				{
-					for (std::size_t idx{}; idx < my_array.m_array_size; idx++)
+					for (TX idx{}; idx < my_array.m_array_size; idx++)
 					{
 						m_array[idx] = my_array.m_array[idx];
 					}
@@ -45,16 +47,26 @@ class MyArray
 			}
 		}
 
-		MyArray(MyArray<T>&& my_array) : m_array_size {my_array.m_array_size}, m_array {my_array.m_array}
+		MyArray(MyArray<TX, TY>&& my_array) : m_array_size {my_array.m_array_size}, m_array {my_array.m_array}
 		{
 			my_array.m_array = nullptr;
-			my_array.m_array_size = V_ZERO<size_t>;
+			my_array.m_array_size = V_ZERO<TX>;
 		}
 
-		const T& operator[](const std::size_t index) const
-		{return (checkLimits(index)) ? m_array[index] : V_ZERO<T>;}
+		const TY& operator[](const TX& index) const
+		{return (checkLimits(index)) ? m_array[index] : V_ZERO<TY>;}
 
-		MyArray<T>& operator=(const MyArray<T>& my_array)
+		TY& operator[](const TX& index)
+		{
+			static TY local_value {};
+
+			if (checkLimits(index))
+				return m_array[index];
+			else
+				return local_value;
+		}
+
+		MyArray<TX, TY>& operator=(const MyArray<TX, TY>& my_array)
 		{
 			if (this != &my_array)
 			{
@@ -66,9 +78,9 @@ class MyArray
 					{
 						m_array_size = my_array.m_array_size;
 
-						if ((m_array = new T[m_array_size]{}))
+						if ((m_array = new TY[m_array_size]{}))
 						{
-							for (std::size_t idx{}; idx < my_array.m_array_size; idx++)
+							for (TX idx{}; idx < my_array.m_array_size; idx++)
 							{
 								m_array[idx] = my_array.m_array[idx];
 							}
@@ -80,7 +92,7 @@ class MyArray
 			return *this;
 		}
 
-		MyArray<T>& operator=(MyArray<T>&& my_array)
+		MyArray<TX, TY>& operator=(MyArray<TX, TY>&& my_array)
 		{
 			if (this != &my_array)
 			{
@@ -94,7 +106,7 @@ class MyArray
 						m_array = my_array.m_array;
 
 						my_array.m_array = nullptr;
-						my_array.m_array_size = V_ZERO<size_t>;
+						my_array.m_array_size = V_ZERO<TX>;
 					}
 				}
 			}
@@ -102,21 +114,24 @@ class MyArray
 			return *this;
 		}
 
-		bool checkLimits(const std::size_t& index) const
-		{return (m_array) && (m_array_size > std::numeric_limits<size_t>::min() && m_array_size < std::numeric_limits<size_t>::max()) && (index >= V_ZERO<size_t> && index < m_array_size);}
+		bool checkSize(const TX& array_size = V_ZERO<TX>) const
+		{return (array_size > std::numeric_limits<TX>::min() && array_size < std::numeric_limits<TX>::max());}
 
-		void erase(const std::size_t& index)
+		bool checkLimits(const TX& index) const
+		{return (m_array) && (index >= V_ZERO<TX> && index < m_array_size);}
+
+		void erase(const TX& index)
 		{
-			if (checkLimits(index))
+			if (checkLimits(index) && checkSize(m_array_size))
 			{
-				std::size_t t_array_size {m_array_size - V_ONE<size_t>};
-				T* t_array {new T[t_array_size]()};
+				TX t_array_size {m_array_size - V_ONE<TX>};
+				TY* t_array {new TY[t_array_size]()};
 
-				for (size_t idx{}; idx < index; idx++)
+				for (TX idx{}; idx < index; idx++)
 					t_array[idx] = *(m_array + idx);
 
-				for (size_t idx{index + V_ONE<size_t>}; idx < m_array_size; idx++)
-					t_array[idx - V_ONE<size_t>] = *(m_array + idx);
+				for (TX idx{index + V_ONE<TX>}; idx < m_array_size; idx++)
+					t_array[idx - V_ONE<TX>] = *(m_array + idx);
 
 				release();
 				m_array = t_array;
@@ -124,29 +139,39 @@ class MyArray
 			}
 		}
 
-		const T* const& getPtrArray() const
+		const TY *const &getPtrArray() const
 		{return m_array;}
 
-		const std::size_t& getSize() const
+		const TX& getSize() const
 		{return m_array_size;}
 
-		const T& getValue(const std::size_t& index) const
-		{return checkLimits(index) ? m_array[index] : V_ZERO<T>;}
+		const TY& getValue(const TX& index) const
+		{return checkLimits(index) ? m_array[index] : V_ZERO<TY>;}
 
-		void insert(const std::size_t& index, const T& value)
+		TY& getValue(const TX& index)
 		{
-			if (checkLimits(index))
-			{
-				std::size_t t_array_size {m_array_size + V_ONE<size_t>};
-				T* t_array {new T[t_array_size]()};
+			static TY local_value {};
 
-				for (size_t idx{}; idx < index; idx++)
+			if (checkLimits(index))
+				return m_array[index];
+			else
+				return local_value;
+		}
+
+		void insert(const TX& index, const TY& value)
+		{
+			if (checkLimits(index) && checkSize(m_array_size))
+			{
+				TX t_array_size {m_array_size + V_ONE<TX>};
+				TY* t_array {new TY[t_array_size]()};
+
+				for (TX idx{}; idx < index; idx++)
 					*(t_array + idx) = m_array[idx];
 
 				t_array[index] = value;
 
-				for (size_t idx{index}; idx < m_array_size; idx++)
-					*(t_array + idx + V_ONE<size_t>) = m_array[idx];
+				for (TX idx{index}; idx < m_array_size; idx++)
+					*(t_array + idx + V_ONE<TX>) = m_array[idx];
 
 				release();
 				m_array = t_array;
@@ -171,7 +196,7 @@ class MyArray
 					std::cout << std::endl << "> First element:\t{" << *m_array << "}." << std::endl;
 					std::cout << "* List of items. *" << std::endl;
 
-					for (std::size_t idx{}; idx < m_array_size; idx++)
+					for (TX idx{}; idx < m_array_size; idx++)
 					{
 						std::cout << "#: [" << counter++ << "]\t:\t(" << &m_array[idx] << ")\t=\t{" << m_array[idx] << "}." << std::endl;
 					}
@@ -188,52 +213,47 @@ class MyArray
 			{
 				delete [] m_array;
 				m_array = nullptr;
-				m_array_size = V_ZERO<size_t>;
+				m_array_size = V_ZERO<TX>;
 			}
 		}
 
 		void restore()
 		{
 			if (m_array && m_array_size)
-				for (std::size_t idx{}; idx < m_array_size; idx++)
-					m_array[idx] = V_ZERO<size_t>;
-			else
-				setSize(V_ONE<size_t>);
+				for (TX idx{}; idx < m_array_size; idx++)
+					m_array[idx] = V_ZERO<TX>;
 		}
 
-		void setSize(const std::size_t& array_size)
+		void setSize(const TX& array_size)
 		{
-			if (m_array && m_array_size)
+			if (m_array && m_array_size && checkSize(array_size))
 			{
-				if (array_size > std::numeric_limits<size_t>::min() && array_size < std::numeric_limits<size_t>::max())
-				{
-					std::size_t t_array_size {array_size};
-					T* t_array {new T[t_array_size]()};
+				TX t_array_size {array_size};
+				TY* t_array {new TY[t_array_size]()};
 
-					for (size_t idx{}; idx < t_array_size && idx < m_array_size; idx++)
-						t_array[idx] = *(m_array + idx);
+				for (TX idx{}; idx < t_array_size && idx < m_array_size; idx++)
+					t_array[idx] = *(m_array + idx);
 
-					release();
-					m_array = t_array;
-					m_array_size = t_array_size;
-				}
+				release();
+				m_array = t_array;
+				m_array_size = t_array_size;
 			}
 			else
 			{
 				m_array_size = array_size;
-				m_array = new T[m_array_size]{};
+				m_array = new TY[m_array_size]{};
 			}
 		}
 
-		void setValue(const std::size_t& index, const T& value)
+		void setValue(const TX& index, const TY& value)
 		{if (checkLimits(index)) m_array[index] = value;}
 
 		~MyArray()
 		{release(); s_counter--;}
 };
 
-template <typename T>
-int MyArray<T>::s_counter {V_ZERO<int>};
+template <typename TX, typename TY>
+int MyArray<TX, TY>::s_counter {V_ZERO<int>};
 
 void enter_a_pause(const std::string& str_Message)
 {
@@ -248,12 +268,15 @@ int main()
 {
 	std::cout << "Test Class Array Values." << std::endl;
 
-	MyArray<int> my_array {V_ELEVEN<size_t>};
+	MyArray<size_t, int> my_array {V_TWENTY_THREE<size_t>};
 
 	std::cout << std::endl << "Loading data..." << std::endl;
 	for (size_t idx{}; idx < my_array.getSize(); idx++)
 	{
+		my_array.getValue(idx) = static_cast<int>(idx);
 		my_array.setValue(idx, static_cast<int>(idx));
+		my_array[idx] = static_cast<int>(idx);
+
 		std::cout << "#: [" << idx << "] = [" << my_array.getValue(idx) << "]." << std::endl;
 	}
 	enter_a_pause("Press the ENTER key to continue...");
@@ -263,7 +286,7 @@ int main()
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Inserting a value..." << std::endl;
-	my_array.insert(V_ZERO<size_t>, V_ELEVEN<size_t>);
+	my_array.insert(V_ZERO<size_t>, V_TWENTY_THREE<size_t>);
 	my_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
@@ -280,14 +303,14 @@ int main()
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Copy constructor." << std::endl;
-	MyArray<int> your_array {my_array};
+	MyArray<size_t, int> your_array {my_array};
 	my_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 	your_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Copy assignment." << std::endl;
-	MyArray<int> her_array;
+	MyArray<size_t, int> her_array;
 	her_array = my_array;
 	my_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
@@ -295,14 +318,14 @@ int main()
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Move constructor." << std::endl;
-	MyArray<int> his_array {std::move(your_array)};
+	MyArray<size_t, int> his_array {std::move(your_array)};
 	your_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 	his_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Move assignment." << std::endl;
-	MyArray<int> our_array;
+	MyArray<size_t, int> our_array;
 	our_array = std::move(my_array);
 	my_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
@@ -310,7 +333,7 @@ int main()
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Builder with size information." << std::endl;
-	MyArray<int> their_array{V_ELEVEN<size_t>};
+	MyArray<size_t, int> their_array{V_TWENTY_THREE<size_t>};
 	their_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
@@ -325,7 +348,7 @@ int main()
 	enter_a_pause("Press the ENTER key to continue...");
 
 	std::cout << std::endl << "Setting size array..." << std::endl;
-	their_array.setSize(V_ONE<size_t>);
+	their_array.setSize(V_TWENTY_THREE<size_t>);
 	their_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
@@ -356,6 +379,11 @@ int main()
 
 	std::cout << std::endl << "Restoring..." << std::endl;
 	their_array.restore();
+	their_array.print();
+	enter_a_pause("Press the ENTER key to continue...");
+
+	std::cout << std::endl << "Setting size array..." << std::endl;
+	their_array.setSize(V_TWENTY_THREE<size_t>);
 	their_array.print();
 	enter_a_pause("Press the ENTER key to continue...");
 
