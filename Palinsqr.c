5e8df01eaf09c5477daf0cee02c9a0e32fc6a525
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-10 18:12:04 -0600

    Mathematical Functions General Project
    The primary goal of this program is to generate the greatest possible number of palindromic squares by performing iterations that check whether a number, when squared, yields a result identical to its own reverse.

--- Palinsqr.c
+++ Palinsqr.c
@@ -1,70 +1,86 @@
-#include <stdbool.h>
+/* The primary goal of this program is to generate the greatest possible number
+   of palindromic squares by performing iterations that check whether a number,
+   when squared, yields a result identical to its own reverse.
+*/
+
+/* Standard Work Libraries. */
 #include <stdio.h>
-#include <math.h>
 
-#define	V_ONE	1
-#define V_TEN   10
-#define V_ZERO  0
+/* Symbolic work constants. */
+#define	V_LOWER_LIMIT	1
+#define	V_UPPER_LIMIT	94
 
-//Function to check if a number is a palindrome.
-bool bln_Is_Palindromic_Square(int int_number)
+/* Symbolic numerical working constants. */
+#define	V_ONE		1
+#define	V_TEN		10
+#define	V_ZERO		0
+
+/* Function that returns an integer in reversed form. */
+long reverse_number(long number)
 	{
-		//Initial working variables.
-		int int_original_number = int_number;
-		int int_inverted_number = V_ZERO;
-		int int_rest_number = V_ZERO;
+		/* Preliminary working variables. */
+		long reversed_number = V_ZERO;
 
-		//Invert the number provided.
-		while (int_number > V_ZERO)
+		/* Invert the number provided. */
+		while (number)
 			{
-				int_rest_number = int_number % V_TEN;
-				int_inverted_number = int_inverted_number * V_TEN + int_rest_number;
-				int_number /= V_TEN;
+				reversed_number = reversed_number * V_TEN + (number % V_TEN);
+				number /= V_TEN;
 			}
 
-		//Compare the inverted number with the original number.
-		return int_original_number == int_inverted_number;
+		/* Return of the reversed integer. */
+		return reversed_number;
 	}
 
-//Function to create palindromic numbers.
-void generate_Palindromic_Squares(int int_number)
+/* Function to create palindromic numbers. */
+long generate_palindromic_squares(long number)
 	{
-		//Initial working variables.
-		int int_counter = V_ZERO;
-		int int_idx = V_ONE;
+		/* Initial working variables. */
+		long counter = V_ZERO;
 
-		//As long as 'n' number of palindromic squares have not been generated.
-		while (int_counter < int_number)
+		/* As long as 'n' number of palindromic squares have not been generated. */
+		for (long idx = V_ONE; counter < number; idx++)
 			{
-				//Squared number obtained.
-				int int_square_number = int_idx * int_idx;
+				/* Squared number obtained. */
+				long square_number = idx * idx;
 
-				//Check if the square is palindromic.
-				if (bln_Is_Palindromic_Square(int_square_number))
-					{
-						printf("#%3.d:\t[%10.d].\n", int_counter + V_ONE, int_square_number);
-						int_counter++;
-					}
-
-				int_idx++;
+				/* Check if the square is a palindromic number. */
+				if (square_number == reverse_number(square_number))
+					printf("(%2ld)\t[%10ld]\t{%18ld}.\n", counter++, idx, square_number);
 			}
+
+		printf("[%ld] Output generated results.\n", counter);
+
+		/* Return the count of palindromic squares. */
+		return counter;
 	}
 
-
+//Main function.
 int main()
 	{
-		//Initial working variables.
-		int int_number = V_ZERO;
+		/* Initial working variables. */
+		long counter = V_ZERO, number = V_ZERO;
 
-		//Requests the number of palindromic squares.
-		printf("Program that automatically generates palindromic squares.\n");
-		printf("Enter the number of palindromic squares to generate: ");
-		scanf("%d", &int_number);
+		/* Requests the number of palindromic squares. */
+		printf("Program that generates palindromic squares.\n");
+		printf("Quantity of palindromic squares (%d - %d): ", V_LOWER_LIMIT, V_UPPER_LIMIT);
+		scanf("%ld", &number);
 
-		//Generate palindromic squares.
-		printf("\n");
-		printf("List of generated palindromic squares.\n");
-		generate_Palindromic_Squares(int_number);
+		/* Validation of count limits. */
+		if (number >= V_LOWER_LIMIT && number <= V_UPPER_LIMIT)
+			{
+				/* Generate palindromic squares. */
+				printf("\nList of palindromic squares.\n");
+				counter = generate_palindromic_squares(number);
+
+				printf("\n[%ld] viewed outcomes.\n", counter);
+			}
+		else
+			fprintf(stderr,"\nThe value: [%ld] is outside the range between: [%d] and: [%d].\n", number, V_LOWER_LIMIT, V_UPPER_LIMIT);
+
+		/* Termination messages. */
+		printf("\nDone!\n");
+		printf("This program has ended.\n");
 
 		return V_ZERO;
 	}
