799c353c972dfa75b6b291eb5e9b9590340782c2
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-12 08:40:21 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to reverse a long integer through a series of iterations that reduce and recalculate the number until it is reversed, using the standard formula: reversed_number = reversed_number * 10 + (number % 10).

--- RevNum.c
+++ RevNum.c
@@ -4,10 +4,24 @@
 #include <stdio.h>
 
 /* Symbolic work constants. */
-#define V_TEN   10
-#define V_ZERO  0
+#define V_TEN	10
+#define V_ZERO	0
 
-/* Function that returns an integer in reversed form. */
+long counter_digits(long number)
+	{
+		/* Preliminary working variables. */
+		long counter = V_ZERO;
+
+		/* Counting the number of digits in the number. */
+		for (counter = V_ZERO; number; counter++, number /= V_TEN);
+
+		/* Return of the reversed integer. */
+		return counter;
+	}
+
+/* Function that returns an integer in reversed form.
+   Formula: reversed_number *= V_TEN; reversed_number += number % V_TEN;
+*/
 long reverse_number(long number)
 	{
 		/* Preliminary working variables. */
@@ -16,9 +30,7 @@ long reverse_number(long number)
 		/* Invert the number provided. */
 		while (number)
 			{
-				reversed_number *= V_TEN;
-				reversed_number += number % V_TEN;
-
+				reversed_number = reversed_number * V_TEN + (number % V_TEN);
 				number /= V_TEN;
 			}
 
@@ -30,21 +42,26 @@ long reverse_number(long number)
 int main()
 	{
 		/* Preliminary working variables. */
+		long digits_number = V_ZERO;
 		long number = V_ZERO;
-		long inversed_number = V_ZERO;
+		long reversed_number = V_ZERO;
 
 		/* Initial header messages. */
 		printf("Inversion of an integer.\n");
-		printf("Enter an integer: ");
+		printf("Enter an integer number: ");
 		scanf("%ld", &number);
 
+		/* Call to count the number of digits in the number. */
+		digits_number = counter_digits(number);
+
 		/* Call to the function that reverses the integer. */
-		inversed_number = reverse_number(number);
+		reversed_number = reverse_number(number);
 
 		/* Visualization of results. */
 		printf("\nResults.\n");
 		printf("+ Number:\t[%ld].\n", number);
-		printf("* Reversed:\t[%ld].\n", inversed_number);
+		printf("- Digits:\t[%ld].\n", digits_number);
+		printf("* Reversed:\t[%ld].\n", reversed_number);
 
 		/* Termination messages. */
 		printf("\nDone!\n");
