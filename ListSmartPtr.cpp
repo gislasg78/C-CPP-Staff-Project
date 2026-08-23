cb86627b5228e3af29989f2b149585cc3de0002c
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-23 09:01:46 -0600

    Mathematical Functions General Project
    This C++ example simply stores smart pointers in a list and then saves a pointer to one of the stored objects so that it can subsequently be located and removed from the list. The class is completely empty—containing neither member variables nor member functions—leaving it to the compiler to automatically generate the default constructor, copy and move constructors, copy and move assignment operators, and the destructor.

--- ListSmartPtr.cpp
+++ ListSmartPtr.cpp
@@ -8,23 +8,23 @@ class A {};
 int main()
 {
 	std::list<std::unique_ptr<A>> my_List;
-	std::unique_ptr a = std::make_unique<A>();
-	std::unique_ptr b = std::make_unique<A>();
+	std::unique_ptr u_ptr_a {std::make_unique<A>()};
+	std::unique_ptr u_ptr_b {std::make_unique<A>()};
 
-	A* target = b.get();
+	A* target = u_ptr_b.get();
 
-	my_List.push_back(std::move(a));
-	my_List.push_back(std::move(b));
+	my_List.push_back(std::move(u_ptr_a));
+	my_List.push_back(std::move(u_ptr_b));
 
 	std::cout << "List & Smart Pointers Example." << std::endl;
 	std::cout << "+ Size: [" << my_List.size() << "]." << std::endl;
 
-	std::list<std::unique_ptr<A>>::iterator it_list = std::find_if(my_List.begin(), my_List.end(),
-		[target](const std::unique_ptr<A>& ptr)
-			{return ptr.get() == target;}
+	std::list<std::unique_ptr<A>>::const_iterator itc_list = std::find_if(my_List.cbegin(), my_List.cend(),
+		[target](const std::unique_ptr<A>& u_ptr)
+			{return u_ptr.get() == target;}
 		);
 
-	if (it_list != my_List.end()) my_List.erase(it_list);
+	if (itc_list != my_List.cend()) itc_list = my_List.erase(itc_list);
 
 	std::cout << "+ Size: [" << my_List.size() << "]." << std::endl;
 }
