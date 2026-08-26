f8f98e3129af66a0b55313488b690ee445f27c48
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-26 16:15:12 -0600

    Mathematical Functions General Project
    This program simply does a descending sequential addition through recursion, according to a number entered by the user.

--- Autosum.c
+++ Autosum.c
@@ -1,24 +1,41 @@
-#include <stdio.h>
+#include <stdio.h>\
 
 #define	V_MINUS_ONE	-1
 #define	V_ONE		1
+#define	V_TWO		2
 #define	V_ZERO		0
 
-int autosum(int x)
+long sum(long x)
 	{
-		return (x < V_ONE) ? x : x + autosum(x + V_MINUS_ONE);
+		return (x * (x + V_ONE)) / V_TWO;
+	}
+
+long summation(long x)
+	{
+		return (x < V_ONE) ? x : x + sum(x + V_MINUS_ONE);
+	}
+
+long summatory(long m, long n)
+	{
+		return (m >= n) ? m : m + summatory(m + V_ONE, n);
 	}
 
 int main()
 	{
-		int x = V_ZERO;
+		long x = V_ZERO;
 
-		printf("Value [x] : ");
-		scanf("%d", &x);
+		printf("Recursive summations.\n");
+		printf("Enter a value [x] : ");
+		scanf("%ld", &x);
 
-		printf("\n");
-		printf("Value [x] : [%d].\n", x);
-		printf("Autosum   : [%d].\n", autosum(x));
+		printf("\nResults of the summations.\n");
+		printf("+ Value [x] : [%ld].\n", x);
+		printf("* Sum       : [%ld].\n", sum(x));
+		printf("* Summation : [%ld].\n", summation(x));
+		printf("* Summatory : [%ld].\n", summatory(V_ZERO, x));
+
+		printf("\nDone!\n");
+		printf("This program has ended.\n");
 
 		return V_ZERO;
 	}
